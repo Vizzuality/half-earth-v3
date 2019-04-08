3da8cb8ac4d20d84faa31851d4432e1d41c1a100
Javi Abia <weberjavi@gmail.com>
2019-04-08 16:39:35 +0200

    Delete globe component. Move sidebar to shared

--- src/components/globe/globe.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux'
-import Component from './globe-component.jsx';
-
-const mapStateToProps = ({ sidebar }) => ({ isSidebarOpen: sidebar.open })
-
-export default connect(mapStateToProps, null)(Component);
