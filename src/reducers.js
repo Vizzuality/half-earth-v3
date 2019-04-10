d2653fd37b9cb5579c71c61d96b30bce0d704595
Javi Abia <weberjavi@gmail.com>
2019-04-10 14:18:04 +0200

    Update reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,14 @@
 import { combineReducers } from 'redux';
 import { handleModule } from 'redux-tools';
 import router from './router';
-
+// Redux-modules
+import { reduxConfig as dataGlobeRedux } from 'redux_modules/data-globe';
 // Shared Components
 import { reduxConfig as sidebarRedux } from 'components/shared/sidebar';
+
+const reduxModulesReducers = {
+  dataGlobeSpec: handleModule(dataGlobeRedux)
+};
 
 const componentReducers = {
   sidebar: handleModule(sidebarRedux)
@@ -11,5 +16,6 @@ const componentReducers = {
 
 export default combineReducers({
   location: router.reducer,
+  ...reduxModulesReducers,
   ...componentReducers
 });
