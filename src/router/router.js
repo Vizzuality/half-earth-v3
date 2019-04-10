50d8940c2b55189d586daf78875ae27c6c7717a6
Javi Abia <weberjavi@gmail.com>
2019-04-10 14:15:38 +0200

    Add dataGlobeThunk to router

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,15 @@
 import { connectRoutes, NOT_FOUND, redirect } from 'redux-first-router';
 import qs from 'qs';
-
+import { dataGlobeThunk } from 'redux_modules/data-globe/data-globe-thunks'
 export const APP = 'location/APP';
+
+
 
 export const routes = {
   [APP]: {
     path: '/',
-    page: 'index.js'
+    page: 'index.js',
+    thunk: dataGlobeThunk
   },
   [NOT_FOUND]: { path: '/404', thunk: dispatch => dispatch(redirect({ type: APP })) }
 };
