5c4575adee15ef8726d1738deec301620d33b6af
Javi Abia <weberjavi@gmail.com>
2019-04-10 14:17:57 +0200

    Add redux modules alias

--- config-overrides.js
+++ config-overrides.js
@@ -12,7 +12,8 @@ module.exports = function override(config, env) {
       components: path.resolve(__dirname, 'src/components'),
       icons: path.resolve(__dirname, 'src/assets/icons'),
       styles: path.resolve(__dirname, 'src/styles'),
-      router: path.resolve(__dirname, 'src/router')
+      router: path.resolve(__dirname, 'src/router'),
+      redux_modules: path.resolve(__dirname, 'src/redux-modules')
     }
   }
   config = rewireReactHotLoader(config, env);
