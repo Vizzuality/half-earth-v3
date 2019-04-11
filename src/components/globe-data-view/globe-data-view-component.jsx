ddb6b25cbd73595dd89721ed778fe67d7cc3e7db
Javi Abia <weberjavi@gmail.com>
2019-04-11 09:57:23 +0200

    Lazy load thunks

--- src/components/globe-data-view/globe-data-view-component.jsx
+++ src/components/globe-data-view/globe-data-view-component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import { WebScene } from '@esri/react-arcgis';
-import styles from 'styles/themes/scene-theme.module.scss'
+import styles from 'styles/themes/scene-theme.module.scss';
 
 const GlobeComponent = ({ sceneConfig, isSidebarOpen }) => {
   return (
--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,22 @@
 import { connectRoutes, NOT_FOUND, redirect } from 'redux-first-router';
 import qs from 'qs';
-import { dataGlobeThunk } from 'redux_modules/data-globe/data-globe-thunks'
+
+const lazyLoadDataGlobe = (dispatch, getState) => import('redux_modules/data-globe/data-globe-thunks').then(module => module.default(dispatch, getState));
+const lazyLoadFeaturedGlobe = (dispatch, getState) => import('redux_modules/featured-globe/featured-globe-thunks').then(module => module.default(dispatch, getState));
+
 export const APP = 'location/APP';
-
-
+export const FEATURED = 'location/FEATURED';
 
 export const routes = {
   [APP]: {
-    path: '/',
+    path: '/dataGlobe',
     page: 'index.js',
-    thunk: dataGlobeThunk
+    thunk: lazyLoadDataGlobe
+  },
+  [FEATURED]: {
+    path: '/featuredGlobe',
+    page: 'index.js',
+    thunk: lazyLoadFeaturedGlobe
   },
   [NOT_FOUND]: { path: '/404', thunk: dispatch => dispatch(redirect({ type: APP })) }
 };
