6f542eda10e57b27a58569de42328430d68dea94
Javi Abia <weberjavi@gmail.com>
2019-04-11 17:55:34 +0200

    Register globeSpecs reducer on first thunk dspatch

--- src/redux-modules/data-globe/data-globe-thunks.js
+++ src/redux-modules/data-globe/data-globe-thunks.js
@@ -1,10 +1,15 @@
+import reducerRegistry from 'reducerRegistry';
 import { setDataGlobeLoading, setDataGlobeReady, setDataGlobeError } from './data-globe-actions';
+import { reduxConfig } from './data-globe';
 
 export default async function dataGlobeThunk(dispatch, getState) {
+  // Register reducer to have the dataGlobeSpec available on the store in case it is not registered
+  if (!getState().dataGlobeSpec) { reducerRegistry.registerModule('dataGlobeSpec', reduxConfig) };
   const { data } = getState().dataGlobeSpec;
   if (!data) {
     dispatch(setDataGlobeLoading());
     try {
+      // This API call fetches Arcgis js Web Scene Specification for the given item
       const dataGlobe = await fetch('https://www.arcgis.com/sharing/rest/content/items/cb5148d43124477f88d5e36e5fd566ea/data?f=json').then(response => response.json());
       dispatch(setDataGlobeReady(dataGlobe));
     } catch (e) {
--- src/redux-modules/featured-globe/featured-globe-thunks.js
+++ src/redux-modules/featured-globe/featured-globe-thunks.js
@@ -1,10 +1,15 @@
+import reducerRegistry from 'reducerRegistry';
 import { setFeaturedGlobeLoading, setFeaturedGlobeReady, setFeaturedGlobeError } from './featured-globe-actions';
+import { reduxConfig } from './featured-globe';
 
-export default async function dataGlobeThunk(dispatch, getState) {
-  const { data } = getState().dataGlobeSpec;
+export default async function featuredGlobeThunk(dispatch, getState) {
+  // Register reducer to have the featuredGlobeSpec available on the store in case it is not registered
+  if (!getState().featuredGlobeSpec) { reducerRegistry.registerModule('featuredGlobeSpec', reduxConfig) };
+  const { data } = getState().featuredGlobeSpec;
   if (!data) {
     dispatch(setFeaturedGlobeLoading());
     try {
+      // This API call fetches Arcgis js Web Scene Specification for the given item
       const dataGlobe = await fetch('https://www.arcgis.com/sharing/rest/content/items/f430e65f20bc47ff846c9c9853fe855b/data?f=json').then(response => response.json());
       dispatch(setFeaturedGlobeReady(dataGlobe));
     } catch (e) {
