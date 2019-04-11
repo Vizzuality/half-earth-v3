7c82d4f9bd70f505faee24715fa826db334408f1
Javi Abia <weberjavi@gmail.com>
2019-04-11 17:53:57 +0200

    Add layers selectors

---
+++ src/selectors/layers-selectors.js
@@ -0,0 +1,43 @@
+import { createSelector, createStructuredSelector } from 'reselect';
+
+const getQuery = ({ location }) => location.query || null;
+export const getFeaturedGlobeSpec = ({ featuredGlobeSpec }) => featuredGlobeSpec && (featuredGlobeSpec.data || null);
+export const getDataGlobeSpec = ({ dataGlobeSpec }) => dataGlobeSpec && (dataGlobeSpec.data || null);
+
+export const getActiveLayers = createSelector(
+  getQuery,
+  query => {
+    if (!query || !query.activeLayers) return null;
+    return query.activeLayers;
+  }
+)
+
+export const getFeaturedGlobeLayers = createSelector(
+  getFeaturedGlobeSpec,
+  layerSpec => {
+    if (!layerSpec) return null;
+    return layerSpec.operationalLayers.map(l => ({
+      id: l.id,
+      title: l.title
+    }))
+  }
+)
+
+export const getDataGlobeLayers = createSelector(
+  getDataGlobeSpec,
+  layerSpec => {
+    if (!layerSpec) return null;
+    return layerSpec.operationalLayers.map(l => ({
+      id: l.id,
+      title: l.title
+    }))
+  }
+)
+
+export default createStructuredSelector({
+  featuredGlobeSpec: getFeaturedGlobeSpec,
+  featuredGlobeLayers: getFeaturedGlobeLayers,
+  dataGlobeSpec: getDataGlobeSpec,
+  dataGlobeLayers: getDataGlobeLayers,
+  activeLayers: getActiveLayers
+})
