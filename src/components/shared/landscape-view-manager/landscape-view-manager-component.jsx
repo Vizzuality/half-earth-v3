b83bea8d173fce79b25442416e8c30b3e05a55d3
Javi Abia <weberjavi@gmail.com>
2019-04-24 13:01:04 +0200

    Add goTo functionality

--- src/components/shared/landscape-view-manager/landscape-view-manager-component.jsx
+++ src/components/shared/landscape-view-manager/landscape-view-manager-component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const LandscapeViewManager = ({ view, zoomLevelTrigger, onZoomChange, query, isLandscapeMode }) => {
   const [ isInteracting, setInteracting ] = useState(false);
@@ -16,6 +16,21 @@ const LandscapeViewManager = ({ view, zoomLevelTrigger, onZoomChange, query, isL
   const zoomWatcher = view.watch('zoom', function(zoomValue) {
     setZoomLevel(zoomValue)
   })
+
+  useEffect(() => {
+    const tilt = isLandscapeMode ? 65 : 0;
+    const target = { tilt };
+    const options = {
+      speedFactor: 0.5,
+      duration: 1000
+    }
+    view.goTo(target, options)
+
+    return function cleanUp() {
+      interactionWatcher.remove();
+      updateWatcher.remove();
+    }
+  }, [isLandscapeMode])
 
   if (!isInteracting || !isUpdating) {
     onZoomChange({ query: { ...query, landscapeView: zoomLevel >= zoomLevelTrigger }})
