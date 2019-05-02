d47fc1b0052ed0b7d403991d2f8160874cda5fab
Javi Abia <weberjavi@gmail.com>
2019-05-02 12:57:55 +0200

    Add grid cells painting and removing logic

--- src/components/shared/landscape-view-manager/landscape-view-manager-component.jsx
+++ src/components/shared/landscape-view-manager/landscape-view-manager-component.jsx
@@ -1,75 +1,90 @@
-import { useState, useEffect } from 'react';
+import { loadModules } from '@esri/react-arcgis';
+import { useState, useEffect, useRef } from 'react';
+import { setGridCellStyles, setGridCellGraphic, paintGridCell, removeGridCell, isLandscapeViewOnEvent, isLandscapeViewOffEvent } from 'utils/landscape-view-manager-utils';
 
 const LandscapeViewManager = ({ view, map, zoomLevelTrigger, onZoomChange, query, isLandscapeMode }) => {
-  const [ isInteracting, setInteracting ] = useState(false);
-  const [ isUpdating, setUpdating ] = useState(false);
-  const [ landscapeView, setLandscapeView ] = useState(false);
+  const [watchUtils, setWatchUtils] = useState(null);
+  const [viewExtent, setViewExtent] = useState();
+  // References for cleaning up graphics
+  const gridCellRef = useRef();
+  const landscapeModeRef = useRef()
 
-  const isLandscapeViewOnEvent = (zoomValue, landscapeView) => zoomValue >= zoomLevelTrigger && !landscapeView;
-  const isLandscapeViewOffEvent = (zoomValue, landscapeView) => zoomValue < zoomLevelTrigger && landscapeView;
 
-  const interactionWatcher = view.watch('interacting', function(interactionState) {
-    setInteracting(interactionState)
-  })
-
-  const updateWatcher = view.watch('updating', function(interactionState) {
-    setUpdating(interactionState)
-  })
-
-  const zoomWatcher = view.watch('zoom', function(zoomValue) {
-    if (isLandscapeViewOnEvent(zoomValue, landscapeView)) {
-      setLandscapeView(true)
-    } else if (isLandscapeViewOffEvent(zoomValue, landscapeView)) {
-      setLandscapeView(false)
-    }
-  })
-
+  // Load watchUtils module from ArcGis
   useEffect(() => {
-    const tilt = isLandscapeMode ? 65 : 0;
+    loadModules(["esri/core/watchUtils"]).then(([watchUtils]) => {
+      setWatchUtils(watchUtils);
+    })
+  }, []);
+
+  // Update url param
+  useEffect(() => {
+    const watchHandle = watchUtils && watchUtils.whenTrue(view, "stationary", function() {
+      if (isLandscapeViewOnEvent(view.zoom, zoomLevelTrigger, isLandscapeMode)) {
+        onZoomChange({ query: { ...query, landscapeView: true }})
+        landscapeModeRef.current = true;
+      } else if (isLandscapeViewOffEvent(view.zoom, zoomLevelTrigger, isLandscapeMode)) {
+        onZoomChange({ query: { ...query, landscapeView: false }})
+        landscapeModeRef.current = false;
+      }
+    })
+    return function cleanUp() {
+      watchHandle && watchHandle.remove()
+    }
+  }, [watchUtils, isLandscapeMode, query])
+
+  // CAMERA change effect
+  useEffect(() => {
+    const tilt = isLandscapeMode ? 45 : 0;
     const target = { tilt };
     const options = {
       speedFactor: 0.5,
       duration: 1000
     }
     view.goTo(target, options)
-
-    if (isLandscapeMode) {
-      const { layers } = map;
-  layers.items.forEach(mapLayer => {
-    if (mapLayer.type === 'feature') {
-      view.whenLayerView(mapLayer).then(function(layerView) {
-        console.log('LAYER VIEW',layerView)
-        layerView.watch('updating', function(value) {
-          console.log('updating', value)
-          if (!value) {
-            // wait for the layer view to finish updating
-console.log(view.extent)
-            // query all the features available for drawing.
-            layerView
-              .queryFeatures({
-                geometry: view.extent,
-                spatialRelationship: 'intersects'
-              }).then(function(results) {
-                console.log('RESULTS',results)
-              })
-          }
-        })
-      })
-    }
-  })
-    }
-
-    return function cleanUp() {
-      interactionWatcher.remove();
-      updateWatcher.remove();
-      zoomWatcher.remove();
-    }
   }, [isLandscapeMode])
 
-  
-  if (!isInteracting && !isUpdating) {
-    onZoomChange({ query: { ...query, landscapeView: landscapeView }})
-  }
+  // GRID data and painting effect
+  useEffect(() => {
+    let watchHandle;
+    loadModules(["esri/core/watchUtils", "esri/Graphic", "esri/geometry/geometryEngineAsync"]).then(([watchUtils, Graphic, geometryEngineAsync]) => {
+      const { layers } = map;
+      const gridLayer = layers.items.find(l => l.title === "Birds Rarity");
+        view.whenLayerView(gridLayer).then(function(layerView) {
+          watchHandle = watchUtils.whenTrue(view, "stationary", function() {
+            if (landscapeModeRef.current) {
+              const { extent } = view;
+              setViewExtent(extent);
+              layerView.queryFeatures({
+                geometry: extent,
+                spatialRelationship: 'intersects'
+              }).then(function(results) {
+                const containedGridCells = results.features.filter(gridCell => extent.contains(gridCell.geometry.extent)).map(gc => gc.geometry)
+                console.log('NUMBER OF GRID CELLS PAINTED', containedGridCells.length)
+                containedGridCells && geometryEngineAsync.union(containedGridCells).then(result => {
+                  removeGridCell(view, gridCellRef.current);
+                  // Create a symbol for rendering the graphic
+                  const gridCellSymbol = setGridCellStyles(0.1, 0.9, 2, [24, 186, 180]);
+                  // Create the graphic
+                  const gridCellGraphic = setGridCellGraphic(Graphic, result, gridCellSymbol);
+                  // Store a reference to the gridCell polygon created
+                  gridCellRef.current = gridCellGraphic;
+                  // Add it to the view
+                  paintGridCell(view, gridCellGraphic);
+              }).catch((err) => console.error(err));
+            }).catch((err) => console.error(err));
+           } else {
+            // Remove any created gridCell polygon when not in landscape view
+            removeGridCell(view, gridCellRef.current);
+           }
+          })
+        }).catch((err) => console.error(err));
+    }).catch((err) => console.error(err));
+    return function cleanUp() {
+      watchHandle && watchHandle.remove();
+    }
+  }, [landscapeModeRef, viewExtent])
+
   return null
 }
 
