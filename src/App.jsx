f16eda1793aabe94fc3cd750800853f6f0dba8d6
Javi Abia <weberjavi@gmail.com>
2019-05-06 16:39:38 +0200

    Create app layout and add it to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { hot } from 'react-hot-loader'
-// import DataGlobe from 'components/globe-data-view';
-// import dataGlobeViewConfig from './sceneConfigs/dataGlobeViewConfig';
-import featuredGlobeViewConfig from './sceneConfigs/featuredGlobeViewConfig';
-import FeaturedGlobe from 'components/globe-featured-view';
+import { hot } from 'react-hot-loader';
+import AppLayout from 'layouts/app-layout';
 
 import { Icons as VizzIcons } from 'vizzuality-components';
 
@@ -11,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FeaturedGlobe sceneConfig={featuredGlobeViewConfig} />
+        <AppLayout />
         <VizzIcons />
       </div>
     );
---
+++ src/layouts/app-layout/app-layout-component.jsx
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react';
+import Proptypes from 'prop-types';
+import universal from 'react-universal-component';
+
+const universalConfig = {
+  loading: <div>Loading</div>
+};
+
+const PageComponent = universal(
+  ({ page } /* webpackChunkName: "[request]" */) => import(`../../pages/${page}/index.js`),
+  universalConfig
+);
+
+class App extends PureComponent {
+  render() {
+    const { route } = this.props;
+    const { page } = route;
+    return page ? <PageComponent page={page} /> : null;
+  }
+}
+
+App.propTypes = {
+  route: Proptypes.object.isRequired
+};
+
+export default App;
