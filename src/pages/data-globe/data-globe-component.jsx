3475de87d3f3328d1ff15804aaccbb0d267151d4
Javi Abia <weberjavi@gmail.com>
2019-05-08 10:08:47 +0200

    Move logic to container

--- src/pages/data-globe/data-globe-component.jsx
+++ src/pages/data-globe/data-globe-component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { layerManagerToggle } from 'utils/layer-manager-utils';
 import Globe from 'components/globe';
 import ArcgisLayerManager from 'components/shared/arcgis-layer-manager';
 import LandscapeViewManager from 'components/shared/landscape-view-manager';
@@ -8,22 +7,19 @@ import sceneSettings from './data-globe-settings.js';
 
 const { REACT_APP_DATA_GLOBE_SCENE_ID: SCENE_ID } = process.env;
 
-const DataGlobeComponent = ({ activeLayers, isLandscapeMode, sceneLayers, setDataGlobeSettings }) => {
-  const toggleLayer = e => layerManagerToggle(e, "data-layer-id", activeLayers, setDataGlobeSettings);
-  return (
-    <Globe sceneId={SCENE_ID} sceneSettings={sceneSettings} >
-      <ArcgisLayerManager activeLayers={activeLayers}/>
-      <LandscapeViewManager zoomLevelTrigger={10} onZoomChange={setDataGlobeSettings} isLandscapeMode={isLandscapeMode}/>
-      <Sidebar>
-        {
-          sceneLayers &&
-          sceneLayers.map(l => (
-              <button key={l.id} data-layer-id={l.id} onClick={toggleLayer}>{l.title}</button>
-          ))
-        }
-      </Sidebar>
-    </Globe>
-  );
-}
+const DataGlobeComponent = ({ sceneLayers, activeLayers, isLandscapeMode, handleZoomChange, handleLayerToggle }) => (
+  <Globe sceneId={SCENE_ID} sceneSettings={sceneSettings} >
+    <ArcgisLayerManager activeLayers={activeLayers}/>
+    <LandscapeViewManager zoomLevelTrigger={10} onZoomChange={handleZoomChange} isLandscapeMode={isLandscapeMode}/>
+    <Sidebar>
+      {
+        sceneLayers &&
+        sceneLayers.map(l => (
+            <button key={l.id} data-layer-id={l.id} onClick={handleLayerToggle}>{l.title}</button>
+        ))
+      }
+    </Sidebar>
+  </Globe>
+);
 
 export default DataGlobeComponent;
--- src/pages/featured-globe/featured-globe-component.jsx
+++ src/pages/featured-globe/featured-globe-component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { layerManagerToggle } from 'utils/layer-manager-utils';
 import Globe from 'components/globe';
 import ArcgisLayerManager from 'components/shared/arcgis-layer-manager';
 import LandscapeViewManager from 'components/shared/landscape-view-manager';
@@ -15,28 +14,23 @@ import ZoomWidget from 'components/shared/widgets/zoom-widget';
 
 const { REACT_APP_FEATURED_GLOBE_SCENE_ID: SCENE_ID } = process.env;
 
-const FeaturedGlobeComponent = ({ setFeaturedGlobeSettings, sceneLayers, activeLayers, isLandscapeMode }) => {
-  const toggleLayer = e => layerManagerToggle(e, "data-layer-id", activeLayers, setFeaturedGlobeSettings);
-  return (
-    <>
-      <Globe sceneId={SCENE_ID} sceneSettings={sceneSettings} >
-        <ArcgisLayerManager activeLayers={activeLayers}/>
-        <LandscapeViewManager zoomLevelTrigger={10} onZoomChange={setFeaturedGlobeSettings} isLandscapeMode={isLandscapeMode}/>
-        <LocationWidget />
-        <ZoomWidget />
-        <NavigationToggleWidget />
-      </Globe>
-      <Sidebar>
-        {
-          sceneLayers &&
-          sceneLayers.map(l => (
-              <button key={l.id} data-layer-id={l.id} onClick={toggleLayer}>{l.title}</button>
-          ))
-        }
-      </Sidebar>
-      <Legend />
-    </>
-  );
-}
+const FeaturedGlobeComponent = ({ sceneLayers, activeLayers, isLandscapeMode, handleLayerToggle, handleZoomChange }) => (
+  <Globe sceneId={SCENE_ID} sceneSettings={sceneSettings} >
+    <ArcgisLayerManager activeLayers={activeLayers}/>
+    <LandscapeViewManager zoomLevelTrigger={10} onZoomChange={handleZoomChange} isLandscapeMode={isLandscapeMode}/>
+    <LocationWidget />
+    <ZoomWidget />
+    <NavigationToggleWidget />
+    <Sidebar>
+      {
+        sceneLayers &&
+        sceneLayers.map(l => (
+            <button key={l.id} data-layer-id={l.id} onClick={handleLayerToggle}>{l.title}</button>
+        ))
+      }
+    </Sidebar>
+    <Legend />
+  </Globe>
+);
 
 export default FeaturedGlobeComponent;
