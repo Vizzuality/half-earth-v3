bbaf2897279063e14cb2db5cabb80350c5785269
kfilip <kamil.filip94@gmail.com>
2019-05-14 10:42:39 +0100

    Add theme styles to Location widget

--- src/components/widgets/location-widget/location-widget-component.jsx
+++ src/components/widgets/location-widget/location-widget-component.jsx
@@ -1,18 +1,30 @@
 // Docs for Locate ui widget
 // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Locate.html
 import { loadModules } from '@esri/react-arcgis';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { ReactComponent as LocationIcon } from 'icons/location.svg';
+
+import styles from './location-widget.module.scss';
+
+const LocationComponent = ({ locationWidget }) => (
+  <button className={styles.locationButton} onClick={() => locationWidget.locate()}>
+    <LocationIcon />
+  </button>
+);
 
 const LocationWidgetComponent = ({ view }) => {
   const [locationWidget, setLocationWidget] = useState(null);
 
   useEffect(() => {
-    loadModules(["esri/widgets/Locate"]).then(([Locate]) => {
-      const locationWidget = new Locate({
+    loadModules(["esri/widgets/Locate/LocateViewModel"]).then(([LocateView]) => {
+      const locationWidget = new LocateView({
         view: view
       });
       setLocationWidget(locationWidget);
-      view.ui.add(locationWidget, "top-right");
+      const node = document.createElement("div");
+      view.ui.add(node, "top-right");
+      ReactDOM.render(<LocationComponent locationWidget={locationWidget} />, node);
 
     }).catch((err) => console.error(err));
     return function cleanup() {
