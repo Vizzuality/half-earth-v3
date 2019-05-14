850d5fdf2222c79e9a65cc5751ce3e5ded950e2e
kfilip <kamil.filip94@gmail.com>
2019-05-14 17:06:19 +0100

    Implement locate method

--- src/components/widgets/location-widget/location-widget-component.jsx
+++ src/components/widgets/location-widget/location-widget-component.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as LocationIcon } from 'icons/location.svg';
+import { ReactComponent as HideUiIcon } from 'icons/hideUI.svg';
 
 import styles from './location-widget.module.scss';
 
-const LocationWidgetComponent = ({ locationWidget }) => (
-  <button className={styles.locationButton} onClick={() => locationWidget.locate()}>
-    <LocationIcon />
-  </button>
-);
+const LocationWidgetComponent = ({ locationWidget }) => {
+  const [isAnimated, setAnimated] = useState(false);
+  
+  const locate = () => {
+    if(!isAnimated) {
+      setAnimated(true)
+      locationWidget.locate()
+        .then(() => setAnimated(false))
+        .catch(() => setAnimated(false));
+    } else {
+      locationWidget.cancelLocate()
+      setAnimated(false);
+    }
+  };
+
+  return (
+    <button className={styles.locationButton} onClick={locate}>
+      {isAnimated ? <HideUiIcon /> : <LocationIcon />}
+    </button>
+  );
+};
 
 export default LocationWidgetComponent;
