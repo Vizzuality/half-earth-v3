c892be800c7f13c36a3e56a67d81c1536ef398ac
kfilip <kamil.filip94@gmail.com>
2019-05-14 09:39:24 +0100

    Set theme for zoom widget

--- src/components/widgets/zoom-widget/zoom-widget-component.jsx
+++ src/components/widgets/zoom-widget/zoom-widget-component.jsx
@@ -1,16 +1,36 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { loadModules } from '@esri/react-arcgis';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { ReactComponent as ZoomInIcon } from 'icons/zoomIn.svg';
+import { ReactComponent as ZoomOutIcon } from 'icons/zoomOut.svg';
+
+import styles from './zoom-widget.module.scss';
+
+const ZoomComponent = ({ zoomWidget }) => (
+  <div className={styles.zoomComponent}>
+    <button className={styles.zoomButton} onClick={() => zoomWidget.zoomIn()}>
+      <ZoomInIcon />
+    </button>
+    <span className={styles.spacer} />
+    <button className={styles.zoomButton} onClick={() => zoomWidget.zoomOut()}>
+      <ZoomOutIcon />
+    </button>
+  </div>
+);
 
 const ZoomWidgetComponent = ({ view }) => {
   const [zoomWidget, setZoomWidget] = useState(null);
 
   useEffect(() => {
-    loadModules(["esri/widgets/Zoom"]).then(([Zoom]) => {
-      const zoomWidget = new Zoom({
+    loadModules(["esri/widgets/Zoom/ZoomViewModel"]).then(([ZoomView]) => {
+      const zoomWidget = new ZoomView({
         view: view
       });
       setZoomWidget(zoomWidget);
-      view.ui.add(zoomWidget, "top-left");
+      const node = document.createElement("div");
+      view.ui.add(node, "top-right");
+      ReactDOM.render(<ZoomComponent zoomWidget={zoomWidget} />, node);
 
     }).catch((err) => console.error(err));
     return function cleanup() {
