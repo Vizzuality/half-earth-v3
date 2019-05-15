481c0fddacad73b45ec4f7e680290a5c6546f548
Javi Abia <weberjavi@gmail.com>
2019-05-15 09:57:27 +0200

    Update landscape manager WIP

--- src/components/landscape-view-manager/landscape-view-manager-component.jsx
+++ src/components/landscape-view-manager/landscape-view-manager-component.jsx
@@ -14,7 +14,8 @@ const LandscapeViewManager = ({ view, map, zoomLevelTrigger, onZoomChange, query
   const [watchUtils, setWatchUtils] = useState(null);
   const [viewExtent, setViewExtent] = useState();
   // References for cleaning up graphics
-  const gridCellRef = useRef();
+  const gridCellsGraphicRef = useRef();
+  const gridCellsLayerRef = useRef();
   const landscapeModeRef = useRef()
 
 
@@ -24,6 +25,42 @@ const LandscapeViewManager = ({ view, map, zoomLevelTrigger, onZoomChange, query
       setWatchUtils(watchUtils);
     })
   }, []);
+
+  // Create graphics layer to store grid cells graphics
+  useEffect(() => {
+    if (isLandscapeMode) {
+      const {colorRGB, outlineOpacity, outlineWidth} = gridCellDefaultStyles;
+      loadModules(["esri/Graphic", "esri/layers/GraphicsLayer"]).then(([Graphic, GraphicsLayer]) => {
+        const graphic= new Graphic({
+          symbol: {
+            type: "polygon-3d",
+            symbolLayers: [
+              {
+                type: "fill",
+                material: { color: [0, 255, 255, 0] },
+                outline: {
+                  color: [...colorRGB, outlineOpacity],
+                  size: outlineWidth
+                }
+              }
+            ]
+          },
+          geometry: gridCellsGraphicRef.current || null
+        });
+        gridCellsLayerRef.current = new GraphicsLayer({
+          title: "Selection Layer",
+          graphics: [graphic]
+        });
+        view.map.add(gridCellsLayerRef.current);
+      })
+    } else {
+      view.map.remove(gridCellsLayerRef.current);
+    }
+    return function cleanUp() {
+      view.map.remove(gridCellsLayerRef.current);
+      gridCellsGraphicRef.current = null;
+    }
+  }, [isLandscapeMode, gridCellsGraphicRef.current]);
 
   // Update url param
   useEffect(() => {
@@ -55,36 +92,55 @@ const LandscapeViewManager = ({ view, map, zoomLevelTrigger, onZoomChange, query
   // GRID data and painting effect
   useEffect(() => {
     let watchHandle;
-    loadModules(["esri/core/watchUtils", "esri/Graphic", "esri/geometry/geometryEngineAsync"]).then(([watchUtils, Graphic, geometryEngineAsync]) => {
+    let queryHandle;
+    let selectionGraphic;
+    let selectionLayer;
+    loadModules(["esri/core/watchUtils", "esri/Graphic", "esri/layers/GraphicsLayer", "esri/geometry/geometryEngine"]).then(([watchUtils, Graphic, GraphicsLayer, geometryEngine]) => {
       const { layers } = map;
-      const gridLayer = layers.items.find(l => l.title === "Birds Rarity");
+      const gridLayer = layers.items.find(l => l.title === "rarity-richness-GRID");
+      gridLayer.outFields = ["*"];
         view.whenLayerView(gridLayer).then(function(layerView) {
+                  
           watchHandle = watchUtils.whenTrue(view, "stationary", function() {
             if (landscapeModeRef.current) {
               const { extent } = view;
+              const scaledDownExtent = extent.clone().expand(0.9);
               setViewExtent(extent);
-              layerView.queryFeatures({
-                geometry: extent,
+              queryHandle && (!queryHandle.isFulfilled()) && queryHandle.cancel();
+              queryHandle = layerView.queryFeatures({
+                geometry: scaledDownExtent,
                 spatialRelationship: 'intersects'
               }).then(function(results) {
-                const containedGridCells = results.features.filter(gridCell => extent.contains(gridCell.geometry.extent)).map(gc => gc.geometry)
-                containedGridCells && geometryEngineAsync.union(containedGridCells).then(result => {
-                  // Remove current grid cell before painting a new one
-                  removeGridCell(view, gridCellRef.current);
-                  // Create a symbol for rendering the graphic
-                  const { fillOpacity, outlineOpacity, outlineWidth, colorRGB} = gridCellDefaultStyles;
-                  const gridCellSymbol = setGridCellStyles(fillOpacity, outlineOpacity, outlineWidth, colorRGB);
-                  // Create the graphic
-                  const gridCellGraphic = setGridCellGraphic(Graphic, result, gridCellSymbol);
-                  // Store a reference to the gridCell polygon created
-                  gridCellRef.current = gridCellGraphic;
-                  // Add it to the view
-                  paintGridCell(view, gridCellGraphic);
-              }).catch((err) => console.error(err));
+                const containedGridCells = results.features.filter(gridCell => scaledDownExtent.contains(gridCell.geometry.extent));
+                // const containedData = containedGridCells.reduce((acc, current) => {
+                //   return {
+                //     ...acc,
+                //     [current.attributes.CELL_ID]: {
+                //       CELL_DATA: {
+                //         RAINFED: current.attributes.RAINFED,
+                //         URBAN: current.attributes.URBAN,
+                //         AGRICULTUR: current.attributes.AGRICULTUR,
+                //         Shape__Area: current.attributes.Shape__Area,
+                //         PROP_LAND: current.attributes.PROP_LAND,
+                //         ISISLAND: current.attributes.ISISLAND
+                //       },
+                //       ...acc[current.attributes.CELL_ID],
+                //       [current.attributes.TAXA]: {
+                //         AVE_RSR_PC: current.attributes.AVE_RSR_PC,
+                //         SR_PC: current.attributes.SR_PC,
+                //         FOCAL_SPP: current.attributes.FOCAL_SPP
+                //       }
+                //     }
+                //   }
+                // }, {})
+                // console.log(containedData)
+                const containedGridCellsGeometries = containedGridCells && containedGridCells.map(gc => gc.geometry.extent)
+                gridCellsGraphicRef.current = geometryEngine.union(containedGridCellsGeometries);
             }).catch((err) => console.error(err));
            } else {
             // Remove any created gridCell polygon when not in landscape view
-            removeGridCell(view, gridCellRef.current);
+            // selectionGraphic.geometry = null;
+            // removeGridCell(view, gridCellRef.current);
            }
           })
         }).catch((err) => console.error(err));
