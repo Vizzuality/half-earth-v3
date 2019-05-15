b133e891d1078a1562f4c151c33c8b40467e5d21
Javi Abia <weberjavi@gmail.com>
2019-05-15 17:38:12 +0200

    Connect landscape manager to use grid-cell actions

--- src/components/landscape-view-manager/landscape-view-manager.js
+++ src/components/landscape-view-manager/landscape-view-manager.js
@@ -1,4 +1,8 @@
+import { connect } from 'react-redux';
 import Component from './landscape-view-manager-component';
+// This import forces the registering of the module
+import 'redux_modules/grid-cell-data';
+import * as actions from 'redux_modules/grid-cell-data/grid-cell-data-actions';
 
-export default Component;
+export default connect(null, actions)(Component);
 
