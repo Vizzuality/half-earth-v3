5b47069fd20e516def777644b4d9f0087e6c230a
Javi Abia <weberjavi@gmail.com>
2019-05-21 14:29:56 +0200

    Delete obsolete code

--- config-overrides.js
+++ config-overrides.js
@@ -21,8 +21,7 @@ module.exports = function override(config, env) {
       constants: path.resolve(__dirname, 'src/constants'),
       redux_modules: path.resolve(__dirname, 'src/redux-modules'),
       reducerRegistry: path.resolve(__dirname, 'src/reducerRegistry'),
-      sceneConfigs: path.resolve(__dirname, 'src/sceneConfigs'),
-      services: path.resolve(__dirname, 'src/services')
+      sceneConfigs: path.resolve(__dirname, 'src/sceneConfigs')
     }
   }
   config = rewireReactHotLoader(config, env);
