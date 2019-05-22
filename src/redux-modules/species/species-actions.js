83afa3d1d8192e6241cc61f525c72035b5c6dec7
Javi Abia <weberjavi@gmail.com>
2019-05-22 14:57:46 +0200

    Add loading again :)

--- src/redux-modules/species/species-actions.js
+++ src/redux-modules/species/species-actions.js
@@ -3,3 +3,10 @@ import { createAction } from 'redux-tools';
 export const SPECIES_FETCH_DATA_READY = createAction(
   'SPECIES_FETCH_DATA_READY'
 );
+
+export const SPECIES_FETCH_DATA_LOADING = createAction(
+  'SPECIES_FETCH_DATA_LOADING'
+);
+export const SPECIES_FETCH_DATA_ERROR = createAction(
+  'SPECIES_FETCH_DATA_ERROR'
+);
--- src/redux-modules/species/species-reducers.js
+++ src/redux-modules/species/species-reducers.js
@@ -1,10 +1,13 @@
 import * as actions from './species-actions';
 import iucnList from 'constants/iucn-list';
 
-export const initialState = { data: null };
+export const initialState = { loading: false, error: false, data: null };
+
+function fetchSpeciesLoading(state) {
+  return { ...state, loading: true };
+}
 
 function fetchSpeciesReady(state, { payload }) {
-  console.log(payload)
   const speciesData = payload
   .filter(speciesObject => speciesObject.data.length )
   .map(speciesObject => ({ ...speciesObject.data[0], iucn: iucnList[speciesObject.data[0].redlist] }));
@@ -12,6 +15,12 @@ function fetchSpeciesReady(state, { payload }) {
   return { ...state, data: speciesData };
 }
 
+function fetchSpeciesError(state, { payload }) {
+  return { ...state, loading: false, error: payload };
+}
+
 export default {
-  [actions.SPECIES_FETCH_DATA_READY]: fetchSpeciesReady
+  [actions.SPECIES_FETCH_DATA_LOADING]: fetchSpeciesLoading,
+  [actions.SPECIES_FETCH_DATA_READY]: fetchSpeciesReady,
+  [actions.SPECIES_FETCH_DATA_ERROR]: fetchSpeciesError
 };
