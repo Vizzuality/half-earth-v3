3fb16e609473542c682cd691da5a69d0a12eb2f7
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-05-24 10:10:42 +0100

    Style radio buttons

--- src/components/radio-group/radio-group-component.jsx
+++ src/components/radio-group/radio-group-component.jsx
@@ -6,17 +6,26 @@ import styles from './radio-group-styles.module.scss';
 const RadioGroup = ({ options }) => {
   const [selectedOption, setSelectedOption] = useState(options[0]);
 
+  const isSelected = (option) => selectedOption.value === option.value;
+
   return (
-    <form className={styles.radioGroup}>
+    <>
       {options.map(option => (
         <div key={option.value} className={styles.radioOption}>
-          <label>
-            <input type="radio" name="singleOption" value={option.value} checked={selectedOption.value === option.value} onChange={() => setSelectedOption(option)} />
+          <input 
+            type="radio"
+            name="singleOption"
+            id={option.value}
+            value={option.value}
+            defaultChecked={isSelected(option)}
+            onChange={() => setSelectedOption(option)} 
+          />
+          <label htmlFor={option.value} className={styles.radioInput}>
             {option.value}
           </label>
         </div>
       ))}
-    </form>
+    </>
   )
 }
 
