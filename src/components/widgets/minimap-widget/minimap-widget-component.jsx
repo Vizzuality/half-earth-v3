7ef9eb357c783b253cc0048cd9c29362d47a2483
kfilip <kamil.filip94@gmail.com>
2019-05-28 19:14:17 +0200

    Bind data globe with minimap globe

--- src/components/widgets/minimap-widget/minimap-widget-component.jsx
+++ src/components/widgets/minimap-widget/minimap-widget-component.jsx
@@ -63,7 +63,7 @@ const MinimapWidgetComponent = ({ view }) => {
       <div className={styles.globalWrapper}>
       <div className={styles.halfEarthMeterWrapper}>
         <GlobeComponent sceneId={SCENE_ID} sceneSettings={settings}>
-          <MiniComponent center={center}/>
+          <MiniComponent center={center} globeView={view}/>
         </GlobeComponent>
         {/* <div id='blueLeftCircleGlobal' className={styles.blueLeftCircle}></div>
         <div className={styles.blueRightCircle}></div>
@@ -82,19 +82,121 @@ const MinimapWidgetComponent = ({ view }) => {
 };
 
 
-const MiniComponent = ({ view, map, center }) => {
+const MiniComponent = ({ view: minimapView, map, center, globeView }) => {
   useEffect(() => {
-    disableInteractions(view);
+    disableInteractions(minimapView);
     loadModules(["esri/layers/VectorTileLayer"]).then(([VectorTileLayer]) => {
       const minimapLayer = new VectorTileLayer(minimapLayerStyles);
       map.add(minimapLayer);
     });
   }, []);
 
+  // useEffect(() => {
+  //   const target = { center, zoom: 0 };
+  //   minimapView.goTo(target);
+  // }, [center]);
+
   useEffect(() => {
-    const target = { center, zoom: 0 };
-    view.goTo(target);
-  }, [center])
+    loadModules(["esri/core/watchUtils"]).then(([watchUtils]) => {
+      var synchronizeView = function(view, others) {
+        others = Array.isArray(others) ? others : [others];
+    
+        var viewpointWatchHandle;
+        var viewStationaryHandle;
+        var otherInteractHandlers;
+        var scheduleId;
+    
+        var clear = function() {
+          if (otherInteractHandlers) {
+            otherInteractHandlers.forEach(function(handle) {
+              handle.remove();
+            });
+          }
+          viewpointWatchHandle && viewpointWatchHandle.remove();
+          viewStationaryHandle && viewStationaryHandle.remove();
+          scheduleId && clearTimeout(scheduleId);
+          otherInteractHandlers = viewpointWatchHandle = viewStationaryHandle = scheduleId = null;
+        };
+    
+        var interactWatcher = view.watch("interacting,animation", function(
+          newValue
+        ) {
+          if (!newValue) {
+            return;
+          }
+          if (viewpointWatchHandle || scheduleId) {
+            return;
+          }
+    
+          // start updating the other views at the next frame
+          scheduleId = setTimeout(function() {
+            scheduleId = null;
+            viewpointWatchHandle = view.watch("viewpoint", function(
+              newValue
+            ) {
+              others.forEach(function(otherView) {
+                otherView.viewpoint = newValue;
+              });
+            });
+          }, 0);
+    
+          // stop as soon as another view starts interacting, like if the user starts panning
+          otherInteractHandlers = others.map(function(otherView) {
+            return watchUtils.watch(
+              otherView,
+              "interacting,animation",
+              function(value) {
+                if (value) {
+                  clear();
+                }
+              }
+            );
+          });
+    
+          // or stop when the view is stationary again
+          viewStationaryHandle = watchUtils.whenTrue(
+            view,
+            "stationary",
+            clear
+          );
+        });
+    
+        return {
+          remove: function() {
+            this.remove = function() {};
+            clear();
+            interactWatcher.remove();
+          }
+        };
+      };
+    
+      /**
+       * utility method that synchronizes the viewpoints of multiple views
+       */
+      var synchronizeViews = function(views) {
+        var handles = views.map(function(view, idx, views) {
+          var others = views.concat();
+          others.splice(idx, 1);
+          return synchronizeView(view, others);
+        });
+    
+        return {
+          remove: function() {
+            this.remove = function() {};
+            handles.forEach(function(h) {
+              h.remove();
+            });
+            handles = null;
+          }
+        };
+      };
+    
+      // bind the views
+      synchronizeViews([globeView, minimapView]);
+    });
+  }, []);
+
+ 
 
   return null;
 }
