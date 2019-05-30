cd6999db53fc3adfe9bca7872660fce29d59d235
kfilip <kamil.filip94@gmail.com>
2019-05-30 15:30:51 +0200

    Add Map Instructions section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,11 +1,41 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import { ReactComponent as CloseIcon } from 'icons/x.svg';
 
 import styles from './about-styles.module.scss';
+import MapInstructionsComponent from './map-instructions/map-instructions-component';
 
 const About = () => {
+  const [isAboutPageOpened, setAboutPageOpened] = useState(false);
+
+  const handleOpenAboutPage = () => {
+    setAboutPageOpened(true);
+  };
+
+  const handleCloseAboutPage = () => {
+    setAboutPageOpened(false);
+  };
+
   return (
-    <button className={styles.aboutButton}>About the Half-Earth map</button>
+    <>
+      <button
+        className={styles.aboutButton}
+        onClick={handleOpenAboutPage}
+      >
+        About the Half-Earth map
+      </button>
+      {isAboutPageOpened && 
+        <div className={styles.aboutPage}>
+          <MapInstructionsComponent />
+          <button 
+            className={styles.closeButton}
+            onClick={handleCloseAboutPage}
+          >
+            <CloseIcon />
+          </button>
+          ABOUT
+        </div>
+      }
+    </>
   )
 }
 
