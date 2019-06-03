55e0c3713c3d499f24aed64b8daa493247842f12
Javi Abia <weberjavi@gmail.com>
2019-06-03 12:58:01 +0200

    Move layer creation to biodiversity container

--- src/components/biodiversity-layers/biodiversity-layers.js
+++ src/components/biodiversity-layers/biodiversity-layers.js
@@ -1,3 +1,31 @@
+import React from 'react';
+import { loadModules } from '@esri/react-arcgis';
+import { layersConfig } from 'constants/mol-layers-configs';
 import Component from './biodiversity-layers-component';
 
-export default Component;
+const BiodiversityLayerContainer = props => {
+
+  const handleLayerUpdate = option => {
+    const { map, handleLayerToggle } = props;
+    const selectedLayer = layersConfig.find(l => l.title === option);
+    const layerExists = map.layers.items.some(l => l.id === selectedLayer.slug);
+    if (layerExists) {
+      handleLayerToggle(selectedLayer.slug)
+    } else {
+      loadModules(["esri/layers/WebTileLayer"]).then(([WebTileLayer]) => {
+        const tileLayer = new WebTileLayer({
+          urlTemplate: selectedLayer.url,
+          title: selectedLayer.title,
+          id: selectedLayer.slug,
+          opacity: 0.5,
+          maxScale: 2800000
+        })
+        map.add(tileLayer)
+        handleLayerToggle(selectedLayer.slug)
+      });
+    }
+  }
+  return <Component handleLayerUpdate={handleLayerUpdate} {...props}/>
+}
+
+export default BiodiversityLayerContainer;
--- src/pages/data-globe/data-globe.js
+++ src/pages/data-globe/data-globe.js
@@ -12,9 +12,9 @@ const actions = { ...ownActions, ...biodiversityActions };
 
 const RICHNESS_RARITY_GRID = 'rarity-richness-GRID';
 // const HUMAN_IMPACT_LAYER = 'Human Impact';
-const TAXA_FIELD = 'TAXA';
+// const TAXA_FIELD = 'TAXA';
 
-const handleMapLoad = (map, view, setSpecies, setSpeciesLoading, setSpeciesError) => {
+const handleMapLoad = (map, view) => {
   const { layers } = map;
   const gridLayer = layers.items.find(l => l.title === RICHNESS_RARITY_GRID);
   // set the outFields for the rarity-richness-GRID layer
@@ -24,7 +24,7 @@ const handleMapLoad = (map, view, setSpecies, setSpeciesLoading, setSpeciesError
   // const humanImpactLayer = layers.items.find(l => l.title === HUMAN_IMPACT_LAYER);
 
   loadModules(
-    ["esri/renderers/smartMapping/statistics/uniqueValues", "esri/layers/support/MosaicRule", "esri/layers/WebTileLayer"]).then(([uniqueValues, MosaicRule, WebTileLayer]) => {
+    ["esri/layers/support/MosaicRule"]).then(([MosaicRule]) => {
 
       // TODO: MosaicRule allows to select rasters using the "where" clause. Figure out mosaic method and operation
       // humanImpactLayer.mosaicRule = new MosaicRule({
@@ -32,31 +32,21 @@ const handleMapLoad = (map, view, setSpecies, setSpeciesLoading, setSpeciesError
       //   where: `Name = 'human_impact_urban' OR Name = 'human_impact_all'`
       // });
 
-      var tiledLayer2 = new WebTileLayer({
-        urlTemplate: "https://storage.googleapis.com/cdn.mol.org/half-earth/tiles/phase2/fishing-hours/gfw_purple/all/{level}/{col}/{row}",
-        title: 'All marine fishing types',
-        opacity: 0.5
-       });
-
-       map.add(tiledLayer2)
-       console.log(map)
-
-      setSpeciesLoading();
-      uniqueValues({ layer: gridLayer, field: TAXA_FIELD}).then((result) => {
-        setSpecies(result.uniqueValueInfos);
-      }).catch((err) => {
-        setSpeciesError(err);
-      })
-  }).catch((err) => { console.error(err); setSpeciesError(err) });
+      // setSpeciesLoading();
+      // uniqueValues({ layer: gridLayer, field: TAXA_FIELD}).then((result) => {
+      //   setSpecies(result.uniqueValueInfos);
+      // }).catch((err) => {
+      //   setSpeciesError(err);
+      // })
+  }).catch((err) => { console.error(err)});
 }
 const dataGlobeContainer = props => {
   const toggleLayer = layerId => layerManagerToggle(layerId, props.activeLayers, props.setDataGlobeSettings);
   const handleZoomChange = props.setDataGlobeSettings;
-  const { setSpecies, setSpeciesLoading, setSpeciesError } = props;
   
-  return <Component 
+  return <Component
     handleLayerToggle={toggleLayer}
-    onLoad={(map, view) => handleMapLoad(map, view, setSpecies, setSpeciesLoading, setSpeciesError)}
+    onLoad={(map, view) => handleMapLoad(map, view)}
     handleZoomChange={handleZoomChange}
     {...props}/>
 }
