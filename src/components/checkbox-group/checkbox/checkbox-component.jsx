b9cee4f947b46e2401167708ba7511b05817a1c3
Tomasz Subik <tsubik@gmail.com>
2019-06-10 18:17:55 +0200

    fix human pressures layers checkboxes

--- src/components/checkbox-group/checkbox/checkbox-component.jsx
+++ src/components/checkbox-group/checkbox/checkbox-component.jsx
@@ -9,16 +9,16 @@ import styles from './checkbox-styles.module.scss';
 const Checkbox = ({ option, onChange, checked, theme, handleInfoClick }) => {
   return (
     <div key={option.name} className={cx(
-      styles.checkboxWrapper, 
+      styles.checkboxWrapper,
       { [styles.checkboxWrapperSelected]: checked, [theme]: checked }
     )}>
-      <input 
+      <input
         type="checkbox"
         value={option.value}
         name={option.name}
         id={option.value}
-        defaultChecked={checked}
-        onChange={(e) => onChange(e, option)} 
+        checked={checked}
+        onChange={(e) => onChange(e, option)}
       />
       <label htmlFor={option.value} className={cx(styles.checkbox, { [styles.checkboxSelected]: checked })}>
         <span className={styles.label}>{option.name}</span>
