1c54ad2f7a9a76bed1d457be10ae10dd73d2f608
Javi Abia <weberjavi@gmail.com>
2019-06-13 17:46:01 +0200

    more WIP

--- src/components/grid-layer/grid-layer-component.jsx
+++ src/components/grid-layer/grid-layer-component.jsx
@@ -15,9 +15,19 @@ const GridLayer = ({map, view, setGridCellData, fetchGeoDescription}) => {
 
   const [viewExtent, setViewExtent] = useState();
   const [containedGridCells, setContainedGridCells] = useState(null);
+  // const [geometryEngineModule, setGeometryEngineModule] = useState(null);
+  const [multipleGridCell, setMultipleGridCell] = useState(null);
   // References for cleaning up graphics
   const gridCellRef = useRef();
   const gridCellGeometryRef = useRef();
+  const gridCellGraphicRef = useRef();
+
+  // //
+  // useEffect(()=> {
+  //   loadModules(["esri/geometry/geometryEngine"]).then(([geometryEngine]) => {
+  //     setGeometryEngineModule(geometryEngine)
+  //   })
+  // }, [])
 
   // set the view extent when view stationary
   useEffect(() => {
@@ -49,7 +59,15 @@ const GridLayer = ({map, view, setGridCellData, fetchGeoDescription}) => {
               spatialRelationship: 'intersects'
             }).then(function(results) {
               const containedGridCells = results.features.filter(gridCell => scaledDownExtent.contains(gridCell.geometry.extent));
-              setContainedGridCells(containedGridCells);
+              const hasContainedGridCells = containedGridCells.length > 0;
+              // If there are not a group of cells pick the one in the center
+              const singleGridCell = !hasContainedGridCells && results.features.filter(gridCell => gridCell.geometry.contains(view.center));
+              const gridCells = hasContainedGridCells ? containedGridCells : singleGridCell;
+              if (!gridCellRef.current || !isEqual(gridCellRef.current[0], gridCells)) {
+                setMultipleGridCell(hasContainedGridCells)
+                setContainedGridCells(gridCells);
+              }
+              gridCellRef.current = gridCells
             })
           }
         })
@@ -61,87 +79,53 @@ const GridLayer = ({map, view, setGridCellData, fetchGeoDescription}) => {
   }, [viewExtent]);
   
   useEffect(() => {
-    let watchHandle;
-    let queryHandle;
-    let layerViewHandle;
     loadModules(
       [
-        "esri/core/watchUtils",
         "esri/Graphic",
-        "esri/layers/GraphicsLayer",
         "esri/geometry/geometryEngine",
+        "esri/layers/GraphicsLayer",
         "esri/geometry/support/webMercatorUtils"
-      ]).then(([watchUtils, Graphic, GraphicsLayer, geometryEngine, webMercatorUtils]) => {
-        const { layers } = map;
-      const gridLayer = layers.items.find(l => l.id === BIODIVERSITY_FACETS_LAYER);
-      view.whenLayerView(gridLayer).then(function(layerView) {
-        watchHandle = watchUtils.whenTrue(view, "stationary", function() {
-          if (containedGridCells) {
-            const { extent } = view;
-            const scaledDownExtent = extent.clone().expand(0.9);
-            queryHandle && (!queryHandle.isFulfilled()) && queryHandle.cancel();
-            queryHandle = layerView.queryFeatures({
-              geometry: extent,
-              spatialRelationship: 'intersects'
-            }).then(function(results) {
-              const containedGridCells = results.features.filter(gridCell => scaledDownExtent.contains(gridCell.geometry.extent));
-              const hasContainedGridCells = containedGridCells.length > 0;
-              const containedGridCellsGeometries = hasContainedGridCells && containedGridCells.map(gc => gc.geometry);
-              // If there are not a group of cells pick the one in the center
-              const singleGridCell = !hasContainedGridCells && results.features.filter(gridCell => gridCell.geometry.contains(view.center));
-              const gridCellGeometry = hasContainedGridCells ? geometryEngine.union(containedGridCellsGeometries) : singleGridCell[0].geometry;
-              const isDifferentGridCell = gridCellGeometryRef.current && isEqual(gridCellGeometryRef.current.rings, gridCellGeometry.rings) === false
-
-              if (!gridCellGeometryRef.current || isDifferentGridCell) {
-              
-                
-                //// AQUI ESTAMOS CREANDO EL GEOJSON Y PILLANDO LA GEODESCRIPTION (solamente deberiamos hacerlo si la geometria ha cambiado)
-                const geoGeometry = webMercatorUtils.webMercatorToGeographic(gridCellGeometry);
-                const geoJSON = esriGeometryToGeojson(geoGeometry);
-                fetchGeoDescription(geoJSON);
-                /////////////////////////////////////////////////////////////////
-
-
-                /////// AQUI PILLAMOS TODAS LAS CELDAS  Y LAS ANHADIMOS AL STORE (solamente deberiamos hacerlo si la geometria ha cambiado)
-                const cellData = hasContainedGridCells ? containedGridCells : singleGridCell;
-                // Add data to the store
-                setGridCellData(cellData);
-                /////////////////////////////////////
-
-
-                //////////////// AQUI BORRAMOS, PINTAMOS Y GUARDAMOS REFERENCIA (solamente deberiamos hacerlo si la geometria ha cambiado)
-                // Remove current grid cell before painting a new one
-                gridCellRef.current && removeGridCell(view, gridCellRef.current);
-                // Create a symbol for rendering the graphic
-                const { fillOpacity, outlineOpacity, outlineWidth, colorRGB } = gridCellDefaultStyles;
-                const gridCellSymbol = setGridCellStyles(fillOpacity, outlineOpacity, outlineWidth, colorRGB);
-                // Create the graphic
-                const gridCellGraphic = setGridCellGraphic(Graphic, gridCellGeometry, gridCellSymbol);
-                // Store a reference to the gridCell polygon created
-                gridCellRef.current = gridCellGraphic;
-                // Add it to the view
+      ]).then(([Graphic, geometryEngine, GraphicsLayer, webMercatorUtils]) => {
+        const gridCellGeometry = multipleGridCell ? geometryEngine.union(containedGridCells.map(gc => gc.geometry)) : containedGridCells[0].geometry;
+          //// AQUI ESTAMOS CREANDO EL GEOJSON Y PILLANDO LA GEODESCRIPTION (solamente deberiamos hacerlo si la geometria ha cambiado)
+          const geoGeometry = webMercatorUtils.webMercatorToGeographic(gridCellGeometry);
+          const geoJSON = esriGeometryToGeojson(geoGeometry);
+          fetchGeoDescription(geoJSON);
+          /////////////////////////////////////////////////////////////////
+          
+          
+          /////// AQUI PILLAMOS TODAS LAS CELDAS  Y LAS ANHADIMOS AL STORE (solamente deberiamos hacerlo si la geometria ha cambiado)
+          // const cellData = hasContainedGridCells ? containedGridCells : singleGridCell;
+          // Add data to the store
+          setGridCellData(containedGridCells);
+          /////////////////////////////////////
+          
+          
+          //////////////// AQUI BORRAMOS, PINTAMOS Y GUARDAMOS REFERENCIA (solamente deberiamos hacerlo si la geometria ha cambiado)
+          // Remove current grid cell before painting a new one
+          console.log('REMOVING', gridCellGraphicRef.current)
+          gridCellGraphicRef.current && removeGridCell(view, gridCellGraphicRef.current);
+          // Create a symbol for rendering the graphic
+          const { fillOpacity, outlineOpacity, outlineWidth, colorRGB } = gridCellDefaultStyles;
+          const gridCellSymbol = setGridCellStyles(fillOpacity, outlineOpacity, outlineWidth, colorRGB);
+          // Create the graphic
+          const gridCellGraphic = setGridCellGraphic(Graphic, gridCellGeometry, gridCellSymbol);
+          // Store a reference to the gridCell polygon created
+          gridCellGraphicRef.current = gridCellGraphic;
+          // Add it to the view
+          console.log('PAINTING', gridCellGraphic)
                 paintGridCell(view, gridCellGraphic);
-                //////////////////////////////////////////////////////
-              }
-              gridCellGeometryRef.current = gridCellGeometry
-
-            
-
-          }).catch((err) => console.error(err));
-        }
-        })
-      }).catch((err) => console.error(err));
     }).catch((err) => console.error(err));
-    return function cleanUp() {
-      watchHandle && watchHandle.remove();
-      layerViewHandle && layerViewHandle.remove();
-    }
-  }, [viewExtent, containedGridCells])
+    // return function cleanUp() {
+    //   console.log('CLEANING UP', gridCellGraphicRef.current)
+    //   removeGridCell(view, gridCellGraphicRef.current);
+    // }
+  }, [containedGridCells])
 
   useEffect(() => {
     return function cleanUp() {
       // remove geometry before unmounting
-      removeGridCell(view, gridCellRef.current);
+      removeGridCell(view, gridCellGraphicRef.current);
     }
   },[])
 
