42ad72f0c3fe909f8392505cf9a0279355fdc168
Javi Abia <weberjavi@gmail.com>
2019-06-20 16:29:39 +0200

    Move handlers to container

--- src/components/legend/legend.js
+++ src/components/legend/legend.js
@@ -1,6 +1,38 @@
-
+import React from 'react';
 import { connect } from 'react-redux';
 import Component from './legend-component';
+import actions from 'redux_modules/metadata';
 import mapStateToProps from './legend-selectors';
 
-export default connect(mapStateToProps, null)(Component);
+const LegendContainer = props => {
+
+  const handleChangeOpacity = (layer, opacity) => {
+    const { setLayerOpacity } = props;
+    setLayerOpacity(layer.id, opacity);
+  }
+
+  const handleRemoveLayer = (layer) => {
+    const { setLayerVisibility } = props;
+    setLayerVisibility(layer.id, false)
+  }
+
+  const handleInfoClick = layer => {
+    const { setModalMetadata } = props;
+    setModalMetadata({
+      slug: `${layer.id}`,
+      title: `${layer.legendConfig.title} metadata`,
+      isOpen: true
+    });
+  };
+
+  return (
+    <Component
+      handleRemoveLayer={handleRemoveLayer}
+      handleChangeOpacity={handleChangeOpacity}
+      handleInfoClick={handleInfoClick}
+      {...props}
+    />
+  )
+}
+
+export default connect(mapStateToProps, actions)(LegendContainer);
