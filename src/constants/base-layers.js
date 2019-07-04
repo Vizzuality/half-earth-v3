fd9e5e184bee4d428bc36cd4c47f28575b26b78c
Javi Abia <weberjavi@gmail.com>
2019-07-04 15:24:03 +0200

    Updated layers ids

--- src/constants/base-layers.js
+++ src/constants/base-layers.js
@@ -1,3 +1,3 @@
-export const FIREFLY_LAYER = '16b4fc6cb63-layer-1';
-export const FEATURES_LABELS_LAYER = '16b74159ed8-layer-0';
+export const FIREFLY_LAYER = '16bbc6e8f62-layer-0';
+export const FEATURES_LABELS_LAYER = '16bbc738e1d-layer-4';
 export const GRID_LAYER = 'Grid layer';
