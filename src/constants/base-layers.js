b76032e1dfd665ee609830549e78cd2588269d58
Javi Abia <weberjavi@gmail.com>
2019-07-05 11:13:23 +0200

    Update layer id

--- src/constants/base-layers.js
+++ src/constants/base-layers.js
@@ -1,3 +1,3 @@
 export const FIREFLY_LAYER = '16bbc6e8f62-layer-0';
-export const FEATURES_LABELS_LAYER = '16bbc738e1d-layer-4';
+export const FEATURES_LABELS_LAYER = '16bc158ba9a-layer-0';
 export const GRID_LAYER = 'Grid layer';
