eb10ea065c4d43ff0b4ebfbe1fb48c1585290f7e
Javi Abia <weberjavi@gmail.com>
2019-07-08 10:45:39 +0200

    Add styles config on separate file

--- src/components/labels-layer/labels-layer-component.jsx
+++ src/components/labels-layer/labels-layer-component.jsx
@@ -1,22 +1,26 @@
 import { loadModules } from '@esri/react-arcgis';
 import { FEATURES_LABELS_LAYER } from 'constants/base-layers';
+import { stylesConfig } from './labels-layer-styles-config';
 
-const labelClassFactory = (LabelClassConstructor, feature, color, minScale, maxScale) => {
+const labelClassFactory = (LabelClassConstructor, styleGroup) => {
+  const config = stylesConfig[styleGroup];
   return new LabelClassConstructor({
-    labelExpressionInfo: {
-      expression: "$feature.name" // Text for labels comes from COUNTY field
-    },
-    // where: `featurecla = ${feature} AND featurecla = `,
     labelPlacement: 'center-center',
-    // minScale: "$feature.max_scale",
-    // maxScale: "$feature.min_scale",
+    labelExpressionInfo: {
+      expression: "$feature.name"
+    },
+    where: `style = ${styleGroup}`,
     symbol: {
       type: "label-3d",
       symbolLayers: [{
         type: "text",
-        material: { color: 'black' },
-        size: 8, // points
-        halo: { color: [...color, 0.4], size: '1px' }
+        font: {
+          family: config.fontFamily,
+          style: config.fontStyle,
+          size: config.fontSize
+        },
+        material: { color: config.color }
+        //halo: { color: [...color, 0.4], size: '1px' }
       }]
     }
   });
@@ -27,17 +31,19 @@ const LabelsLayerComponent = ({ map }) => {
   loadModules(["esri/layers/support/LabelClass"])
     .then(([LabelClass]) => {
 
-      var mountains = labelClassFactory(LabelClass, 'Range/mtn', [49,163,84], 4500000, 1000);
-      // var basins = labelClassFactory(LabelClass, 'Basin', [239, 189, 136], 3500000, 1000);
-      // var deserts = labelClassFactory(LabelClass, 'Desert', [239, 189, 136], 3500000, 1000);
-      // var geoArea = labelClassFactory(LabelClass, 'Geoarea', [239, 189, 136], 2500000, 1000);
-      // var islands = labelClassFactory(LabelClass, 'Island', [239, 189, 136], 10000000, 1000);
-      // var archipielago = labelClassFactory(LabelClass, 'Continent', [239, 189, 136], 130000000, 10000000);
+      var style_1_ter = labelClassFactory(LabelClass, 'style_1_ter');
+      var style_2_ter = labelClassFactory(LabelClass, 'style_2_ter');
+      var style_3_ter = labelClassFactory(LabelClass, 'style_3_ter');
+      var style_4_ter = labelClassFactory(LabelClass, 'style_4_ter');
+      var style_1_aq = labelClassFactory(LabelClass, 'style_1_aq');
+      var style_2_aq = labelClassFactory(LabelClass, 'style_2_aq');
+      var style_3_aq = labelClassFactory(LabelClass, 'style_3_aq');
+      var style_4_aq = labelClassFactory(LabelClass, 'style_4_aq');
 
       // Add labels to the feature layer
       labelsLayer.opacity = 0;
       labelsLayer.labelsVisible = true;
-      labelsLayer.labelingInfo = [mountains];
+      labelsLayer.labelingInfo = [style_1_ter, style_2_ter, style_3_ter, style_4_ter, style_1_aq, style_2_aq, style_3_aq, style_4_aq];
     })
   
   return null
---
+++ src/components/labels-layer/labels-layer-styles-config.js
@@ -0,0 +1,58 @@
+export const stylesConfig = {
+  style_1_ter: {
+    allCaps: true,
+    fontFamily: "PierSans-Bold",
+    fontStyle: "italic",
+    fontSize: "18px",
+    color: [255, 255, 255]
+  },
+  style_2_ter: {
+    allCaps: false,
+    fontFamily: "PierSans-Bold",
+    fontStyle: "italic",
+    fontSize: "18px",
+    color: [255, 255, 255]
+  },
+  style_3_ter: {
+    allCaps: true,
+    fontFamily: "PierSans-Bold",
+    fontStyle: "italic",
+    fontSize: "10px",
+    color: [255, 255, 255]
+  },
+  style_4_ter: {
+    allCaps: false,
+    fontFamily: "PierSans-Bold",
+    fontStyle: "italic",
+    fontSize: "12px",
+    color: [255, 255, 255]
+  },
+  style_1_aq: {
+    allCaps: true,
+    fontFamily: "PierSans-Bold",
+    fontStyle: "normal",
+    fontSize: "18px",
+    color: [174, 234, 255]
+  },
+  style_2_aq: {
+    allCaps: false,
+    fontFamily: "PierSans",
+    fontStyle: "normal",
+    fontSize: "18px",
+    color: [174, 234, 255]
+  },
+  style_3_aq: {
+    allCaps: false,
+    fontFamily: "PierSans",
+    fontStyle: "italic",
+    fontSize: "16px",
+    color: [174, 234, 255]
+  },
+  style_4_aq: {
+    allCaps: false,
+    fontFamily: "PierSans",
+    fontStyle: "italic",
+    fontSize: "12px",
+    color: [174, 234, 255]
+  }
+}
