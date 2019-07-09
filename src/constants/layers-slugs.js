e493f1f87566bcbaeded38920478dedeff412854
Javi Abia <weberjavi@gmail.com>
2019-07-09 13:54:09 +0200

    Update layer groups with slugs

--- src/constants/layers-slugs.js
+++ src/constants/layers-slugs.js
@@ -7,6 +7,8 @@ export const COMMUNITY_AREAS_FEATURE_LAYER = 'community_areas_feature_layer';
 export const COMMUNITY_AREAS_VECTOR_TILE_LAYER = 'community_areas_vector_tile_layer';
 // Biodiversity facets layer. Disolved layer created by John Grayson based on MoL biodiversity facets.
 export const BIODIVERSITY_FACETS_LAYER = 'biodiversity_facets';
+// Dinamically created graphics layer to paint aggregated gridcells
+export const GRID_LAYER = 'grid_layer';
 // Terrestrial human pressures imagery layer (it is the aggregation of rainfed, irrigated and urban pressures).
 export const LAND_HUMAN_PRESSURES_IMAGE_LAYER = 'land_human_pressures';
 // Label layer to display features labels (marine, terrestrial and urban).
@@ -14,4 +16,4 @@ export const LAND_HUMAN_PRESSURES_IMAGE_LAYER = 'land_human_pressures';
 export const LABELS_LAYER_GROUP = 'labels_layer_group';
 export const LANDSCAPE_LABELS_LAYER = 'landscape_labels_layer';
 // Basemap (added as a layer to display satellite imagery on higher zoom levels)
-export const FIREFLY_BASEMAP = 'firefly_basemap';
+export const FIREFLY_BASEMAP_LAYER = 'firefly_basemap';
