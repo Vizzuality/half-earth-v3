feb00723545269027c3e05f2792acd6811296e8c
Javi Abia <weberjavi@gmail.com>
2019-07-09 13:45:01 +0200

    Create layers slugs constants file

---
+++ src/constants/layers-slugs.js
@@ -0,0 +1,17 @@
+// Protected areas layers. Based on WDPA and RAISIG data.
+export const PROTECTED_AREAS_LAYER_GROUP = 'protected_areas_layer_group';
+export const PROTECTED_AREAS_FEATURE_LAYER = 'protected_areas_feature_layer';
+export const PROTECTED_AREAS_VECTOR_TILE_LAYER = 'protected_areas_vector_tile_layer';
+export const COMMUNITY_AREAS_LAYER_GROUP = 'community_areas_layer_group';
+export const COMMUNITY_AREAS_FEATURE_LAYER = 'community_areas_feature_layer';
+export const COMMUNITY_AREAS_VECTOR_TILE_LAYER = 'community_areas_vector_tile_layer';
+// Biodiversity facets layer. Disolved layer created by John Grayson based on MoL biodiversity facets.
+export const BIODIVERSITY_FACETS_LAYER = 'biodiversity_facets';
+// Terrestrial human pressures imagery layer (it is the aggregation of rainfed, irrigated and urban pressures).
+export const LAND_HUMAN_PRESSURES_IMAGE_LAYER = 'land_human_pressures';
+// Label layer to display features labels (marine, terrestrial and urban).
+// The scale in which they are displayed is set directly on the layer on ArcGIS online item.
+export const LABELS_LAYER_GROUP = 'labels_layer_group';
+export const LANDSCAPE_LABELS_LAYER = 'landscape_labels_layer';
+// Basemap (added as a layer to display satellite imagery on higher zoom levels)
+export const FIREFLY_BASEMAP = 'firefly_basemap';
