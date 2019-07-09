845b52505e2ac74b5d3b55ded4f992e9d1a7e2c4
Javi Abia <weberjavi@gmail.com>
2019-07-09 16:40:32 +0200

    minor fixes

--- src/pages/data-globe/data-globe-initial-state.js
+++ src/pages/data-globe/data-globe-initial-state.js
@@ -1,6 +1,6 @@
 import {
   BIODIVERSITY_FACETS_LAYER,
-  LANDSCAPE_LABELS_LAYER,
+  LABELS_LAYER_GROUP,
   FIREFLY_BASEMAP_LAYER,
   GRID_LAYER
 } from 'constants/layers-slugs';
@@ -10,7 +10,7 @@ export default {
     activeLayers: [
       { title: BIODIVERSITY_FACETS_LAYER }, // Biodiversity Facets (new grid)
       { title: GRID_LAYER }, // This is the layer used to paint aggregated grid cells
-      { title: LANDSCAPE_LABELS_LAYER }, // This is the layer used to paint aggregated grid cells
+      { title: LABELS_LAYER_GROUP }, // This is the layer used to paint aggregated grid cells
       { title: FIREFLY_BASEMAP_LAYER } // half-earth firefly
     ],
     rasters: {},
