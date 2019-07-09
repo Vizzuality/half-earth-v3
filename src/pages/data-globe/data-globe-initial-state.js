59040166960e2e2908ebba77b39bea6301d4ce84
Javi Abia <weberjavi@gmail.com>
2019-07-09 14:02:49 +0200

    Update data globe initial state with slugs

--- src/pages/data-globe/data-globe-initial-state.js
+++ src/pages/data-globe/data-globe-initial-state.js
@@ -1,13 +1,17 @@
-import { BIODIVERSITY_FACETS_LAYER } from 'constants/biodiversity';
-import { FIREFLY_LAYER, FEATURES_LABELS_LAYER, GRID_LAYER } from 'constants/base-layers';
+import {
+  BIODIVERSITY_FACETS_LAYER,
+  LANDSCAPE_LABELS_LAYER,
+  FIREFLY_BASEMAP_LAYER,
+  GRID_LAYER
+} from 'constants/layers-slugs';
 
 export default {
   globe: {
     activeLayers: [
-      { id: BIODIVERSITY_FACETS_LAYER }, // Biodiversity Facets (new grid)
-      { id: GRID_LAYER }, // This is the layer used to paint aggregated grid cells
-      { id: FEATURES_LABELS_LAYER }, // This is the layer used to paint aggregated grid cells
-      { id: FIREFLY_LAYER } // half-earth firefly
+      { title: BIODIVERSITY_FACETS_LAYER }, // Biodiversity Facets (new grid)
+      { title: GRID_LAYER }, // This is the layer used to paint aggregated grid cells
+      { title: LANDSCAPE_LABELS_LAYER }, // This is the layer used to paint aggregated grid cells
+      { title: FIREFLY_BASEMAP_LAYER } // half-earth firefly
     ],
     rasters: {},
     zoom: 3,
