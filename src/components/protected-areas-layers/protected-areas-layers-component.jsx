818344b0ecd03f2d314af992ec075f52799d89a5
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-07-29 18:10:12 +0100

    Move painting layers to custom hook

--- src/components/protected-areas-layers/protected-areas-layers-component.jsx
+++ src/components/protected-areas-layers/protected-areas-layers-component.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import MultipleActiveLayers from 'components/multiple-active-layers';
 
+import { usePaintLayer } from 'hooks/esri';
 import { handleLayerRendered } from 'utils/layer-manager-utils';
 import { WDPALayers, PROTECTED_AREAS_COLOR, COMMUNITY_AREAS_COLOR } from 'constants/protected-areas';
 import { PROTECTED_AREAS_VECTOR_TILE_LAYER, PROTECTED_AREAS_LAYER_GROUP, COMMUNITY_AREAS_LAYER_GROUP, COMMUNITY_AREAS_VECTOR_TILE_LAYER } from 'constants/layers-slugs';
@@ -11,32 +12,15 @@ import styles from './protected-areas-layers-styles.module';
 
 const ProtectedAreasLayers = ({ handleGlobeUpdating, handleLayerToggle, activeLayers, map, view, addLayerAnalyticsEvent, removeLayerAnalyticsEvent }) => {
   const { layers } = map;
+  
+  // Paint areas on different than default colors
+  const protectedGroupLayer = layers.items.find(l => l.title === PROTECTED_AREAS_LAYER_GROUP);
+  const communityGroupLayer = layers.items.find(l => l.title === COMMUNITY_AREAS_LAYER_GROUP);
+  const ProtectedVTLLayer = protectedGroupLayer.layers.items.find(l => l.title === PROTECTED_AREAS_VECTOR_TILE_LAYER);
+  const CommunityVTLLayer = communityGroupLayer.layers.items.find(l => l.title === COMMUNITY_AREAS_VECTOR_TILE_LAYER);
 
-  // Paint Protected Areas on a different that default color
-  useEffect(() => {
-    const groupLayer = layers.items.find(l => l.title === PROTECTED_AREAS_LAYER_GROUP);
-    const VTLLayer = groupLayer.layers.items.find(l => l.title === PROTECTED_AREAS_VECTOR_TILE_LAYER);
-
-    const paintProperties = VTLLayer.getPaintProperties('WDPA_poly_Latest');
-
-    paintProperties['fill-color'] = PROTECTED_AREAS_COLOR;
-    paintProperties['fill-outline-color'] = PROTECTED_AREAS_COLOR;
-
-    VTLLayer.setPaintProperties('WDPA_poly_Latest', paintProperties);
-  }, [])
-
-  // Paint Community Areas on a different that default color
-  useEffect(() => {
-    const groupLayer = layers.items.find(l => l.title === COMMUNITY_AREAS_LAYER_GROUP);
-    const VTLLayer = groupLayer.layers.items.find(l => l.title === COMMUNITY_AREAS_VECTOR_TILE_LAYER);
-
-    const paintProperties = VTLLayer.getPaintProperties('WDPA_poly_Latest/1');
-
-    paintProperties['fill-color'] = COMMUNITY_AREAS_COLOR;
-    paintProperties['fill-outline-color'] = COMMUNITY_AREAS_COLOR;
-
-    VTLLayer.setPaintProperties('WDPA_poly_Latest/1', paintProperties);
-  }, [])
+  usePaintLayer(ProtectedVTLLayer, 'WDPA_poly_Latest', PROTECTED_AREAS_COLOR);
+  usePaintLayer(CommunityVTLLayer, 'WDPA_poly_Latest/1', COMMUNITY_AREAS_COLOR);
 
   const toggleLayer = (layersPassed, option) => {
     const layerNotRendered = !activeLayers.some(layer => layer.title === option.id);
