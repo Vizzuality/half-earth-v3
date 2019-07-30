e627bb99c05f3431877a64c1453502c2bf539e14
Kamil Filip <kamil.filip94@gmail.com>
2019-07-30 11:03:10 +0100

    Tweak geo-describer back button perfomance

--- src/components/landscape-sidebar/geo-description-widget/geo-description-widget.js
+++ src/components/landscape-sidebar/geo-description-widget/geo-description-widget.js
@@ -9,8 +9,17 @@ import 'redux_modules/geo-description';
 const GeoDescriptionWidget = props => {
   const handleBackClick = () => {
     const { view } = props;
-    const target = { center: view.center, zoom: 7 }
-    view.goTo(target);
+    const { center } = view;
+    const target = {
+      position: {
+        x: center.longitude,
+        y: center.latitude,
+        z: 1000000
+      },
+      heading: 0,
+      tilt: 0
+    }
+    view.goTo(target)
   };
 
   return <Component handleBackClick={handleBackClick} {...props}/>
