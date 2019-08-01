6eac88cafce2b645e2cb3c977f4f1b2de7abe975
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-08-01 17:09:36 +0100

    Remove globe thunks that are not used anymore

--- src/router.js
+++ src/router.js
@@ -1,9 +1,6 @@
 import { connectRoutes, NOT_FOUND, redirect } from 'redux-first-router';
 import { decodeUrlForState, encodeStateForUrl } from 'utils/stateToUrl';
 
-const lazyLoadDataGlobe = (dispatch, getState) => import('redux_modules/data-globe/data-globe-thunks').then(module => module.default(dispatch, getState));
-const lazyLoadFeaturedGlobe = (dispatch, getState) => import('redux_modules/featured-globe/featured-globe-thunks').then(module => module.default(dispatch, getState));
-
 export const DATA = 'location/DATA';
 export const FEATURED = 'location/FEATURED';
 export const MAP_IFRAME = 'location/MAP_IFRAME';
@@ -12,23 +9,19 @@ export const MAP_IFRAME = 'location/MAP_IFRAME';
 export const routes = {
   [DATA]: {
     path: '/v2',
-    page: 'experiences',
-    thunk: lazyLoadDataGlobe
+    page: 'experiences'
   },
   [DATA]: {
     path: '/dataGlobe',
-    page: 'experiences',
-    thunk: lazyLoadDataGlobe
+    page: 'experiences'
   },
   [FEATURED]: {
     path: '/featuredGlobe',
-    page: 'experiences',
-    thunk: lazyLoadFeaturedGlobe
+    page: 'experiences'
   },
   [MAP_IFRAME]: {
     path: '/map',
-    page: 'map-iframe',
-    thunk: lazyLoadDataGlobe
+    page: 'map-iframe'
   },
   [NOT_FOUND]: { path: '/404', thunk: dispatch => dispatch(redirect({ type: DATA })) }
 };
