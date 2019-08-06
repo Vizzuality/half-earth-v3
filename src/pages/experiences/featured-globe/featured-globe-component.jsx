ac91cacd40331b7a2dcb1ec0cb9326041c731da9
Javi Abia <weberjavi@gmail.com>
2019-08-06 07:22:58 +0200

    Add ui elements to featured map

--- src/pages/experiences/featured-globe/featured-globe-component.jsx
+++ src/pages/experiences/featured-globe/featured-globe-component.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import loadable from '@loadable/component'
+import { ZOOM_LEVEL_TRIGGER } from 'constants/landscape-view-constants';
 import Globe from 'components/globe';
+import LandscapeViewManager from 'components/landscape-view-manager';
 import Switcher from 'components/switcher';
+import About from 'components/about';
+
+// WIDGETS
+import LocationWidget from 'components/widgets/location-widget';
+import ZoomWidget from 'components/widgets/zoom-widget';
+import ToggleUiWidget from 'components/widgets/toggle-ui-widget';
+import SearchWidget from 'components/widgets/search-widget';
+import MinimapWidget from 'components/widgets/minimap-widget';
+
+const InfoModal = loadable(() => import('components/modal-metadata'));
 
 const { REACT_APP_FEATURED_GLOBE_SCENE_ID: SCENE_ID } = process.env;
 
-const FeaturedGlobeComponent = ({ sceneSettings, handleSwitch, onLoad }) => {
+const FeaturedGlobeComponent = ({ hasMetadata, sceneSettings, handleSwitch, onLoad, isFullscreenActive, handleZoomChange, isLandscapeMode }) => {
   return (
-    <Globe sceneId={SCENE_ID} sceneSettings={sceneSettings} onLoad={onLoad}>
-      <Switcher handleClick={handleSwitch} />
-    </Globe>
+    <>
+      <Globe sceneId={SCENE_ID} sceneSettings={sceneSettings} onLoad={onLoad}>
+        <LandscapeViewManager zoomLevelTrigger={ZOOM_LEVEL_TRIGGER} onZoomChange={handleZoomChange} isLandscapeMode={isLandscapeMode} />
+        <LocationWidget />
+        <ToggleUiWidget isFullscreenActive={isFullscreenActive} />
+        <ZoomWidget />
+        <MinimapWidget />
+        <SearchWidget />
+        <Switcher handleClick={handleSwitch} />
+      </Globe>
+      {hasMetadata && <InfoModal />}
+      <About />
+    </>
   )
 };
 
