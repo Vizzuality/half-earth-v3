8dad17369fbc3d1c367de60ac7bdb463fbf017f3
Javi Abia <weberjavi@gmail.com>
2019-08-08 12:43:56 +0200

    Use redux data for cards content

--- src/components/featured-map-card/featured-map-card-component.jsx
+++ src/components/featured-map-card/featured-map-card-component.jsx
@@ -5,7 +5,8 @@ import ShareModalButton from 'components/share-modal';
 import styles from './featured-map-card-styles.module.scss'
 import { ReactComponent as ChevronIcon } from 'icons/arrow_right.svg';
 
-const FeaturesMapCardComponent = ({ view, className, isSidebarOpen, isLandscapeMode, isFullscreenActive }) => {
+const FeaturesMapCardComponent = ({ view, className, isSidebarOpen, isLandscapeMode, isFullscreenActive, featuredMap }) => {
+  console.log(featuredMap)
   const slide = useSpring({
     from: { marginLeft: -400 },
     marginLeft: isSidebarOpen && !isLandscapeMode && !isFullscreenActive ? 0 : -400,
@@ -15,13 +16,13 @@ const FeaturesMapCardComponent = ({ view, className, isSidebarOpen, isLandscapeM
     <animated.div className={cx(className, styles.cardContainer)} style={slide}>
       <section
         className={styles.titleSection}
-        style={ {backgroundImage: `url('https://www.explore-atacama.com/fotos/guias/full/llama-2-1346707018.jpg')`}}
+        style={ {backgroundImage: `url(${featuredMap.image})`}}
       >
         <ShareModalButton theme={{ shareButton: styles.shareButton}} view={view} />
-        <h2 className={styles.title}>E.O. Wilson’s best places in the Biosphere</h2>
+        <h2 className={styles.title}>{featuredMap.title}</h2>
       </section>
       <section className={styles.descriptionSection}>
-        <p className={styles.description}>Extracted from the Half-Earth book, these are the personal choices of E. O. Wilson. These places stand out because of their richness, uniqueness or need of research and protection. </p>
+        <p className={styles.description}>{featuredMap.description}</p>
         <button className={styles.allMapsButton}>
           <span className={styles.buttonText}>All maps</span>
           <ChevronIcon className={styles.arrowIcon}/>
--- src/components/featured-map-card/featured-map-card.js
+++ src/components/featured-map-card/featured-map-card.js
@@ -1,3 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
 import Component from './featured-map-card-component';
+import mapStateToProps from './featured-map-card-selectors';
 
-export default Component;
+const FeaturedMapCardContainer = props => {
+  const { featuredMaps, selectedFeaturedMap } = props;
+  const featuredMap = featuredMaps && featuredMaps.find(map => map.slug === selectedFeaturedMap)
+  return (
+    <Component
+      featuredMap={featuredMap}
+      {...props }
+    />
+  )
+}
+
+export default connect(mapStateToProps, null)(FeaturedMapCardContainer);
