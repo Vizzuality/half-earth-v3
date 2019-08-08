696a568267376d14d434f49d2c85f0d1ee0f257c
Javi Abia <weberjavi@gmail.com>
2019-08-08 14:22:13 +0200

    Dispatch url ui action to select map s list

--- src/components/featured-map-card/featured-map-card-component.jsx
+++ src/components/featured-map-card/featured-map-card-component.jsx
@@ -5,12 +5,20 @@ import ShareModalButton from 'components/share-modal';
 import styles from './featured-map-card-styles.module.scss'
 import { ReactComponent as ChevronIcon } from 'icons/arrow_right.svg';
 
-const FeaturesMapCardComponent = ({ view, className, isSidebarOpen, isLandscapeMode, isFullscreenActive, featuredMap }) => {
-  console.log(featuredMap)
+const FeaturesMapCardComponent = ({ 
+  view,
+  className,
+  selectedSidebar,
+  isLandscapeMode,
+  isFullscreenActive,
+  featuredMap,
+  handleAllMapsClick
+}) => {
+  const isOpen = selectedSidebar === 'featuredMapCard';
   const slide = useSpring({
     from: { marginLeft: -400 },
-    marginLeft: isSidebarOpen && !isLandscapeMode && !isFullscreenActive ? 0 : -400,
-    delay: isSidebarOpen && !isLandscapeMode && !isFullscreenActive ? 400 : 0
+    marginLeft: isOpen && !isLandscapeMode && !isFullscreenActive ? 0 : -400,
+    delay: isOpen && !isLandscapeMode && !isFullscreenActive ? 400 : 0
   })
   return (
     <animated.div className={cx(className, styles.cardContainer)} style={slide}>
@@ -23,7 +31,10 @@ const FeaturesMapCardComponent = ({ view, className, isSidebarOpen, isLandscapeM
       </section>
       <section className={styles.descriptionSection}>
         <p className={styles.description}>{featuredMap.description}</p>
-        <button className={styles.allMapsButton}>
+        <button
+          className={styles.allMapsButton}
+          onClick={handleAllMapsClick}
+        >
           <span className={styles.buttonText}>All maps</span>
           <ChevronIcon className={styles.arrowIcon}/>
         </button>
--- src/components/featured-map-card/featured-map-card.js
+++ src/components/featured-map-card/featured-map-card.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Component from './featured-map-card-component';
 import mapStateToProps from './featured-map-card-selectors';
+import * as actions from 'actions/url-actions';
+
 
 const FeaturedMapCardContainer = props => {
   const { featuredMaps, selectedFeaturedMap } = props;
-  const featuredMap = featuredMaps && featuredMaps.find(map => map.slug === selectedFeaturedMap)
+  const featuredMap = featuredMaps && featuredMaps.find(map => map.slug === selectedFeaturedMap);
+  const handleAllMapsClick = () => props.changeUI({ selectedSidebar: 'featuredMapsList' });
   return (
     <Component
       featuredMap={featuredMap}
+      handleAllMapsClick={handleAllMapsClick}
       {...props }
     />
   )
 }
 
-export default connect(mapStateToProps, null)(FeaturedMapCardContainer);
+export default connect(mapStateToProps, actions)(FeaturedMapCardContainer);
