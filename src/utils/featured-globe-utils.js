aabcd37018171a949e2f90a1dd00c7e4f25f0cbe
Javi Abia <weberjavi@gmail.com>
2019-08-19 13:29:05 +0200

    Update place only if a point is clicked

--- src/utils/featured-globe-utils.js
+++ src/utils/featured-globe-utils.js
@@ -4,6 +4,6 @@ export const setSelectedFeaturedPlace = (event, featuredPlacesLayer, view, chang
   view.hitTest(event).then(function(response) {
     const featuredPoint = response.results.filter((result) => result.graphic.layer === featuredPlacesLayer);
     const selectedFeaturedPlace = featuredPoint.length ? featuredPoint[0].graphic.attributes.nam_slg : null;
-    changeUI({ selectedFeaturedPlace });
+    if (selectedFeaturedPlace) changeUI({ selectedFeaturedPlace });
   });
 }
