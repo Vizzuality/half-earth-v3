d7053c27c0c54d351b38b37ac27f51e297778ed6
Javi Abia <weberjavi@gmail.com>
2019-08-20 16:29:15 +0200

    Create onload function on featured globe

--- src/pages/featured-globe/featured-globe.js
+++ src/pages/featured-globe/featured-globe.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { loadModules } from '@esri/react-arcgis';
+import { setRasterFuntion, mosaicRuleFix } from 'utils/raster-layers-utils';
 import { layerManagerToggle } from 'utils/layer-manager-utils';
+import { LAND_HUMAN_PRESSURES_IMAGE_LAYER } from 'constants/layers-slugs';
+import { HUMAN_PRESSURES_COLOR_RAMP } from 'constants/human-pressures';
 import { DATA } from 'router';
 
 import { createAction } from 'redux-tools';
@@ -13,7 +17,22 @@ import featuredMapsActions from 'redux_modules/selected-featured-map';
 const handleSwitch = createAction(DATA);
 
 const actions = { ...ownActions, ...featuredMapsActions, handleSwitch}
+const handleMapLoad = map => {
+  const { layers } = map;
+  // This fix has been added as a workaround to a bug introduced on v4.12
+  // The bug was causing the where clause of the mosaic rule to not work
+  // It will be probably fixed on v4.13
+  const humanImpactLayer = layers.items.find(l => l.title === LAND_HUMAN_PRESSURES_IMAGE_LAYER);
+  loadModules(["esri/config"]).then(([esriConfig]) => {
+    mosaicRuleFix(esriConfig, humanImpactLayer)
+  })
 
+  // Update default human impact layer color ramp
+  loadModules(["esri/layers/support/RasterFunction", "esri/Color"]).then(([RasterFunction, Color]) => {
+    humanImpactLayer.noData = 0;
+    humanImpactLayer.renderingRule = setRasterFuntion(RasterFunction, Color, HUMAN_PRESSURES_COLOR_RAMP);
+  })
+}
 const feturedGlobeContainer = props => {
 
   useEffect(() => {
@@ -24,7 +43,11 @@ const feturedGlobeContainer = props => {
   const toggleLayer = layerId => layerManagerToggle(layerId, props.activeLayers, props.setFeaturedGlobeSettings);
   const handleZoomChange = props.setFeaturedGlobeSettings;
 
-  return <Component handleLayerToggle={toggleLayer} handleZoomChange={handleZoomChange} {...props}/>
+  return <Component 
+    handleLayerToggle={toggleLayer}
+    handleZoomChange={handleZoomChange}
+    onLoad={(map, view) => handleMapLoad(map)}
+    {...props}/>
 }
 
 export default connect(mapStateToProps, actions)(feturedGlobeContainer);
