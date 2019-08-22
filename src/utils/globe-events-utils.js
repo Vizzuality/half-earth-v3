63cfa75512e046dcff11338dd0246f85e97482ac
Javi Abia <weberjavi@gmail.com>
2019-08-22 15:49:45 +0200

    Use image

--- src/utils/globe-events-utils.js
+++ src/utils/globe-events-utils.js
@@ -1,6 +1,7 @@
 import { loadModules } from '@esri/react-arcgis';
+import { get } from 'lodash'
 
-export const setAvatarImage = (view, viewPoint, featuredPlacesLayerTitle) => {
+export const setAvatarImage = (view, viewPoint, featuredPlacesLayerTitle, featuredMapPlaces) => {
   const featuredPoint = viewPoint.results.filter((result) => result.graphic.layer.title === featuredPlacesLayerTitle);
   let marker = document.getElementById('avatar');
   const defaultStyles = 'overflow: hidden; border-radius: 20px; position: absolute; display: none; width: 40px; height: 40px; pointer-events: none; background-size: cover;';
@@ -12,11 +13,13 @@ export const setAvatarImage = (view, viewPoint, featuredPlacesLayerTitle) => {
   }
   if (featuredPoint.length) {
     const { latitude, longitude } = featuredPoint[0].graphic.geometry;
+    const slug = get(featuredPoint, '[0].graphic.attributes.nam_slg');
+    const imageUrl = featuredMapPlaces.data[slug].imageUrl ;
     loadModules(["esri/geometry/Point"]).then(([Point]) => {
       const point = new Point({latitude, longitude});
       const screenCoords = view.toScreen(point);
       document.body.style.cursor = 'pointer';
-      marker.setAttribute("style", `${defaultStyles} left: ${screenCoords.x}px; top: ${screenCoords.y}px; transform: translate(-20px, -20px); display: block; border: 1px solid white; background-image: url(https://i.pinimg.com/originals/1c/c3/c5/1cc3c5ac87c9433861270c7b26a2f89d.jpg)`)
+      marker.setAttribute("style", `${defaultStyles} left: ${screenCoords.x}px; top: ${screenCoords.y}px; transform: translate(-20px, -20px); display: block; border: 1px solid white; background-image: url(${imageUrl})`)
     })
   } else {
     document.body.style.cursor = 'default';
@@ -26,6 +29,6 @@ export const setAvatarImage = (view, viewPoint, featuredPlacesLayerTitle) => {
 
 export const setSelectedFeaturedPlace = (viewPoint, featuredPlacesLayerTitle, changeUI) => {
   const featuredPoint = viewPoint.results.filter((result) => result.graphic.layer.title === featuredPlacesLayerTitle);
-  const selectedFeaturedPlace = featuredPoint.length ? featuredPoint[0].graphic.attributes.nam_slg : null;
+  const selectedFeaturedPlace = featuredPoint.length ? get(featuredPoint, '[0].graphic.attributes.nam_slg') : null;
   if (selectedFeaturedPlace) changeUI({ selectedFeaturedPlace });
 }
