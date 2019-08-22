b4ce53ee66567947210a487c35e6086f7734af9f
Javi Abia <weberjavi@gmail.com>
2019-08-22 15:48:00 +0200

    Create map places redux module

---
+++ src/redux-modules/featured-map-places/featured-map-places-reducers.js
@@ -0,0 +1,27 @@
+import * as actions from './featured-map-places-actions';
+
+export const initialState = {
+  data: null,
+  loading: false,
+  loaded: true,
+  error: false
+};
+
+const fetchFeaturedMapPlacesData = state => ({ ...state, loading: true, loaded: false });
+
+const fetchFeaturedMapPlacesFail = (state, { payload }) => ({ ...state, loading: false, loaded: false, error: payload });
+
+const fetchFeaturedMapPlacesReady = (state, { payload }) => {
+  return ({
+    ...state,
+    loading: false,
+    loaded: true,
+    data: payload.data
+  })
+};
+
+export default {
+  [actions.setFeaturedMapPlaces]: fetchFeaturedMapPlacesData,
+  [actions.fetchFeaturedMapPlacesFail]: fetchFeaturedMapPlacesFail,
+  [actions.fetchFeaturedMapPlacesReady]: fetchFeaturedMapPlacesReady
+}
