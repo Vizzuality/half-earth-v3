8894790ea24f906462fcbdcb8431d4e2d2506f85
Javi Abia <weberjavi@gmail.com>
2019-08-28 13:41:39 +0200

    Update template and build

--- src/service-worker-build.js
+++ src/service-worker-build.js
@@ -1,7 +1,5 @@
 const workboxBuild = require('workbox-build');
 
-// config options here:
-// https://developers.google.com/web/tools/workbox/modules/workbox-build
 const buildSW = () => {
   // This will return a Promise
   return workboxBuild.injectManifest({
@@ -10,15 +8,11 @@ const buildSW = () => {
     globDirectory: 'build',
     globIgnores: ['./custom-service-worker.js', 'node_modules/**/*'],
     globPatterns: [
-      '**/*.{js,css,html}',
+      '**/*.{js,css,html,svg}',
     ],
     // Increase the limit to 4mb:
     maximumFileSizeToCacheInBytes: 4 * 1024 * 1024
-  }).then(({count, size, warnings}) => {
-    // Optionally, log any warnings and details.
-    warnings.forEach(console.warn);
-    console.log(`${count} files will be precached, totaling ${size} bytes.`);
-  });
+  })
 }
 
 buildSW();
--- src/service-worker-template.js
+++ src/service-worker-template.js
@@ -1,28 +1,31 @@
 if ('function' === typeof importScripts) {
+  /* global importScripts */
   importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
   );
   /* global workbox */
   if (workbox) {
-    console.log('Workbox is loaded');
 
-    workbox.setConfig({
-      debug: true
+    workbox.core.setCacheNameDetails({
+      prefix: 'HEv3',
+      suffix: 'v1',
+      precache: 'install-time-cache',
+      runtime: 'run-time-cache'
     });
 
-    /* injection point for manifest files. */
+    // This will be populated at build time
+    // the precached url are the ones defined on 'globPatterns'
+    // on the service-worker-build.js file
     workbox.precaching.precacheAndRoute([]);
 
     /* custom cache rules*/
-    workbox.routing.registerNavigationRoute('/index.html', {
-      blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-    });
+    workbox.routing.registerNavigationRoute('/index.html');
 
     // Cache own images
     workbox.routing.registerRoute(
       /\.(?:png|jpg|jpeg|svg|gif)$/,
       workbox.strategies.cacheFirst({
-        cacheName: 'HEv3-images',
+        cacheName: 'images',
         plugins: [
           new workbox.expiration.Plugin({
             maxEntries: 60,
@@ -39,76 +42,27 @@ if ('function' === typeof importScripts) {
         cacheName: 'HEv3-fonts',
         plugins: [
           new workbox.expiration.Plugin({
-            maxEntries: 60,
             maxAgeSeconds: 120 * 24 * 60 * 60, // 120 Days
           }),
         ],
       })
     );
 
-    // Cache TILES for Firefly and Vibrant base maps from esri servers
-    workbox.routing.registerRoute(
-      /https:\/\/tiles\.arcgis\.com\/tiles\/nGt4QxSblgDfeJn9\/arcgis\/rest\/services\/(Vibrant|Half_Earth_Firefly)\/MapServer\/tile\/\d\/\d\/\d/,
-      workbox.strategies.cacheFirst({
-        cacheName: 'HEv3-esri-tiles',
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 Days
-          }),
-        ],
-      })
-    )
+    // Cache arcgis js API
+    // Not working as expected
+    // Check https://stackoverflow.com/questions/57688543/workbox-cachefirst-strategy-fetching-after-serving-cache-content
+    // workbox.routing.registerRoute(
+    //   /https:\/\/js\.arcgis\.com\/4.12\/esri\/[/\w-]*.(?:js|css)$/,
+    //   workbox.strategies.cacheFirst({
+    //     cacheName: 'arcgis-js-API-4.12',
+    //     plugins: [
+    //       new workbox.cacheableResponse.Plugin({
+    //         statuses: [0, 200],
+    //       })
+    //     ]
+    //   })
+    // )
 
-    // Cache TILEMAPS for Firefly and Vibrant base maps from esri servers
-    workbox.routing.registerRoute(
-      /https:\/\/tiles\.arcgis\.com\/tiles\/nGt4QxSblgDfeJn9\/arcgis\/rest\/services\/(Vibrant|Half_Earth_Firefly)\/MapServer\/tilemap\/\d\/\d\/\d\/32\/32\?f=json/,
-      workbox.strategies.cacheFirst({
-        cacheName: 'HEv3-esri-tiles',
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 Days
-          }),
-        ],
-      })
-    )
-
-    // Cache METADATA for Firefly and Vibrant base maps from esri servers
-    workbox.routing.registerRoute(
-      /https:\/\/tiles\.arcgis\.com\/tiles\/nGt4QxSblgDfeJn9\/arcgis\/rest\/services\/(Vibrant|Half_Earth_Firefly)\/MapServer\?f=json/,
-      workbox.strategies.cacheFirst({
-        cacheName: 'HEv3-esri-tiles',
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 Days
-          }),
-        ],
-      })
-    )
-
-    // TODO arcgis api assets (check version?)
-    workbox.routing.registerRoute(
-      /https:\/\/js\.arcgis\.com\/4.12\/esri\/[/\w-]*.(?:js|css)$/,
-      workbox.strategies.cacheFirst({
-        cacheName: 'HEv3-arcgis-js-API-4.12',
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 Days
-          }),
-        ],
-      })
-    )
-
-    workbox.routing.registerRoute(
-      /https:\/\/www\.arcgis\.com\/sharing\/rest\/content\/items\/[/\w]*\?f=json/,
-      workbox.strategies.staleWhileRevalidate({
-        cacheName: 'HEv3-arcgis-items-description'
-      })
-    )
-    // TODO 3d elevation tiles
-    // TODO remove hemlet preload
-    // TODO world imagery tiles (to be fetched only when zoom higher that 8)
-
-    // TODO handle errors when no getting some layers data
 
   } else {
     console.log('Workbox could not be loaded. No Offline support');
