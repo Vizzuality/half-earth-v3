3571fc76bea12f672f43c8fc987ce572bd801883
Kamil Filip <kamil.filip94@gmail.com>
2019-09-03 11:32:38 +0100

    Remove species legacy redux module & fetch species data by feature layer

--- src/components/landscape-sidebar/species-widget/species-widget-component.jsx
+++ src/components/landscape-sidebar/species-widget/species-widget-component.jsx
@@ -1,17 +1,23 @@
 
 import React, { useEffect } from 'react';
+import { loadModules } from '@esri/react-arcgis';
 
 const SpeciesWidgetComponent = ({ map, terrestrialCellData, setSpecies }) => {
-  const speciesLayer = map.layers.items.find(l => l.title === 'TerrestrialVertebrateSpeciesSHP');
-  const queryParams = speciesLayer.createQuery();
 
   useEffect(() => {
-      if (terrestrialCellData) {
-      queryParams.where = `HBWID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`;
-      speciesLayer.queryFeatures(queryParams).then(function(results){
-        const { features } = results;
-        setSpecies(features.map(c => c.attributes));
-      });
+    if (terrestrialCellData) {
+      loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
+        const speciesLayer = new FeatureLayer({
+          // URL to the service
+          url: "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/TerrestrialVertebrateSpeciesSHP/FeatureServer"
+        });
+        const queryParams = speciesLayer.createQuery();
+        queryParams.where = `HBWID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`;
+        speciesLayer.queryFeatures(queryParams).then(function(results){
+          const { features } = results;
+          setSpecies(features.map(c => c.attributes));
+        });
+      })
     }
   }, [terrestrialCellData])
 
--- src/redux-modules/species-legacy/species.js
+++
@@ -1,8 +0,0 @@
-import reducerRegistry from 'reducerRegistry';
-
-import * as actions from './species-actions';
-import reducers, { initialState } from './species-reducers';
-
-export const reduxConfig = { actions, reducers, initialState };
-
-export default reducerRegistry.registerModule('species', reduxConfig);
