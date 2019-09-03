bc529c19b23181bfd9c1e72d52cc674a19606751
Kamil Filip <kamil.filip94@gmail.com>
2019-09-03 11:32:38 +0100

    Remove species legacy redux module & fetch species data by feature layer

--- src/components/landscape-sidebar/species-widget/species-widget-component.jsx
+++ src/components/landscape-sidebar/species-widget/species-widget-component.jsx
@@ -1,9 +1,29 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { loadModules } from '@esri/react-arcgis';
 
-const SpeciesWidgetComponent = () => (
-  <div>{/* SPECIES WIDGET SOON HERE */}</div>
-);
+const SpeciesWidgetComponent = ({ map, terrestrialCellData, setSpecies }) => {
 
+  useEffect(() => {
+    if (terrestrialCellData) {
+      loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
+        const speciesLayer = new FeatureLayer({
+          // URL to the service
+          url: "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/TerrestrialVertebrateSpeciesSHP/FeatureServer"
+        });
+        const queryParams = speciesLayer.createQuery();
+        queryParams.where = `HBWID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`;
+        speciesLayer.queryFeatures(queryParams).then(function(results){
+          const { features } = results;
+          setSpecies(features.map(c => c.attributes));
+        });
+      })
+    }
+  }, [terrestrialCellData])
+
+  return (
+    <div></div>
+  );
+}
 
 export default SpeciesWidgetComponent;
