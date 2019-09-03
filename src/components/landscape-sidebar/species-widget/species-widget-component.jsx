649c98e759d74667a50b087f93e73c931edcd8fc
Kamil Filip <kamil.filip94@gmail.com>
2019-09-03 14:10:13 +0100

    Use 'definition expression' in the FeatureLayer / Species widget

--- src/components/landscape-sidebar/species-widget/species-widget-component.jsx
+++ src/components/landscape-sidebar/species-widget/species-widget-component.jsx
@@ -4,16 +4,18 @@ import { loadModules } from '@esri/react-arcgis';
 
 const SpeciesWidgetComponent = ({ map, terrestrialCellData, setSpecies }) => {
 
+ 
+
   useEffect(() => {
     if (terrestrialCellData) {
       loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
         const speciesLayer = new FeatureLayer({
           // URL to the service
-          url: "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/TerrestrialVertebrateSpeciesSHP/FeatureServer"
+          url: "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/TerrestrialVertebrateSpeciesSHP/FeatureServer",
+          definitionExpression: `HBWID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`,
         });
-        const queryParams = speciesLayer.createQuery();
-        queryParams.where = `HBWID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`;
-        speciesLayer.queryFeatures(queryParams).then(function(results){
+        const query = speciesLayer.createQuery();
+        speciesLayer.queryFeatures(query).then(function(results){
           const { features } = results;
           setSpecies(features.map(c => c.attributes));
         });
