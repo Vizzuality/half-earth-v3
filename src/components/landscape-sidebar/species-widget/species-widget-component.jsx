c1df4b3ac9d7eb08ab8d06e99f9092b2b025f7d6
Kamil Filip <kamil.filip94@gmail.com>
2019-09-04 11:22:06 +0100

    Species widget - initial commit

--- src/components/landscape-sidebar/species-widget/species-widget-component.jsx
+++ src/components/landscape-sidebar/species-widget/species-widget-component.jsx
@@ -1,28 +1,38 @@
 
-import React, { useEffect } from 'react';
-import { loadModules } from '@esri/react-arcgis';
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+import styles from './species-widget-styles.module.scss';
 
-const SpeciesWidgetComponent = ({ map, terrestrialCellData, setSpecies }) => {
-
-  useEffect(() => {
-    if (terrestrialCellData) {
-      loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
-        const speciesLayer = new FeatureLayer({
-          // URL to the service
-          url: "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/TerrestrialVertebrateSpeciesSHP/FeatureServer"
-        });
-        const queryParams = speciesLayer.createQuery();
-        queryParams.where = `HBWID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`;
-        speciesLayer.queryFeatures(queryParams).then(function(results){
-          const { features } = results;
-          setSpecies(features.map(c => c.attributes));
-        });
-      })
-    }
-  }, [terrestrialCellData])
+const SpeciesWidgetComponent = ({ data }) => {
 
   return (
-    <div></div>
+    <div className={styles.container}>
+      <div className={styles.reptilesChart}></div>
+      <div className={styles.birdsChart}>
+        {data && data.map( s => {
+          return (
+            <>
+              <div 
+                className={styles.point}
+                style={{ bottom: s.pointCoordinates.y, left: s.pointCoordinates.x }}
+                data-tip
+                data-for={s.name}
+                data-effect='solid'
+                data-delay-show={0}
+              >
+              </div>
+              <ReactTooltip id={s.name} className='infoTooltipStyle'>
+                {s.proportion}
+              </ReactTooltip>
+            </>
+          )
+        })}
+      </div>
+      <div className={styles.mammalsChart}></div>
+      <div className={styles.amphibiansChart}></div>
+
+    </div>
+    
   );
 }
 
--- src/components/landscape-sidebar/species-widget/species-widget-selectors.js
+++ src/components/landscape-sidebar/species-widget/species-widget-selectors.js
@@ -1,6 +1,103 @@
-import { createStructuredSelector } from 'reselect';
+import { createSelector, createStructuredSelector } from 'reselect';
 import { getTerrestrialCellData } from 'selectors/grid-cell-selectors';
 
+const getSpeciesData = ({ speciesData }) => (speciesData && speciesData.data) || null;
+
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+const toRadians = (angle) => {
+  return angle * (Math.PI / 180);
+}
+
+const calculateChartPosition = (minAngle, maxAngle, propRange) => {
+  const alphaAngle = getRndInteger(minAngle, maxAngle);
+  const betaAngle = 180 - 90 - alphaAngle;
+
+  const c = 100 - propRange * 100; //revert the axis on the chart
+  const x = c * Math.sin(toRadians(betaAngle));
+  const y = c * Math.cos(toRadians(betaAngle));
+
+  return { x, y };
+}
+
+const getReptilesChartData = createSelector(getSpeciesData, speciesData => {
+  if (!speciesData) return [];
+  console.log(speciesData)
+  
+  const data = speciesData.filter(s => s.taxa === 'reptiles').map((s) => {
+    return {
+      id: s.HBWID,
+      name: s.scntfcn,
+      rangeArea: s.RANGE_A,
+      proportion: s.PROP_RA * 100,
+      taxa: s.taxa,
+      imageURL: s.url_sp,
+      pointCoordinates: calculateChartPosition(91, 179, s.PROP_RA)
+    }
+  }).slice(0, 9);
+  return data;
+})
+
+const getBirdsChartData = createSelector(getSpeciesData, speciesData => {
+  if (!speciesData) return [];
+  
+  const data = speciesData.filter(s => s.taxa === 'birds').map((s) => {
+    return {
+      id: s.HBWID,
+      name: s.scntfcn,
+      rangeArea: s.RANGE_A,
+      proportion: s.PROP_RA * 100,
+      taxa: s.taxa,
+      imageURL: s.url_sp,
+      pointCoordinates: calculateChartPosition(1, 89, s.PROP_RA)
+    }
+  }).slice(0, 9);
+  return data;
+})
+
+const getMammalsChartData = createSelector(getSpeciesData, speciesData => {
+  if (!speciesData) return [];
+  
+  const data = speciesData.filter(s => s.taxa === 'mammals').map((s) => {
+    return {
+      id: s.HBWID,
+      name: s.scntfcn,
+      rangeArea: s.RANGE_A,
+      proportion: s.PROP_RA * 100,
+      taxa: s.taxa,
+      imageURL: s.url_sp,
+      pointCoordinates: calculateChartPosition(181, 269, s.PROP_RA)
+    }
+  }).slice(0, 9);
+  return data;
+})
+
+const getAmphibiansChartData = createSelector(getSpeciesData, speciesData => {
+  if (!speciesData) return [];
+  
+  const data = speciesData.filter(s => s.taxa === 'amphibians').map((s) => {
+    return {
+      id: s.HBWID,
+      name: s.scntfcn,
+      rangeArea: s.RANGE_A,
+      proportion: s.PROP_RA * 100,
+      taxa: s.taxa,
+      imageURL: s.url_sp,
+      pointCoordinates: calculateChartPosition(271, 359, s.PROP_RA)
+    }
+  }).slice(0, 9);
+  return data;
+})
+
+const getData = createSelector([getBirdsChartData, getReptilesChartData, getMammalsChartData, getAmphibiansChartData],
+  (birdsData, reptilesData, mammalsData, amphibiansData) => {
+    // if(!birdsData || !reptilesData) return null;
+    return [...birdsData, ...reptilesData, ...mammalsData, ...amphibiansData];
+  }
+);
+
 export default createStructuredSelector({
+  data: getData,
   terrestrialCellData: getTerrestrialCellData,
 }); 
