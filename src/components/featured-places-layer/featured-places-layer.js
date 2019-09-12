702379e3263f46564357ff522d1992c54a3d953e
Javi Abia <weberjavi@gmail.com>
2019-09-12 16:31:15 +0200

    WIP

--- src/components/featured-places-layer/featured-places-layer.js
+++ src/components/featured-places-layer/featured-places-layer.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { loadModules } from '@esri/react-arcgis';
+import { layerManagerVisibility, createLayer } from 'utils/layer-manager-utils'; 
 
 
 
 const FeaturedMapLayer = ({ map, view, selectedFeaturedMap, featuredPlacesLayer }) => {
 
   const [featuredPlacesLayerView, setFeaturedPlacesLayerView] = useState(null);
+  const [priorityPolygonsFeatureLayer, setPriorityPolygonsFeatureLayer] = useState(null);
 
   // store featured places layer view to query against it
   useEffect(() => {
@@ -26,6 +28,29 @@ const FeaturedMapLayer = ({ map, view, selectedFeaturedMap, featuredPlacesLayer 
       })
     }
   }, [featuredPlacesLayerView, selectedFeaturedMap])
+
+  useEffect(() => {
+    if (selectedFeaturedMap === 'biodiversityPlaces') {
+      if (!priorityPolygonsFeatureLayer) {
+        loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
+         const priorityPolygonsLayer = new FeatureLayer({
+           // URL to the service
+           url: "https://utility.arcgis.com/usrsvcs/servers/9d466a49b2594519bd5dbf4a9d38195f/rest/services/disPriorFacet_v1/FeatureServer"
+         });
+         setPriorityPolygonsFeatureLayer(priorityPolygonsLayer);
+       })
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (selectedFeaturedMap === 'biodiversityPlaces' && priorityPolygonsFeatureLayer) {
+
+      map.add(priorityPolygonsFeatureLayer);
+      const layer = map.layers.items.find(l => l.title === 'DisPriorFacet v1');
+      console.log(layer)
+    } 
+  }, [selectedFeaturedMap, priorityPolygonsFeatureLayer])
 
   return null;
 }
