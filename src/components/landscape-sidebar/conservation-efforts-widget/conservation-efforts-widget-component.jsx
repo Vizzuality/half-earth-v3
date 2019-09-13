ad357d037db99037d35a1e34baedf40735666633
Kamil Filip <kamil.filip94@gmail.com>
2019-09-13 09:53:23 +0200

    Move hooks from Conservation efforts component to container

--- src/components/landscape-sidebar/conservation-efforts-widget/conservation-efforts-widget-component.jsx
+++ src/components/landscape-sidebar/conservation-efforts-widget/conservation-efforts-widget-component.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { loadModules } from '@esri/react-arcgis';
+import React from 'react';
 import PieChart from 'components/pie-chart';
 import CheckboxGroup from 'components/checkbox-group';
 import { 
@@ -7,8 +6,6 @@ import {
   PROTECTED
 } from 'components/landscape-sidebar/conservation-efforts-widget/conservation-efforts-widget-selectors';
 import styles from './conservation-efforts-widget-styles.module.scss';
-
-const findInDOM = (id) => document.getElementById(id);
 
 const ConservationEffortsDescription = ({ allProp, rawData }) => {
   return (
@@ -20,8 +17,6 @@ const ConservationEffortsDescription = ({ allProp, rawData }) => {
 };
 
 const ConservationEffortsWidget = ({
-  setConservationEfforts,
-  terrestrialCellData,
   dataFormatted,
   colors,
   rawData,
@@ -31,53 +26,6 @@ const ConservationEffortsWidget = ({
   activeSlices,
   toggleLayer
 }) => {
-  const [conservationPropsLayer, setConservationPropsLayer] = useState(null);
-
-  useEffect(() => {
-    if (!conservationPropsLayer) {
-      loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
-        const consPropLayer = new FeatureLayer({
-          url: "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/ConsProp/FeatureServer"
-        });
-        setConservationPropsLayer(consPropLayer)
-      });
-    }
-  }, []);
-
-  const queryParams = conservationPropsLayer && conservationPropsLayer.createQuery();
-
-  const orangeActive = alreadyChecked['Protected areas'];
-  const yellowActive = alreadyChecked['Community areas'];
-
-  useEffect(() => {
-    const svg = findInDOM('conservation-widget');
-    const orangeSlice = findInDOM(colors[PROTECTED]);
-    const yellowSlice = findInDOM(colors[COMMUNITY_BASED]);
-
-    if (svg && orangeSlice) {
-      if (orangeActive && yellowActive && orangeSlice && yellowSlice) {
-        // bring both to front
-        svg.appendChild(yellowSlice);
-        svg.appendChild(orangeSlice);
-      } else if (yellowActive && yellowSlice && !orangeActive) {
-        svg.appendChild(yellowSlice);
-      } else if (!yellowActive && orangeSlice && orangeActive) {
-        svg.appendChild(orangeSlice);
-      } else {
-        svg.appendChild(orangeSlice);
-      }
-    }
-  }, [orangeActive, yellowActive])
-
-  useEffect(() => {
-    if (terrestrialCellData && queryParams) {
-      queryParams.where = `CELL_ID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`;
-      conservationPropsLayer.queryFeatures(queryParams).then(function(results){
-        const { features } = results;
-        setConservationEfforts(features.map(c => c.attributes));
-      });
-    }
-  }, [terrestrialCellData])
 
   return (
     <>
--- src/components/landscape-sidebar/conservation-efforts-widget/conservation-efforts-widget.js
+++ src/components/landscape-sidebar/conservation-efforts-widget/conservation-efforts-widget.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { loadModules } from '@esri/react-arcgis';
 import conservationEffortsActions from 'redux_modules/conservation-efforts';
 import { handleLayerRendered } from 'utils/layer-manager-utils';
-
 import { addLayerAnalyticsEvent, removeLayerAnalyticsEvent } from 'actions/google-analytics-actions';
-
+import { 
+  COMMUNITY_BASED,
+  PROTECTED
+} from './conservation-efforts-widget-selectors';
 import Component from './conservation-efforts-widget-component';
 import mapStateToProps from './conservation-efforts-widget-selectors';
 
 const actions = { ...conservationEffortsActions, addLayerAnalyticsEvent, removeLayerAnalyticsEvent };
+
+const findInDOM = (id) => document.getElementById(id);
 
 const ConservationEffortsWidget = (props) => {
   const {
@@ -19,8 +24,59 @@ const ConservationEffortsWidget = (props) => {
     addLayerAnalyticsEvent,
     removeLayerAnalyticsEvent,
     handleLayerToggle,
-    alreadyChecked
+    alreadyChecked,
+    colors,
+    terrestrialCellData,
+    setConservationEfforts
   } = props;
+
+  const [conservationPropsLayer, setConservationPropsLayer] = useState(null);
+
+  useEffect(() => {
+    if (!conservationPropsLayer) {
+      loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
+        const consPropLayer = new FeatureLayer({
+          url: "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/ConsProp/FeatureServer"
+        });
+        setConservationPropsLayer(consPropLayer)
+      });
+    }
+  }, []);
+
+  const queryParams = conservationPropsLayer && conservationPropsLayer.createQuery();
+
+  const orangeActive = alreadyChecked['Protected areas'];
+  const yellowActive = alreadyChecked['Community areas'];
+
+  useEffect(() => {
+    const svg = findInDOM('conservation-widget');
+    const orangeSlice = findInDOM(colors[PROTECTED]);
+    const yellowSlice = findInDOM(colors[COMMUNITY_BASED]);
+
+    if (svg && orangeSlice) {
+      if (orangeActive && yellowActive && orangeSlice && yellowSlice) {
+        // bring both to front
+        svg.appendChild(yellowSlice);
+        svg.appendChild(orangeSlice);
+      } else if (yellowActive && yellowSlice && !orangeActive) {
+        svg.appendChild(yellowSlice);
+      } else if (!yellowActive && orangeSlice && orangeActive) {
+        svg.appendChild(orangeSlice);
+      } else {
+        svg.appendChild(orangeSlice);
+      }
+    }
+  }, [orangeActive, yellowActive])
+
+  useEffect(() => {
+    if (terrestrialCellData && queryParams) {
+      queryParams.where = `CELL_ID IN (${terrestrialCellData.map(i => i.CELL_ID).join(', ')})`;
+      conservationPropsLayer.queryFeatures(queryParams).then(function(results){
+        const { features } = results;
+        setConservationEfforts(features.map(c => c.attributes));
+      });
+    }
+  }, [terrestrialCellData])
 
   const toggleLayer = (layersPassed, option) => {
     const layerNotRendered = !activeLayers.some(layer => layer.title === option.id);
