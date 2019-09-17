2a00a833dfd4ff21a0dc3d92f1a278545875e105
Javi Abia <weberjavi@gmail.com>
2019-09-17 09:57:34 +0200

    Add priority places slugs

--- src/constants/layers-slugs.js
+++ src/constants/layers-slugs.js
@@ -23,3 +23,6 @@ export const FEATURED_PLACES_LAYER = 'featured_places';
 
 // Basemap (featured mode)
 export const VIBRANT_BASEMAP_LAYER = 'Vibrant';
+
+export const PRIORITY_PLACES_POLYGONS = 'priority_places_polygons';
+export const PRIORITY_POLYGONS_GRAPHIC_LAYER = 'priority_polygons_graphic_layer';
