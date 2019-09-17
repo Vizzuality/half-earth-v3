5aab3b7cde03cd045e0f678cfe0c8cb8b45fda0a
Javi Abia <weberjavi@gmail.com>
2019-09-17 09:56:43 +0200

    Taxa selector styling and interactions

--- src/components/featured-taxa-selector/featured-taxa-selector-component.jsx
+++ src/components/featured-taxa-selector/featured-taxa-selector-component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 import { PRIORITY_PLACES_POLYGONS } from 'constants/layers-slugs';
 import layersConfig from 'constants/layers-config';
 
@@ -6,15 +7,24 @@ import styles from './featured-taxa-styles.module.scss';
 
 const taxa = layersConfig[PRIORITY_PLACES_POLYGONS].categories;
 
-const FeaturedTaxaSelectorComponent = () => (
+const FeaturedTaxaSelectorComponent = ({
+  selectedTaxa,
+  handleTaxaButtonClick
+}) => (
   <div className={styles.container}>
     {taxa.map(t => (
-      <div className={styles.taxaButton}>
-        <div className={styles.taxaIconContainer} style={{backgroundColor: `${t.color}`}}>
+      <button className={styles.taxaButton} onClick={() => handleTaxaButtonClick(t.slug)}>
+        <div 
+          className={cx(
+            styles.taxaIconContainer,
+            {[styles.selectedTaxa] : selectedTaxa === t.slug}
+          )}
+          style={{backgroundColor: `${t.color}`}}
+        >
           <t.icon />
         </div>
         <p className={styles.taxaName}>{t.title}</p>
-      </div>
+      </button>
     ))}
   </div>
 )
--- src/components/featured-taxa-selector/featured-taxa-selector.js
+++ src/components/featured-taxa-selector/featured-taxa-selector.js
@@ -1,2 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions/url-actions';
 import Component from './featured-taxa-selector-component.jsx';
-export default Component;
+
+
+const FeaturedTaxaSelectorContainer = props => {
+  const handleTaxaButtonClick = slug => {
+    const { changeUI } = props;
+    changeUI({ selectedTaxa: slug })
+  };
+
+  return (
+    <Component
+      handleTaxaButtonClick={handleTaxaButtonClick}
+      {...props }
+    />
+  )
+}
+
+export default connect(null, actions)(FeaturedTaxaSelectorContainer);
