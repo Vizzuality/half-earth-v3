c593cb4c76603d44f6d3115653169223ea485147
Javi Abia <weberjavi@gmail.com>
2019-09-23 16:59:40 +0200

    Add loader and dynamic id to scene

--- src/components/scene/scene-component.jsx
+++ src/components/scene/scene-component.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { loadModules } from 'esri-loader';
+import Spinner from 'components/spinner';
 
-const SceneComponent = ({ sceneId, children, loaderOptions, sceneSettings }) => {
+const SceneComponent = ({ sceneId, children, loaderOptions, sceneSettings, onLoad = null, loadElement }) => {
 
   const [map, setMap] = useState(null);
   const [view, setView] = useState(null);
+  const [loadState, setLoadState] = useState('loading');
 
   useEffect(() => {
     loadModules(["esri/WebScene", "esri/views/SceneView"], loaderOptions)
@@ -17,7 +19,7 @@ const SceneComponent = ({ sceneId, children, loaderOptions, sceneSettings }) => 
         setMap(_map)
         const _view = new SceneView({
           map: _map,
-          container: "scene-container",
+          container: `scene-container-${sceneId}`,
           ...sceneSettings
         });
         setView(_view)
@@ -27,14 +29,35 @@ const SceneComponent = ({ sceneId, children, loaderOptions, sceneSettings }) => 
       });
   }, [])
 
+  useEffect(() => {
+    if (map && view) {
+      setLoadState('loaded');
+    }
+  }, [map, view])
 
-  return (
-    <div id='scene-container' style={{width:'100vw', height:'100vh'}}>
-      {view && map && React.Children.map(children || null, (child, i) => {
+  useEffect(() => {
+    if (loadState === 'loaded') {
+      onLoad && onLoad(map, view);
+    }
+  }, [loadState])
+
+  if (loadState === 'loading') {
+    return (
+    <>
+      <div id={`scene-container-${sceneId}`} style={{width:'0%', height:'0%'}} />
+      <Spinner spinnerWithOverlay floating/>
+    </>
+    )
+  } else if (loadState === 'loaded') {
+    return (
+      <div id={`scene-container-${sceneId}`} style={{width:'100%', height:'100%', backgroundColor: '#0A212E'}}>
+        {React.Children.map(children || null, (child, i) => {
           return child && <child.type key={i} map={map} view={view} {...child.props}/>;
         })}
     </div>
-  )
+    )
+  }
+
 }
 
 export default SceneComponent;
