df5ef36b2c48d452dccbd734540965ed686814c7
Javi Abia <weberjavi@gmail.com>
2019-09-23 17:02:51 +0200

    Layer toggle small changes on biodiversity layers

--- src/components/biodiversity-layers/biodiversity-layers.js
+++ src/components/biodiversity-layers/biodiversity-layers.js
@@ -3,11 +3,13 @@ import { connect } from 'react-redux';
 import { loadModules } from '@esri/react-arcgis';
 import { layersConfig } from 'constants/mol-layers-configs';
 import { createLayer } from 'utils/layer-manager-utils';
+import * as urlActions from 'actions/url-actions';
+import { layerManagerToggle, exclusiveLayersToggle } from 'utils/layer-manager-utils';
 
 import Component from './biodiversity-layers-component';
 import { addLayerAnalyticsEvent, removeLayerAnalyticsEvent } from 'actions/google-analytics-actions';
 
-const actions = { addLayerAnalyticsEvent, removeLayerAnalyticsEvent }
+const actions = { addLayerAnalyticsEvent, removeLayerAnalyticsEvent, ...urlActions }
 
 const BiodiversityLayerContainer = props => {
   const flyToLayerExtent = bbox => {
@@ -22,25 +24,23 @@ const BiodiversityLayerContainer = props => {
   }
 
   const handleSimpleLayerToggle = layerName => {
-    const { handleLayerToggle, removeLayerAnalyticsEvent } = props;
+    const { removeLayerAnalyticsEvent, activeLayers, changeGlobe, activeCategory } = props;
     const layer = layersConfig.find(l => l.slug === layerName);
-
-    handleLayerToggle(layer.slug);
-    
+    layerManagerToggle(layer.slug, activeLayers, changeGlobe, activeCategory);
     removeLayerAnalyticsEvent({ slug: layer.slug });
   }
 
   const handleExclusiveLayerToggle = (layerToAdd, layerToRemove) => {
-    const { map, exclusiveLayerToggle, addLayerAnalyticsEvent, removeLayerAnalyticsEvent } = props;
-
+    const { map, activeLayers, addLayerAnalyticsEvent, removeLayerAnalyticsEvent, changeGlobe } = props;
     const layer = layersConfig.find(l => l.slug === layerToAdd);
+    console.log(layer)
     const removeLayer = layersConfig.find(l => l.slug === layerToRemove);
     const layerExists = map.layers.items.some(l => l.title === layer.slug);
     const removeSlug = removeLayer && removeLayer.slug;
 
     
     !layerExists && createLayer(layer, map);
-    exclusiveLayerToggle(layer.slug, removeSlug);
+    exclusiveLayersToggle(layer.slug, removeSlug, activeLayers, changeGlobe, 'Biodiversity');
     layer.bbox && flyToLayerExtent(layer.bbox);
 
     removeLayer && removeLayerAnalyticsEvent({ slug: removeLayer.slug });
