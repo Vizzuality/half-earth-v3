71107cac060deb59679b5406c2818630151b1068
Javi Abia <weberjavi@gmail.com>
2019-09-23 17:02:26 +0200

    Modify app layout to consume new data globe

--- src/layouts/app-layout/app-layout-component.jsx
+++ src/layouts/app-layout/app-layout-component.jsx
@@ -2,13 +2,14 @@ import React, { PureComponent } from 'react';
 import Proptypes from 'prop-types';
 import FeaturedGlobe from 'pages/featured-globe';
 import DataGlobe from 'pages/data-globe';
+import DataGlobeSimple from 'pages/simple';
 
 
 class App extends PureComponent {
   render() {
     const { route } = this.props;
     const { page } = route;
-    return page === 'featured-globe' ? <FeaturedGlobe /> : <DataGlobe />;
+    return page === 'featured-globe' ? <FeaturedGlobe /> : <DataGlobeSimple />;
   }
 }
 
---
+++ src/pages/simple/scene-settings.js
@@ -0,0 +1,21 @@
+export default {
+  environment: {
+    atmosphereEnabled: false,
+    background: {
+      type: "color",
+      color: [0,10,16]
+    },
+    alphaCompositingEnabled: true
+  },
+  constraints: {
+    altitude: {
+      max: 35512548,
+      min: 10000
+    }
+  },
+  zoom: 1,
+  center: [16.9515536, 0.116959],
+  ui: {
+    components: []
+  }
+}
