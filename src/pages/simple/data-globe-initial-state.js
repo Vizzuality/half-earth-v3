b8d9e5749a4c76aaf34540fadce01cbf3602e8e5
Javi Abia <weberjavi@gmail.com>
2019-10-02 15:05:35 +0200

    Add all taxa as default on data globe

--- src/pages/simple/data-globe-initial-state.js
+++ src/pages/simple/data-globe-initial-state.js
@@ -11,7 +11,8 @@ export default {
       { title: FIREFLY_BASEMAP_LAYER }, 
       { title: GRID_LAYER }, 
       { title: LANDSCAPE_FEATURES_LABELS_LAYER },
-      { title: CITIES_LABELS_LAYER }
+      { title: CITIES_LABELS_LAYER },
+      { title: 'all-taxa-rarity' }
     ],
     zoom: 1,
     center: [16.9515536, 0.116959],
