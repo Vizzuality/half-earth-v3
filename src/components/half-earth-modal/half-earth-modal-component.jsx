103a1c3c24a9d8380e7c009afd05f24584adca69
Kamil Filip <kamil.filip94@gmail.com>
2019-10-05 12:08:48 +0100

    Add smaller version of HE globe & style mobile tweaks

--- src/components/half-earth-modal/half-earth-modal-component.jsx
+++ src/components/half-earth-modal/half-earth-modal-component.jsx
@@ -3,6 +3,8 @@ import ReactMarkdown from 'react-markdown/with-html';
 import useEventListener from 'hooks/use-event-listener';
 import { ReactComponent as CloseIcon } from 'icons/close.svg';
 import GlobeImage from 'images/globe.png';
+import GlobeSmallImage from 'images/globeSmall.png';
+import { isMobile } from 'constants/responsive';
 
 import data from './half-earth-modal-data';
 
@@ -19,9 +21,11 @@ const HalfEarthModalComponent = ({ handleModalClose, textData }) => {
 
   useEventListener('keydown', keyEscapeEventListener);
 
+  const isOnMobile = isMobile();
+
   return (
     <div className={styles.halfEarthModal}>
-      <div className={styles.wrapper}>
+      <div className={styles.grid}>
         <h1 className={styles.title}>{textData && textData.title}</h1>
         <ReactMarkdown
           className={styles.description}
@@ -37,7 +41,7 @@ const HalfEarthModalComponent = ({ handleModalClose, textData }) => {
         ))}
         <div className={styles.globeWrapper}>
           <div className={styles.progresBars}>
-            <img src={GlobeImage} className={styles.globe} alt="Half-Earth globe" />
+            <img src={isOnMobile ? GlobeSmallImage : GlobeImage} className={styles.globe} alt="Half-Earth globe" />
           </div>
         </div>
       </div>
