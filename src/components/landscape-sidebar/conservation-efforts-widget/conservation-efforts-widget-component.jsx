19ff4b4948c8c1a286c88f8362e892e96d07a19d
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-10-05 12:03:56 +0100

    Render landscape components with fixed heights and titles before data is loaded

--- src/components/landscape-sidebar/conservation-efforts-widget/conservation-efforts-widget-component.jsx
+++ src/components/landscape-sidebar/conservation-efforts-widget/conservation-efforts-widget-component.jsx
@@ -28,30 +28,36 @@ const ConservationEffortsWidget = ({
 }) => {
   return (
     <>
-      {rawData && (
-        <div className={styles.container}>
-          <div className={styles.fixBlur} />
-          <div className={styles.padding}>
-            <h3 className={styles.title}>Conservation Efforts</h3>
-            {rawData && <ConservationEffortsDescription allProp={allProp} rawData={rawData} />}
-            <PieChart
-              data={rawData}
-              activeSlices={activeSlices}
-              colors={colors}
-              alreadyChecked={alreadyChecked}
-            />
-          </div>
-          <CheckboxGroup 
-            handleClick={toggleLayer}
-            checkedOptions={alreadyChecked}
-            options={protectedLayers}
-            theme={styles}
-          />
-          <p className={styles.notUnderConservationLabel}>
-            Not under conservation {dataFormatted.notUnderConservation}%
-          </p>
+      <div className={styles.container}>
+        <div className={styles.fixBlur} />
+        <div className={styles.padding}>
+          <h3 className={styles.title}>Conservation Efforts</h3>
+          {rawData && (
+            <>
+              <ConservationEffortsDescription allProp={allProp} rawData={rawData} />
+              <PieChart
+                data={rawData}
+                activeSlices={activeSlices}
+                colors={colors}
+                alreadyChecked={alreadyChecked}
+              />
+            </>
+          )}
         </div>
-      )}
+        {rawData && (
+          <>
+            <CheckboxGroup 
+              handleClick={toggleLayer}
+              checkedOptions={alreadyChecked}
+              options={protectedLayers}
+              theme={styles}
+            />
+            <p className={styles.notUnderConservationLabel}>
+              Not under conservation {dataFormatted.notUnderConservation}%
+            </p>
+          </>
+        )}
+      </div>
     </>
   )
 }
