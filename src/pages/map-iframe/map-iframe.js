4a1a36725faea3e6679600558969d4b47abde55b
Javi Abia <weberjavi@gmail.com>
2020-01-21 12:46:22 +0100

    Create signed pledge layer on iframe load

--- src/pages/map-iframe/map-iframe.js
+++ src/pages/map-iframe/map-iframe.js
@@ -4,7 +4,7 @@ import postRobot from 'post-robot';
 
 import { loadModules } from 'esri-loader';
 
-import { LAND_HUMAN_PRESSURES_IMAGE_LAYER, PLEDGES_LAYER } from 'constants/layers-slugs';
+import { LAND_HUMAN_PRESSURES_IMAGE_LAYER, PLEDGES_LAYER, SIGNED_PLEDGE_GRAPHIC_LAYER } from 'constants/layers-slugs';
 import { PLEDGES_LAYER_URL } from 'constants/layers-urls';
 import { 
   layerManagerToggle,
@@ -12,29 +12,36 @@ import {
 } from 'utils/layer-manager-utils';
 import { layersConfig } from 'constants/mol-layers-configs';
 import { createLayer, addLayerToMap } from 'utils/layer-manager-utils';
+import { getCoordsFromZipAndCountry } from 'utils/locator-utils';
+import { createPointGraphic, createGraphicLayer, simplePictureMarker } from 'utils/graphic-layer-utils';
 
 import Component from './map-iframe-component.jsx';
 import mapStateToProps from './map-iframe-selectors';
 import pledgeLightIcon from 'icons/pledge.svg'
+import signedPledgeLightIcon from 'images/my_pledge.png'
 
 import ownActions from './map-iframe-actions.js';
 import { handleLayerCreation } from '../../utils/layer-manager-utils.js';
 const actions = { ...ownActions };
 
-const pledgeLight = {
-  type: "point-3d", // autocasts as new PointSymbol3D()
-  symbolLayers: [
-    {
-      type: "icon", // autocasts as new IconSymbol3DLayer()
-      resource: {
-        href: pledgeLightIcon // move the pledge icon to contentful?
-      },
-      size: 15
-    }
-  ]
-};
+const addSignedPledgeToMap = (map, view, signedPledgeZIP, signedPledgeCountry) => {
+  loadModules(["esri/layers/GraphicsLayer", "esri/Graphic", "esri/tasks/Locator"]).then(([
+    GraphicsLayer,
+    Graphic,
+    Locator
+  ]) => {
+    getCoordsFromZipAndCountry(Locator, signedPledgeZIP, signedPledgeCountry).then(data => {
+      const { x, y } = data;
+      const signedPledgeLight = simplePictureMarker(signedPledgeLightIcon);
+      const pointGraphic = createPointGraphic(Graphic, signedPledgeLight, x, y);
+      const signedPledgeGraphicLayer = createGraphicLayer(GraphicsLayer, pointGraphic, SIGNED_PLEDGE_GRAPHIC_LAYER);
+      map.add(signedPledgeGraphicLayer);
+      view.goTo({ center: [x, y] });
+    })
+})
+}
 
-const handleMapLoad = (map, view, activeLayers, isPledgesActive) => {
+const handleMapLoad = (map, view, activeLayers, isPledgesActive, signedPledgeZIP, signedPledgeCountry) => {
 
   // This fix has been added as a workaround to a bug introduced on v4.12
   // The bug was causing the where clause of the mosaic rule to not work
@@ -59,10 +66,11 @@ const handleMapLoad = (map, view, activeLayers, isPledgesActive) => {
     FeatureLayer
   ]) => {
     const layer = new FeatureLayer({
+      title: PLEDGES_LAYER,
       url: PLEDGES_LAYER_URL,
       renderer: {
         type: 'simple',
-        symbol: pledgeLight
+        symbol: simplePictureMarker(pledgeLightIcon)
       },
       featureReduction: { type: 'selection' }
     });
@@ -70,6 +78,10 @@ const handleMapLoad = (map, view, activeLayers, isPledgesActive) => {
     layer.visible = activeLayers.find(l => l.title === PLEDGES_LAYER) || isPledgesActive === 'true';
     map.add(layer);
   })
+
+  if (signedPledgeZIP && signedPledgeCountry) {
+    addSignedPledgeToMap(map, view, signedPledgeZIP, signedPledgeCountry);
+  }
 
   const biodiversityLayerIDs = activeLayers
     .filter(({ category }) => category === "Biodiversity")
@@ -80,8 +92,7 @@ const handleMapLoad = (map, view, activeLayers, isPledgesActive) => {
       const newLayer = createLayer(layerConfig);
       addLayerToMap(newLayer, map);
     });
-}
-
+  }
 const dataGlobeContainer = props => {
   const attachPostRobotListeners = () => {
     postRobot.on('mapFlyToCoordinates', event => {
@@ -116,7 +127,7 @@ const dataGlobeContainer = props => {
   return <Component
     handleLayerToggle={toggleLayer}
     handleZoomChange={handleZoomChange}
-    onLoad={(map, view) => handleMapLoad(map, view, props.activeLayers, props.isPledgesActive)}
+    onLoad={(map, view) => handleMapLoad(map, view, props.activeLayers, props.isPledgesActive, props.signedPledgeZIP, props.signedPledgeCountry)}
     {...props}/>
 }
 
