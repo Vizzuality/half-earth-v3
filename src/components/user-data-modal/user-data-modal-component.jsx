be8c896914497caa39af8794ae88b18030ec7f30
Javi Abia <weberjavi@gmail.com>
2020-02-03 12:54:56 +0100

    Wire component and service

--- src/components/user-data-modal/user-data-modal-component.jsx
+++ src/components/user-data-modal/user-data-modal-component.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-
 import { Modal } from 'he-components';
 import styles from './user-data-modal-styles.module.scss';
 import { ReactComponent as ScreenIcon } from 'icons/screen.svg';
 
 const PROFESSIONS = ['Student', 'Researcher', 'Conservationist', 'Other']
 
-const RadioButton = ({ text, value, checked, onClick, name }) => {
+const RadioButton = ({ text, value, checked, name, onChange }) => {
   return (
     <div className={styles.radioButton}>
       <input
@@ -15,7 +14,7 @@ const RadioButton = ({ text, value, checked, onClick, name }) => {
         id={value}
         value={value}
         checked={checked}
-        onClick={onClick}
+        onChange={onChange}
         readOnly
       />
       <label htmlFor={value} className={styles.radioInput}>
@@ -25,58 +24,75 @@ const RadioButton = ({ text, value, checked, onClick, name }) => {
   )
 }
 
-const step2 = () => {
-  return (
-    <section className={styles.stepContainer}>
-      <ScreenIcon className={styles.screenIcon}/>
-      <p className={styles.title}>THANK YOU! JUST ONE MORE THING…</p>
-      <p className={styles.question}>Would you like to help us improve the map?</p>
-      <p className={styles.bodyText}>If you share your email with us, we will contact you and invite you to share with us your experience with the map, so we can improve it.</p>
-      <input type='text' className={styles.textInput} placeholder={'Writte your email here'} onChange={(event) => console.log(event.target.value)}/>
-      <button className={styles.button} onClick={() => console.log('sent')}>done</button>
-    </section>
-  )
-}
 
-const step1 = (setActiveStep) => {
-  return (
-  <section className={styles.stepContainer}>
-    <p className={styles.title}>WELCOME TO THE NEW HALF-EARTH MAP:</p>
-    <p className={styles.bodyText}>We are trying to improve your experience on the map. Can you tell us a bit about yourself?</p>
-    <p className={styles.question}>Which is your profession?</p>
-    <div className={styles.radioGroupContainer}>
-      {PROFESSIONS.map(profession => (
-        <RadioButton
-          name={'professions'}
-          id={profession}
-          value={profession}
-          text={profession}
-          onClick={() => console.log(profession)}
-        />
-      ))}
-    </div>
-    <input type='text' className={styles.textInput} placeholder={'Writte your profession here'}/>
-    <p className={styles.question}>Please tell us what you use the map for:</p>
-    <input type='text' className={styles.textInput} placeholder={'What do you use the map for?'} onChange={(event) => console.log(event.target.value)}/>
-    <button className={styles.button} onClick={() => setActiveStep('step2')}>SEND</button>
-    </section>
-  )
-}
-
-
-const UserDataModalComponent = () => {
+const UserDataModalComponent = ({ isModalOpen, handleModalClose }) => {
 
   const [activeStep, setActiveStep] = useState('step1');
+  const [userData, setUserData] = useState({
+    job_role: null,
+    job_role_other: null,
+    map_usage: null,
+    email: null
+  });
 
-return (
-  <Modal
-  onRequestClose={() => console.log('close')}
-  isOpen={true}
-  theme={styles}
-  >
-    {activeStep === 'step1' ? step1(setActiveStep) : step2() }
-  </Modal>
-)
+  const userDataUpdate = (key, value) => {
+    setUserData({...userData, [key]:value})
+  }
+
+  const step2 = () => {
+    return (
+      <section className={styles.stepContainer}>
+        <ScreenIcon className={styles.screenIcon}/>
+        <p className={styles.title}>THANK YOU! JUST ONE MORE THING…</p>
+        <p className={styles.question}>Would you like to help us improve the map?</p>
+        <p className={styles.bodyText}>If you share your email with us, we will contact you and invite you to share with us your experience with the map, so we can improve it.</p>
+        <input type='text' className={styles.textInput} placeholder={'Writte your email here'} onChange={(event) => userDataUpdate('email', event.target.value)}/>
+        <button className={styles.button} onClick={() => handleModalClose(userData)}>done</button>
+      </section>
+    )
+  }
+  
+  const step1 = (setActiveStep) => {
+    return (
+    <section className={styles.stepContainer}>
+      <p className={styles.title}>WELCOME TO THE NEW HALF-EARTH MAP:</p>
+      <p className={styles.bodyText}>We are trying to improve your experience on the map. Can you tell us a bit about yourself?</p>
+      <p className={styles.question}>Which is your profession?</p>
+      <div className={styles.radioGroupContainer}>
+
+        {PROFESSIONS.map(profession => (
+          <RadioButton
+            name={'professions'}
+            id={profession}
+            value={profession}
+            text={profession}
+            onChange={(e) => userDataUpdate('job_role', e.target.value)}
+          />
+        ))}
+      </div>
+      <input
+        type='text'
+        className={styles.textInput}
+        placeholder={'Writte your profession here'}
+        onChange={(event) => userDataUpdate('job_role_other', event.target.value)}
+        disabled={userData.job_role !== 'Other'}
+      />
+      <p className={styles.question}>Please tell us what you use the map for:</p>
+      <input type='text' className={styles.textInput} placeholder={'What do you use the map for?'} onChange={(event) => userDataUpdate('map_usage', event.target.value)}/>
+      <button className={styles.button} onClick={() => setActiveStep('step2')}>SEND</button>
+      </section>
+    )
+  }
+
+  return (
+    <Modal
+    onRequestClose={userData => handleModalClose(userData)}
+    isOpen={isModalOpen}
+    theme={styles}
+    >
+      {activeStep === 'step1' ? step1(setActiveStep) : step2() }
+    </Modal>
+  )
 }
 
 export default UserDataModalComponent;
--- src/components/user-data-modal/user-data-modal.js
+++ src/components/user-data-modal/user-data-modal.js
@@ -1,3 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { createUserEntry } from 'services/airtable.js';
 import Component from './user-data-modal-component';
 
-export default Component;
+const UserDataModalContainer = () => {
+  const [isModalOpen, setModalOpen] = useState(true);
+
+  useEffect(() => {
+    const doesModalKeyExists = localStorage.getItem('user-data-modal');
+    if (doesModalKeyExists === null) {
+      localStorage.setItem('user-data-modal', 'true')
+    } else if (doesModalKeyExists === 'false') {
+      setModalOpen(false);
+    }
+  }, [])
+
+  const handleModalClose = userData => {
+    createUserEntry({...userData});
+    localStorage.setItem('user-data-modal', 'false')
+    setModalOpen(false);
+  }
+
+ return (
+  <Component
+    isModalOpen={isModalOpen}
+    handleModalClose={handleModalClose}
+  />
+ )
+}
+
+export default UserDataModalContainer;
