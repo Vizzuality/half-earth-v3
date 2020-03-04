87280f15249ca381ca8bd90114386b05a109d706
Javier Abia <weberjavi@gmail.com>
2020-03-04 08:44:43 +0100

    Feature/terrestrial and marine grid (#156) https://www.pivotaltracker.com/story/show/171566485
    * Update utils
    
    * Update layers urls
    
    * Aggregate multiple cells
    
    * Handle new terrestrial cells ID attribute
    
    * Paint view center cell when no contained cells

--- src/constants/layers-urls.js
+++ src/constants/layers-urls.js
@@ -50,7 +50,8 @@ import {
 const bucketUrl = "https://storage.googleapis.com/cdn.mol.org/half-earth/tiles/phase2";
 const templatePattern = "{level}/{col}/{row}";
 
-export const BIODIVERSITY_FACETS_SERVICE_URL = "https://utility.arcgis.com/usrsvcs/servers/e6c05ee3ee7b45af9577904bf9238529/rest/services/Biodiversity_Facets_Dissolved/FeatureServer/0";
+export const TERRESTRIAL_GRID_URL = "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/Global_Terrestrial_Species_Richness_and_Rarity_Patterns_for_Select_Taxa/FeatureServer";
+export const MARINE_GRID_URL = "https://utility.arcgis.com/usrsvcs/servers/e6c05ee3ee7b45af9577904bf9238529/rest/services/Biodiversity_Facets_Dissolved/FeatureServer/0";
 export const PLEDGES_LAYER_URL = "https://services9.arcgis.com/IkktFdUAcY3WrH25/arcgis/rest/services/PledgeLocationsURL/FeatureServer";
 
 
