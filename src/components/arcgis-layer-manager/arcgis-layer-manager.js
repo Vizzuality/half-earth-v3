9d2bad3a8d92dc8d51124dfc2fe6257a37624fa4
Javi Abia <weberjavi@gmail.com>
2020-03-06 09:38:06 +0100

    Simplify arcgis layer manager and utils

--- src/components/arcgis-layer-manager/arcgis-layer-manager.js
+++ src/components/arcgis-layer-manager/arcgis-layer-manager.js
@@ -9,28 +9,19 @@ const ArcgisLayerManager = ({ map, activeLayers, customFunctions }) => {
   const { items } = layers;
   
   useEffect(() => {
+    items.forEach(sceneLayer => {
+      const isVisible = activeLayers.some(activeLayer => activeLayer.title === sceneLayer.title)
+      if (isVisible) {
+        sceneLayer.visible = true;
+        setOpacity(sceneLayer, activeLayers);
+      } else {
+        sceneLayer.visible = false;
+      }
+      customFunctions && customFunctions.forEach(fn => fn({ layer: sceneLayer, isVisible }))
+    })
     setLayerOrder(activeLayers, map);
   }, [activeLayers])
 
-  items.forEach(mapLayer => {
-    const setActive = activeLayers && activeLayers.some(activeLayer => activeLayer.title === mapLayer.title);
-    mapLayer.layers && mapLayer.layers.items && mapLayer.layers.items.forEach(layer => {
-      const setActive = activeLayers && activeLayers.some(activeLayer => activeLayer.title === layer.title);
-      setOpacity(layer, activeLayers);
-      layer.visible = !!setActive;
-      mapLayer.visible = !!setActive;
-    })
-    setOpacity(mapLayer, activeLayers);
-
-    // Set group layers as visible to allow sublayers to be visible
-    if (mapLayer.type === "group") {
-      mapLayer.visible = true
-    } else {
-      mapLayer.visible = !!setActive;
-    }
-
-    customFunctions && customFunctions.forEach(fn => fn({ layer: mapLayer, setActive }))
-  })
   return null
 }
 
--- src/utils/arcgis-layer-manager-utils.js
+++ src/utils/arcgis-layer-manager-utils.js
@@ -4,14 +4,9 @@ import { LEGEND_FREE_LAYERS } from 'constants/layers-groups';
 export const setLayerOrder = (activeLayers, map) => {
   const { layers } = map;
   const activeLayersIds = activeLayers.filter(l => !includes(LEGEND_FREE_LAYERS, l.title)).map(l => l.title);
-  const visibleLayers = layers.items.reduce((acc, layer) => {
-    const subLayers = layer.layers;
-    const isSublayerActive = subLayers && subLayers.items.some(l => includes(activeLayersIds, l.title))
-    const layerKeyValue = isSublayerActive 
-      ? {[subLayers.items.filter(l => includes(activeLayersIds, l.title))[0].title]: layer}
-      : {[layer.title]: layer}
-    return {...acc, ...layerKeyValue}
-  }, {});
+  const visibleLayers = layers.items.reduce((acc, layer) => (
+    !layer.visible ? {...acc} : { ...acc, [layer.title]: layer}
+  ), {});
   const reversedLayers = [...activeLayersIds].reverse();
   reversedLayers.forEach((layer, i) => {
     map.reorder(visibleLayers[layer], i + LEGEND_FREE_LAYERS.length);
