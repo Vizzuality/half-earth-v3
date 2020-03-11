0e295c7f1c3ea94374b7714e973a3f2fddb31cd3
Javi Abia <weberjavi@gmail.com>
2020-03-11 12:58:36 +0100

    Calculate base layers number instead of legendfree

--- src/utils/arcgis-layer-manager-utils.js
+++ src/utils/arcgis-layer-manager-utils.js
@@ -3,13 +3,15 @@ import { LEGEND_FREE_LAYERS } from 'constants/layers-groups';
 
 export const setLayerOrder = (activeLayers, map) => {
   const { layers } = map;
+  const { items } = layers;
   const activeLayersIds = activeLayers.filter(l => !includes(LEGEND_FREE_LAYERS, l.title)).map(l => l.title);
-  const visibleLayers = layers.items.reduce((acc, layer) => (
+  const visibleLayers = items.reduce((acc, layer) => (
     !layer.visible ? {...acc} : { ...acc, [layer.title]: layer}
-  ), {});
-  const reversedLayers = [...activeLayersIds].reverse();
+    ), {});
+    const reversedLayers = [...activeLayersIds].reverse();
+    const baseLayersNumber = Object.keys(visibleLayers).length - Object.keys(activeLayersIds).length;
   reversedLayers.forEach((layer, i) => {
-    map.reorder(visibleLayers[layer], i + LEGEND_FREE_LAYERS.length);
+    map.reorder(visibleLayers[layer], i + baseLayersNumber);
   })
 }
 
