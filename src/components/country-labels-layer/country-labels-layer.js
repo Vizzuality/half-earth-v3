7a2ebf6e6911535d5525e6897d03b7c11eecf018
Javi Abia <weberjavi@gmail.com>
2020-03-18 16:02:00 +0100

    Add events to the labels layer

--- src/components/country-labels-layer/country-labels-layer.js
+++ src/components/country-labels-layer/country-labels-layer.js
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import * as urlActions from 'actions/url-actions';
 import { loadModules } from 'esri-loader';
 import { handleLayerCreation } from 'utils/layer-manager-utils';
 import { COUNTRIES_LABELS_FEATURE_LAYER } from 'constants/layers-slugs';
@@ -6,7 +8,52 @@ import { layersConfig } from 'constants/mol-layers-configs';
 
 
 const LabelsLayer = props => {
-  const { map } = props;
+  const { map, view, changeGlobe } = props;
+
+  const getLabelsLayer = (results) => {
+    if (!results.length) return null;
+    return results.find(result => result.graphic.layer.id === COUNTRIES_LABELS_FEATURE_LAYER)
+  }
+
+const onLabelClick = (event) => {
+  event.stopPropagation();
+  view.hitTest(event).then( response => {
+    const { results } = response;
+    const labelsLayer = getLabelsLayer(results);
+    if (labelsLayer) {
+      const { graphic } = labelsLayer;
+      const { attributes } = graphic;
+      changeGlobe({countryISO: attributes.GID_0});
+    }
+  })
+}
+const onLabelHover = (event) => {
+  event.stopPropagation();
+  view.hitTest(event).then( response => {
+    const { results } = response;
+    const labelsLayer = results.length && results.find(result => result.graphic.layer.id === COUNTRIES_LABELS_FEATURE_LAYER);
+    if (labelsLayer) {
+      document.body.style.cursor = 'pointer';
+    } else {
+      document.body.style.cursor = 'default';
+    }
+  })
+}
+
+  useEffect(() => {
+    const eventHandler = view.on("click", onLabelClick)
+    return function cleanUp() {
+      eventHandler && eventHandler.remove();
+    }
+  }, [])
+
+  useEffect(() => {
+    const eventHandler = view.on("pointer-move", onLabelHover);
+    return function cleanUp() {
+      eventHandler && eventHandler.remove();
+    }
+  }, [])
+
 
   useEffect(() => {
     loadModules(["esri/layers/support/LabelClass"])
@@ -20,9 +67,9 @@ const LabelsLayer = props => {
           type: "text",
           color: [213,207,202],
           font: {
-            family: "EB Garamond",
-            size: 13,
-            weight: "bold"
+            family: "Helvetica",
+            size: 12,
+            weight: "normal"
           },
           haloColor: [0, 0, 0, 255],
           haloSize: 1
@@ -52,4 +99,4 @@ const LabelsLayer = props => {
   return null
 }
 
-export default LabelsLayer;
+export default connect(null, urlActions)(LabelsLayer);
