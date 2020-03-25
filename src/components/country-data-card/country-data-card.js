bdf67b88e684be8a14624e1486047a36fbb59dc1
Javi Abia <weberjavi@gmail.com>
2020-03-25 16:19:22 +0100

    Add country data to the store on country mode

--- src/components/country-data-card/country-data-card.js
+++ src/components/country-data-card/country-data-card.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import * as actions from 'actions/url-actions';
+import { loadModules } from 'esri-loader';
+import { COUNTRIES_DATA_FEATURE_LAYER } from 'constants/layers-slugs';
+import { LAYERS_URLS } from 'constants/layers-urls';
+import * as urlActions from 'actions/url-actions';
+import countryDataActions from 'redux_modules/country-data';
 import Component from './country-data-card-component';
 
+const actions = { ...urlActions, ...countryDataActions};
+
 const CountryDataCardContainer = (props) => {
-  const { sceneMode, changeUI } = props;
+  const {
+    sceneMode,
+    changeUI,
+    countryISO,
+    setCountryDataLoading,
+    setCountryDataReady,
+    setCountryDataError
+  } = props;
+  
+  const [countriesDataLayer, setCountriesDataLayer] = useState(null);
+
+  useEffect(() => {
+    loadModules(["esri/layers/FeatureLayer"]).then(([FeatureLayer]) => {
+      const _countriesDataLayer = new FeatureLayer({
+        url: LAYERS_URLS[COUNTRIES_DATA_FEATURE_LAYER]
+      });
+      _countriesDataLayer.outFields = ['*'];
+      setCountriesDataLayer(_countriesDataLayer)
+    });
+  }, [])
+
+  useEffect(() => {
+    if (countriesDataLayer && countryISO) {
+      setCountryDataLoading();
+      const query = countriesDataLayer.createQuery();
+      query.where = `GID_0 = '${countryISO}'`;
+      countriesDataLayer.queryFeatures(query)
+      .then((results) => {
+        const { features } = results;
+        setCountryDataReady(features);
+      })
+      .catch((error) => {
+        setCountryDataError(error);
+      });
+    }
+  }, [countriesDataLayer, countryISO])
+
   const handleSceneModeChange = () => {
     changeUI({ sceneMode: sceneMode === 'global' ? 'local' : 'global', selectedCountry: false });
   }
   return (
-    <Component handleSceneModeChange={handleSceneModeChange}{...props} />
+    <Component handleSceneModeChange={handleSceneModeChange} {...props}/>
   )
 }
 
