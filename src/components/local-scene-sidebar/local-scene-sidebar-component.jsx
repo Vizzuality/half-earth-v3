0b8f7389dd72e276dee95529548a1e5a29e3cc14
Javi Abia <weberjavi@gmail.com>
2020-04-17 09:19:44 +0200

    Add local scene sidebar elements

--- src/components/local-scene-sidebar/local-scene-sidebar-component.jsx
+++ src/components/local-scene-sidebar/local-scene-sidebar-component.jsx
@@ -2,21 +2,59 @@ import React from 'react';
 import cx from 'classnames';
 
 import CountryDataCard from 'components/country-data-card';
+import BiodiversitySidebarCard from 'components/biodiversity-sidebar-card';
+import ProtectedAreasSidebarCard from 'components/protected-areas-sidebar-card';
+import HumanImpactSidebarCard from 'components/human-impact-sidebar-card';
 import animationStyles from 'styles/common-animations.module.scss';
 import styles from './local-scene-sidebar-styles.module.scss';
 
 const LocalSceneSidebarComponent = ({
+  map,
   view,
+  rasters,
+  setRasters,
   countryISO,
   countryName,
-  isFullscreenActive
+  activeLayers,
+  activeCategory,
+  isFullscreenActive,
+  handleGlobeUpdating,
+  countedActiveLayers
 }) => {
   const sidebarHidden = isFullscreenActive;
   return (
     <div className={cx(styles.container, {
       [animationStyles.leftHidden]: sidebarHidden,
     })}>
-      <CountryDataCard view={view} countryISO={countryISO} countryName={countryName}/>
+      <CountryDataCard
+        view={view}
+        countryISO={countryISO}
+        countryName={countryName}
+      />
+      <BiodiversitySidebarCard
+        map={map}
+        activeLayers={activeLayers}
+        activeCategory={activeCategory}
+        countedActiveLayers={countedActiveLayers}
+        className={styles.biodiversitySidebarCard}
+      />
+      <ProtectedAreasSidebarCard
+        map={map}
+        activeLayers={activeLayers}
+        activeCategory={activeCategory}
+        handleGlobeUpdating={handleGlobeUpdating}
+        countedActiveLayers={countedActiveLayers}
+      />
+      <HumanImpactSidebarCard
+        map={map}
+        view={view}
+        rasters={rasters}
+        setRasters={setRasters}
+        activeLayers={activeLayers}
+        activeCategory={activeCategory}
+        handleGlobeUpdating={handleGlobeUpdating}
+        countedActiveLayers={countedActiveLayers}
+      />
     </div>
   )
 }
