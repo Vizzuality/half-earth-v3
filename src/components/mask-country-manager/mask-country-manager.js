c1284cea54827d60bca091dbcd6255c6928bee77
Javier Abia <weberjavi@gmail.com>
2020-04-17 15:05:53 +0200

    Feature/countries dropdown (#183) https://www.pivotaltracker.com/story/show/171812911
    * Add dropdown to fixed header
    
    * Add vertebrates data
    
    * Add animation on toggle button
    
    * Fix key warning
    
    * Create countries list redux module
    
    * Use list in dropdown
    
    * Update country selection on option click
    
    * Update dorpdown styles
    
    * Avoid no population error
    
    * Order countries list
    
    * Asign layer visibility after extent change
    
    * Filter out selected country
    
    * Add vertical scroll styling
    
    * Update css API assets version

--- src/components/mask-country-manager/mask-country-manager.js
+++ src/components/mask-country-manager/mask-country-manager.js
@@ -13,7 +13,7 @@ const queryCountryData = (countryLayer, countryISO, spatialReference, countryExt
     query.geometry = extentGeometry;
 
     countryLayer.queryFeatures(query)
-      .then(async function(results){
+    .then(async function(results){
         const { features } = results;
         const countryGeometry = features.find(({ attributes }) => attributes.GID_0 === countryISO).geometry;
         const neighbourCountriesGeometry = features.filter(({ attributes }) => attributes.GID_0 !== countryISO);
@@ -57,7 +57,7 @@ const MaskCountryManager = props => {
     if(graphicsLayer) {
       graphicsLayer.visible = isCountryMode;
     }
-  }, [isCountryMode]);
+  }, [countryExtent, isCountryMode]);
 
   useEffect(() => {
     if (countryLayer && countryISO  && spatialReference && countryExtent && graphicsLayer && isCountryMode) {
