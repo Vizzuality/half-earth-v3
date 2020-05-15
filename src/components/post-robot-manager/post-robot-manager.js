d96e4b90e565fb21f0535988bcf4e8aae2154aef
javiabia <weberjavi@gmail.com>
2020-05-15 14:09:14 +0200

    Remove post robot utils and hooks

--- src/components/post-robot-manager/post-robot-manager.js
+++ src/components/post-robot-manager/post-robot-manager.js
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
-import { loadModules } from 'esri-loader';
+import { addSignedPledgeToMap } from 'utils/constant-contact-layers-utils';
 import postRobot from 'post-robot';
-import { getCoordsFromZipAndCountry } from 'utils/locator-utils';
-import { createPointGraphic, createGraphicLayer, simplePictureMarker } from 'utils/graphic-layer-utils';
-import signedPledgeLightIcon from 'images/my_pledge.png'
-import { SIGNED_PLEDGE_GRAPHIC_LAYER } from 'constants/layers-slugs';
+
 import {
   layerManagerToggle,
   layerManagerOpacity
@@ -26,27 +23,9 @@ const PostRobotManagerContainer = ({ map, view, activeLayers, listeners, handleP
 
   const toggleLayer = async (layerTitle, activeLayers) => {
     const layerConfig = layersConfig[layerTitle];
+    if (!layerConfig) return;
     await handleLayerCreation(layerConfig, map);
     layerManagerToggle(layerTitle, activeLayers, handlePostRobotUpdates);
-  }
-
-  const addSignedPledgeToMap = (map, view, ZIP, countryCode) => {
-    console.log('addSignedPledgeToMap')
-    loadModules(["esri/layers/GraphicsLayer", "esri/Graphic", "esri/tasks/Locator"]).then(([
-      GraphicsLayer,
-      Graphic,
-      Locator
-    ]) => {
-      getCoordsFromZipAndCountry(Locator, ZIP, countryCode).then(data => {
-        console.log(data)
-          const { x, y } = data;
-          const signedPledgeLight = simplePictureMarker(signedPledgeLightIcon);
-          const pointGraphic = createPointGraphic(Graphic, signedPledgeLight, x, y);
-          const signedPledgeGraphicLayer = createGraphicLayer(GraphicsLayer, pointGraphic, SIGNED_PLEDGE_GRAPHIC_LAYER);
-          map.add(signedPledgeGraphicLayer);
-          view.goTo({ center: [x, y] });
-      }, (error) => {console.warn(error)})
-  })
   }
 
 
