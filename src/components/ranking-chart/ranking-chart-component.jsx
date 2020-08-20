1c52375ae6e66ea53f644ab6b5664ff7b5db7b8d
Alvaro Leal <pursuance@gmail.com>
2020-08-20 22:38:29 +0200

    Create tooltip start scroll indication

--- src/components/ranking-chart/ranking-chart-component.jsx
+++ src/components/ranking-chart/ranking-chart-component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ranking-chart-styles.module.scss';
 import cx from 'classnames';
+import { Tooltip } from 'react-tippy';
 import { SORT } from './ranking-chart';
 import { ReactComponent as QuestionIcon } from 'icons/borderedQuestion.svg';
+
 
 const legendText = {
   species: {
@@ -12,7 +14,7 @@ const legendText = {
   },
   human: {
     veryHigh: 'Very high human modification',
-    totalMinusVeryHigh: 'human modification'
+    totalMinusVeryHigh: 'Human modification'
   },
   protection: {
     protected: 'Protected areas',
@@ -32,14 +34,41 @@ const RankingChart = ({
 }) => {
   const renderBar = (name, d) =>
     !d || !d[name] ? null : (
-      <div className={styles.fullBar}>
-        {Object.keys(d[name]).map((k) => (
-          <span
-            className={cx(styles.bar, styles[k])}
-            style={{ width: `${d[name][k]}%` }}
-          />
-        ))}
-      </div>
+      <Tooltip
+        html={
+          <div className={styles.tooltip}>
+            <div className={styles.labels}>
+              {Object.keys(d[name]).map((k) => (
+                <div>{legendText[name][k]}: </div>
+              ))}
+            </div>
+            <div className={styles.values}>
+              {Object.keys(d[name]).map(
+                (k) =>
+                  (<div
+                    className={cx(styles.valuesBox, styles[k])}
+                    style={{
+                      height: `${100 / Object.keys(d[name]).length}%`
+                    }}
+                  >
+                    {Math.floor(d[name][k] * 100) / 100}%
+                  </div>)
+              )}
+            </div>
+          </div>
+        }
+        animation="none"
+        position="bottom"
+      >
+        <div className={styles.fullBar}>
+          {Object.keys(d[name]).map((k) => (
+            <span
+              className={cx(styles.bar, styles[k])}
+              style={{ width: `${d[name][k]}%` }}
+            />
+          ))}
+        </div>
+      </Tooltip>
     );
   const renderHeaderItem = (category) => {
     const sortedCategory = sortRankingCategory && sortRankingCategory.split('-')[0];
@@ -74,38 +103,40 @@ const RankingChart = ({
         <QuestionIcon className={styles.question} onClick={handleInfoClick} />
       </div>
       {data && data.length ? (
-        <div className={styles.rankingChartContent}>
-          <div className={styles.table}>
-            <div className={cx(styles.row, styles.header)}>
-              {categories.map((c) => renderHeaderItem(c.toUpperCase()))}
-            </div>
-            {data.map((d) => (
-              <div className={styles.row} key={d.name}>
-                {categories.map((c) => renderBar(c, d))}
-                <div className={styles.spiCountry}>
-                  <span className={cx(styles.titleText, styles.spiIndex)}>
-                    {d.index}.
-                  </span>
-                  <span className={styles.titleText}>
-                    {`${d.name} (${d.spi})`}
-                  </span>
-                </div>
+        <div className={styles.rankingChartContentContainer}>
+          <div className={styles.rankingChartContent}>
+            <div className={styles.table}>
+              <div className={cx(styles.row, styles.header)}>
+                {categories.map((c) => renderHeaderItem(c.toUpperCase()))}
               </div>
-            ))}
-          </div>
-          <div className={styles.legend}>
-            {Object.keys(legendText).map((category) => (
-              <div className={styles.legendBlock}>
-                {Object.keys(legendText[category]).map((text) => (
-                  <div className={styles.legendItem}>
-                    <span className={cx(styles.legendColor, styles[text])} />
-                    <span className={styles.legendText}>
-                      {legendText[category][text]}
+              {data.map((d) => (
+                <div className={styles.row} key={d.name}>
+                  {categories.map((c) => renderBar(c, d))}
+                  <div className={styles.spiCountry}>
+                    <span className={cx(styles.titleText, styles.spiIndex)}>
+                      {d.index}.
+                    </span>
+                    <span className={styles.titleText}>
+                      {`${d.name} (${d.spi})`}
                     </span>
                   </div>
-                ))}
-              </div>
-            ))}
+                </div>
+              ))}
+            </div>
+            <div className={styles.legend}>
+              {Object.keys(legendText).map((category) => (
+                <div className={styles.legendBlock}>
+                  {Object.keys(legendText[category]).map((text) => (
+                    <div className={styles.legendItem}>
+                      <span className={cx(styles.legendColor, styles[text])} />
+                      <span className={styles.legendText}>
+                        {legendText[category][text]}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       ) : null}
