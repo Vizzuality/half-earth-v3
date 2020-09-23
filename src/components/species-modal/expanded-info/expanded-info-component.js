47942fbd4ccbd30bc90c0debfd862e0e681ece77
Alvaro <alvaro.leal@vizzuality.com>
2020-09-23 17:00:35 +0200

    Don't show image if its missing (#228) https://half-earth-map.atlassian.net/browse/HE-162

--- src/components/species-modal/expanded-info/expanded-info-component.js
+++ src/components/species-modal/expanded-info/expanded-info-component.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cx from 'classnames';
 import { Loading, Button } from 'he-components';
 import styles from './expanded-info-styles.module.scss';
 import buttonTheme from 'styles/themes/button-theme.module.scss';
@@ -17,17 +18,34 @@ const ExpandedInfo = (props) => {
     );
   }
 
+  const Image = (props) => {
+    const { className, alt, src } = props;
+    const [hasError, setHasError] = useState(false);
+
+    if (!src) return null;
+
+    return (
+      <img
+        className={cx(className, [hasError])}
+        onError={() => setHasError(true)}
+        alt={alt}
+        {...props}
+      />
+    );
+  };
+
   const { rangemap, image, info, scientificname } = data[0];
+
   return (
     <div className={styles.expandedInfo}>
       <div className={styles.imagesContainer}>
-        <img
+        <Image
           title={`${speciesName} Range map`}
           alt={`${speciesName} Range map`}
           src={rangemap}
           className={styles.image}
         />
-        <img
+        <Image
           title={`${speciesName}`}
           alt={`${speciesName}`}
           src={image && image.url}
