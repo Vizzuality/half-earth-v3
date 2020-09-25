329a3e5da5725a5992f4f0a1aa358f05ade9dd79
javiabia <weberjavi@gmail.com>
2020-09-25 10:27:22 +0200

    Move continents to country constants

--- src/components/country-challenges-chart/country-challenges-modal-additional/country-challenges-modal-additional-component.jsx
+++ src/components/country-challenges-chart/country-challenges-modal-additional/country-challenges-modal-additional-component.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import cx from 'classnames';
 import kebabCase from 'lodash/kebabCase';
+
+import { CONTINENTS } from 'constants/country-mode-constants';
+
 import styles from './country-challenges-modal-additional-styles.module.scss';
 
-const CountryChallengesModalAdditional = () => {
-  const renderContinent = (title) => (
-    <div key={`legend-${title}`} className={cx(styles.continent)}>
-      <span className={cx(styles.legendDot, styles[kebabCase(title)])} />
-      <span className={cx(styles.legendContinent, styles[title])}>{title}</span>
+const CountryChallengesModalAdditional = () => (
+  <div className={styles.modalAdditionalContent}>
+    <div className={styles.countriesLegend}>
+      {CONTINENTS.map((continent) => (
+        <div key={`legend-${continent}`} className={cx(styles.continent)}>
+          <span className={cx(styles.legendDot, styles[kebabCase(continent)])} />
+          <span className={cx(styles.legendContinent, styles[continent])}>{continent}</span>
+        </div>
+      ))}
     </div>
-  );
-
-  const CONTINENTS = [
-    'Africa',
-    'Antarctica',
-    'Asia',
-    'Europe',
-    'North america',
-    'Oceania',
-    'South america'
-  ];
-
-  return (
-    <div className={styles.modalAdditionalContent}>
-      <div className={styles.countriesLegend}>
-        {CONTINENTS.map((continent) => renderContinent(continent))}
-      </div>
-    </div>
-  );
-};
+  </div>
+);
 
 export default CountryChallengesModalAdditional;
