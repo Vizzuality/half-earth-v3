2e99b97738e11c1ba3dfcb0d5486411053ea0dcf
javiabia <weberjavi@gmail.com>
2020-09-28 22:24:34 +0200

    Styled highlighted species

--- src/components/highlighted-species-list/highlighted-species-list-component.jsx
+++ src/components/highlighted-species-list/highlighted-species-list-component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import speciesPlaceholder from 'images/speciesPlaceholder.svg';
+
+import styles from './highlighted-species-list-styles.module.scss';
 
 const HighlightedSpeciesListComponent = ({
   highlightedSpecies
 }) => (
-  <div>
+  <div className={styles.container}>
     {highlightedSpecies && highlightedSpecies.map(species => (
-      <div>
-        <section>image</section>
-        <section>
-          <span>Name</span>
-          <span>{species.attributes.species}</span>
-          <span>Global range protected</span>
-          <span>{species.attributes.percentprotectedglobal}</span>
+      <div className={styles.species}>
+        <img className={styles.image} src={species.imageUrl || speciesPlaceholder} alt={`${species.name}`}/>
+        <section className={styles.dataSection}>
+          <p className={styles.name}>{species.name}</p>
+          <p className={styles.scientificName}>{species.scientificName}</p>
+          <p className={styles.rangeSentence}>Global range protected:</p>
+          <p className={styles.range}>{species.rangeProtected}</p>
         </section>
       </div>
     ))}
--- src/components/highlighted-species-list/highlighted-species-list.js
+++ src/components/highlighted-species-list/highlighted-species-list.js
@@ -1,22 +1,45 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useReducer} from 'react';
 import { random } from 'lodash';
  import Component from './highlighted-species-list-component';
 import EsriFeatureService from 'services/esri-feature-service';
+import MolService from 'services/mol';
 import { HIGHLIGHTED_COUNTRY_SPECIES_URL } from 'constants/layers-urls';
 
 const HighlightedSpeciesContainer = (props) => {
   const {countryISO, maxHighlightedSpecies } = props;
+  const [highlightedSpeciesInitial, setHiglightedSpeciesInitial] = useState(null);
   const [highlightedSpecies, setHiglightedSpecies] = useState(null);
 
   useEffect(() => {
     const randomizeSpecies = random(1, maxHighlightedSpecies);
     EsriFeatureService.getFeatures({
       url: HIGHLIGHTED_COUNTRY_SPECIES_URL,
-      whereClause: `iso3 = '${countryISO}' AND random = ${randomizeSpecies}`
+      whereClause: `GID_0 = '${countryISO}' AND random = ${randomizeSpecies}`
     }).then((features) => {
-      setHiglightedSpecies(features)
+      const _highlightedSpeciesInitial = features.map((species) => ({
+            rangeProtected: species.attributes.percentprotectedglobal,
+            scientificName: species.attributes.species_scientific_name,
+          }))
+          setHiglightedSpeciesInitial(_highlightedSpeciesInitial)
     })
   }, [countryISO])
+
+  useEffect(() => {
+    if (highlightedSpeciesInitial) {
+      const speciesNames = highlightedSpeciesInitial.map(species => species.scientificName);
+      MolService.getSpecies(speciesNames).then((results) => {
+        const _highlightedSpecies = results.map((species, index) => (
+          {
+            ...highlightedSpeciesInitial[index],
+            name: species.commonname,
+            imageUrl: species.image && species.image.url
+          }
+        ))
+        setHiglightedSpecies(_highlightedSpecies)
+      }
+      )
+    }
+  }, [highlightedSpeciesInitial])
 
   return (
     <Component
