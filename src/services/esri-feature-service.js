7b612de14190fa2f6116ceb9e956f3b52692cd63
javiabia <weberjavi@gmail.com>
2020-09-29 14:43:44 +0200

    use constant for outSpatialReference

--- src/services/esri-feature-service.js
+++ src/services/esri-feature-service.js
@@ -1,6 +1,7 @@
 import { loadModules } from 'esri-loader';
+import { LOCAL_SPATIAL_REFERENCE } from 'constants/scenes-constants';
 
-function getFeatures({ url, whereClause = "", outFields = ["*"], returnGeometry = false, outSpatialReference = 102100 }) {
+function getFeatures({ url, whereClause = "", outFields = ["*"], returnGeometry = false, outSpatialReference = LOCAL_SPATIAL_REFERENCE }) {
   return new Promise((resolve, reject) => {
     loadModules(["esri/tasks/QueryTask", "esri/tasks/support/Query"]).then(([QueryTask, Query]) => {
       var queryTask = new QueryTask({
