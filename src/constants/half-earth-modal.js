f0be2f9d09a7537c7eb74798784ece10b6fb7b2f
javiabia <weberjavi@gmail.com>
2020-09-30 12:58:02 +0200

    move constants to constants :)

---
+++ src/constants/half-earth-modal.js
@@ -0,0 +1,17 @@
+import LandLegend from 'images/landLegend.png';
+import MarineLegend from 'images/marineLegend.png';
+
+export const MARINE_LABEL = 'CURRENT MARINE PROTECTED AREAS';
+export const LAND_LABEL = 'CURRENT LAND PROTECTED AREAS'
+
+export const legend = [
+  {
+    protectionLabel: MARINE_LABEL ,
+    imageSrc: MarineLegend
+  },
+  {
+    protectionLabel: LAND_LABEL,
+    imageSrc: LandLegend
+  }
+];
+
