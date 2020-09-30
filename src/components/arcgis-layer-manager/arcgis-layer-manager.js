179f7ed6e40accd8c914011dc066e75066dea5ce
Alvaro Leal <pursuance@gmail.com>
2020-09-30 15:29:28 +0200

    Clean up. We dont need the promises array

--- src/components/arcgis-layer-manager/arcgis-layer-manager.js
+++ src/components/arcgis-layer-manager/arcgis-layer-manager.js
@@ -7,7 +7,6 @@ import { layersConfig as MOLLayersConfig } from 'constants/mol-layers-configs';
 import { setLayerOrder, setOpacity } from 'utils/arcgis-layer-manager-utils';
 import { createLayer, addLayerToMap } from 'utils/layer-manager-utils';
 
-
 const ArcgisLayerManager = ({ map, activeLayers, userConfig, customFunctions }) => {
   // Map prop is inherited from Webscene component
   // reference: https://github.com/Esri/react-arcgis#advanced-usage
@@ -15,28 +14,21 @@ const ArcgisLayerManager = ({ map, activeLayers, userConfig, customFunctions }) 
   const { layers } = map;
   const { items } = layers;
 
+  // Active layers will be always checked and added to the map if they are not
   useEffect(() => {
     const checkCreateLayers = async () => {
       const addLayer = async (layer) => {
         const newLayer = await createLayer(layer);
-        const addedLayer = await addLayerToMap(newLayer, map);
-        return addedLayer;
+        addLayerToMap(newLayer, map);
       };
 
-      const createdLayersPromises = [];
       activeLayers.forEach((layer) => {
         const isLayerInMap = items.some((l) => l.title === layer.title);
         if (!isLayerInMap) {
           const layerWithConfig = MOLLayersConfig[layer.title] || layer;
-          const addedLayer = addLayer(layerWithConfig);
-          createdLayersPromises.push({ promise: addedLayer, slug: layer.title });
+          addLayer(layerWithConfig);
         }
       });
-
-      // if (createdLayersPromises.length) {
-      //   await Promise.all(createdLayersPromises.map(l => l.promise));
-      //   Time to style layers (Is this needed)
-      // }
     }
 
     if (activeLayers && activeLayers.length) {
@@ -44,11 +36,13 @@ const ArcgisLayerManager = ({ map, activeLayers, userConfig, customFunctions }) 
     }
   }, [activeLayers]);
 
-
   useEffect(() => {
-    const disabledUserLayers = userConfig && Object.keys(userConfig.layers).filter(
+    const disabledUserLayers =
+      userConfig &&
+      Object.keys(userConfig.layers).filter(
         (layerGroupKey) => !userConfig.layers[layerGroupKey]
       );
+
     items.forEach((sceneLayer) => {
       const isVisible = activeLayers.some(
         (activeLayer) => activeLayer.title === sceneLayer.title
@@ -76,7 +70,6 @@ const ArcgisLayerManager = ({ map, activeLayers, userConfig, customFunctions }) 
       if (customFunctions) {
         customFunctions.forEach((fn) => fn({ layer: sceneLayer, isVisible }));
       }
-
     });
     setLayerOrder(activeLayers, map);
   }, [activeLayers, userConfig]);
