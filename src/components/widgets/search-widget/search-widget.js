9f632ecb992f96112264ee71711a57dc7c2832f5
javiabia <weberjavi@gmail.com>
2020-10-13 10:37:36 +0200

    Add config object to search hook

--- src/components/widgets/search-widget/search-widget.js
+++ src/components/widgets/search-widget/search-widget.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { openPlacesSearchAnalyticsEvent, searchLocationAnalyticsEvent } from 'actions/google-analytics-actions';
 import { changeGlobe } from 'actions/url-actions';
 import SearchWidgetComponent from './search-widget-component';
+import { COUNTRIES_GENERALIZED_BORDERS_FEATURE_LAYER } from 'constants/layers-slugs';
+import { LAYERS_URLS } from 'constants/layers-urls';
 import { useSearchWidgetLogic } from 'hooks/esri';
 
 const actions = { openPlacesSearchAnalyticsEvent, searchLocationAnalyticsEvent, changeGlobe };
@@ -18,11 +20,38 @@ const SearchWidget = ({ view, openPlacesSearchAnalyticsEvent, searchLocationAnal
     }
   }
 
+  const searchSources = (FeatureLayer, Locator) => (
+    [
+      {
+        layer: new FeatureLayer({
+          url: LAYERS_URLS[COUNTRIES_GENERALIZED_BORDERS_FEATURE_LAYER],
+          title: COUNTRIES_GENERALIZED_BORDERS_FEATURE_LAYER,
+        }),
+        outFields: ["*"],
+        searchFields: ["GID_0", "NAME_0"],
+        name: "Explore countries",
+        maxSuggestions: 10
+      },
+      {
+        locator: new Locator("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"),
+        singleLineFieldName: "SingleLine",
+        outFields: ["Addr_type"],
+        categories: ['District', 'City', 'Metro Area','Subregion', 'Region', 'Territory', 'Water Features', 'Land Features', 'Nature Reserve'],
+        name: "other geographic results"
+      }
+    ]
+  )
+
+  const searchWidgetConfig = {
+    postSearchCallback,
+    searchSources
+  }
+
   const { handleOpenSearch, handleCloseSearch, searchWidget } = useSearchWidgetLogic(
     view,
     openPlacesSearchAnalyticsEvent,
     searchLocationAnalyticsEvent,
-    postSearchCallback
+    searchWidgetConfig
   );
 
   return (
