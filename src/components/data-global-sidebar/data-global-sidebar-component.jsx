a951d947a4e057ae42cd0e18e9535536cf88aa68
javiabia <weberjavi@gmail.com>
2020-10-13 17:21:20 +0200

    Use search func inside component

--- src/components/data-global-sidebar/data-global-sidebar-component.jsx
+++ src/components/data-global-sidebar/data-global-sidebar-component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import cx from 'classnames';
 import BiodiversitySidebarCard from 'components/biodiversity-sidebar-card';
 import ProtectedAreasSidebarCard from 'components/protected-areas-sidebar-card';
 import HumanImpactSidebarCard from 'components/human-impact-sidebar-card';
+import { ReactComponent as CloseIcon } from 'icons/close.svg';
 import animationStyles from 'styles/common-animations.module.scss';
 import styles from './data-global-sidebar-styles.module.scss'
 
@@ -12,7 +14,10 @@ const DataGlobeSidebarComponent = ({
   activeLayers,
   isCountryMode,
   activeCategory,
+  showCloseButton,
   isLandscapeMode,
+  handleOpenSearch,
+  handleCloseSearch,
   isFullscreenActive,
   countedActiveLayers,
   handleGlobeUpdating
@@ -21,6 +26,26 @@ const DataGlobeSidebarComponent = ({
   const sidebarHidden = isCountryMode || isLandscapeMode || isFullscreenActive;
   return (
     <div className={cx(styles.sidebarContainer, {[animationStyles.leftHidden]: sidebarHidden})}>
+      <div
+        className={styles.findPlacesCard}
+        onClick={handleOpenSearch}
+      >
+        <span className={styles.searchCardTitle}>Find places</span>
+        <span className={styles.searchSubtitle}>analyze countries,</span>
+        <span className={styles.searchSubtitle}>explore areas</span>
+        { showCloseButton && (
+          ReactDOM.createPortal(
+            <button
+              className={styles.closeButton}
+              onClick={handleCloseSearch}
+            >
+              <CloseIcon />
+            </button>,
+            document.getElementById('root')
+          )
+        )
+      }
+      </div>
       <BiodiversitySidebarCard
         map={map}
         view={view}
--- src/components/data-global-sidebar/data-global-sidebar.js
+++ src/components/data-global-sidebar/data-global-sidebar.js
@@ -1,3 +1,69 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { openPlacesSearchAnalyticsEvent, searchLocationAnalyticsEvent } from 'actions/google-analytics-actions';
+import urlActions from 'actions/url-actions';
 import Component from './data-global-sidebar-component.jsx';
+import { COUNTRIES_GENERALIZED_BORDERS_FEATURE_LAYER } from 'constants/layers-slugs';
+import { LAYERS_URLS } from 'constants/layers-urls';
+import { LOCAL_SCENE } from 'constants/scenes-constants';
+import countrySceneConfig from 'scenes/country-scene/country-scene-config';
+import { useSearchWidgetLogic } from 'hooks/esri';
 
-export default Component;
+const actions = { openPlacesSearchAnalyticsEvent, searchLocationAnalyticsEvent, ...urlActions };
+
+const SearchWidget = (props) => {
+  const { view, openPlacesSearchAnalyticsEvent, searchLocationAnalyticsEvent, changeGlobe, changeUI } = props;
+
+  const postSearchCallback = ({result}) => {
+    const { feature: { attributes: { GID_0, NAME_0 }}} = result;
+    if (GID_0) {
+        changeGlobe({ countryISO: GID_0, countryName: NAME_0, activeLayers: countrySceneConfig.globe.activeLayers })
+        changeUI({ sceneMode: LOCAL_SCENE })
+    }
+  }
+
+  const searchSources = (FeatureLayer, Locator) => (
+    [
+      {
+        layer: new FeatureLayer({
+          url: LAYERS_URLS[COUNTRIES_GENERALIZED_BORDERS_FEATURE_LAYER],
+          title: COUNTRIES_GENERALIZED_BORDERS_FEATURE_LAYER,
+        }),
+        outFields: ["*"],
+        searchFields: ["GID_0", "NAME_0"],
+        name: "Explore countries",
+        maxSuggestions: 10
+      },
+      {
+        locator: new Locator("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"),
+        singleLineFieldName: "SingleLine",
+        outFields: ["Addr_type"],
+        categories: ['District', 'City', 'Metro Area','Subregion', 'Region', 'Territory', 'Water Features', 'Land Features', 'Nature Reserve'],
+        name: "other geographic results"
+      }
+    ]
+  )
+
+  const searchWidgetConfig = {
+    postSearchCallback,
+    searchSources
+  }
+
+  const { handleOpenSearch, handleCloseSearch, searchWidget } = useSearchWidgetLogic(
+    view,
+    openPlacesSearchAnalyticsEvent,
+    searchLocationAnalyticsEvent,
+    searchWidgetConfig
+  );
+
+  return (
+    <Component
+      handleOpenSearch={handleOpenSearch}
+      handleCloseSearch={handleCloseSearch}
+      showCloseButton={!!searchWidget}
+      {...props}
+    />
+  );
+}
+
+export default connect(null, actions)(SearchWidget);
