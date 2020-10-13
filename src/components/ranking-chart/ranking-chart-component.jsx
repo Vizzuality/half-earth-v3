a68621a376d4a5748734f5c8d04439aa0b34ea2a
javiabia <weberjavi@gmail.com>
2020-10-13 15:44:42 +0200

    Keep ranking headers in view

--- src/components/ranking-chart/ranking-chart-component.jsx
+++ src/components/ranking-chart/ranking-chart-component.jsx
@@ -114,6 +114,24 @@ const RankingChart = ({
         </span>
         <QuestionIcon className={styles.question} onClick={handleInfoClick} />
       </div>
+      <div className={styles.header}>
+        {headers.map((category) => (
+          <HeaderItem
+            title={category.toUpperCase()}
+            key={category}
+            className={cx(styles.headerItem, styles.titleText)}
+            isSortSelected={
+              sortRankingCategory &&
+              sortRankingCategory.split('-')[0] &&
+              sortRankingCategory.split('-')[0] === category.toUpperCase()
+            }
+            sortDirection={
+              sortRankingCategory && sortRankingCategory.split('-')[1]
+            }
+            handleSortClick={handleSortClick}
+          />
+        ))}
+      </div>
       {data && data.length ? (
         <div className={styles.rankingChartContentContainer}>
           <div
@@ -123,24 +141,6 @@ const RankingChart = ({
             onScroll={onScroll}
             ref={tableRef}
           >
-            <div className={styles.header}>
-              {headers.map((category) => (
-                <HeaderItem
-                  title={category.toUpperCase()}
-                  key={category}
-                  className={cx(styles.headerItem, styles.titleText)}
-                  isSortSelected={
-                    sortRankingCategory &&
-                    sortRankingCategory.split('-')[0] &&
-                    sortRankingCategory.split('-')[0] === category.toUpperCase()
-                  }
-                  sortDirection={
-                    sortRankingCategory && sortRankingCategory.split('-')[1]
-                  }
-                  handleSortClick={handleSortClick}
-                />
-              ))}
-            </div>
             <div className={styles.table}>
               {data.map((d) => (
                 <div className={styles.row} key={d.name}>
