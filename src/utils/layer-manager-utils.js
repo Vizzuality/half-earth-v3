3b94320ae7be43b56e26944313ada018d3f9d23d
javiabia <weberjavi@gmail.com>
2020-11-16 11:30:31 +0100

    Create setBasemap util function

--- src/utils/layer-manager-utils.js
+++ src/utils/layer-manager-utils.js
@@ -2,7 +2,7 @@ import { LEGEND_FREE_LAYERS } from 'constants/layers-groups';
 import { intersection } from 'lodash';
 import { loadModules } from 'esri-loader';
 import { addLayerAnalyticsEvent, removeLayerAnalyticsEvent } from 'actions/google-analytics-actions';
-import { DEFAULT_OPACITY, LAYERS_CATEGORIES } from 'constants/mol-layers-configs';
+import { DEFAULT_OPACITY, LAYERS_CATEGORIES, layersConfig} from 'constants/mol-layers-configs';
 
 export const batchLayerManagerToggle = (layerNamesArray, activeLayers, callback, category) => {
   const activeLayersNamesArray = activeLayers ? activeLayers.map(l => l.title) : [];
@@ -154,4 +154,17 @@ export const addActiveLayersToScene = (activeLayers, layersConfig, map) => {
       handleLayerCreation(layerConfig, map);
     });
   }
+}
+
+export const setBasemap = async ({map, surfaceColor, layersArray}) => {
+  map.ground.surfaceColor = surfaceColor || '#0A212E'; // set surface color, before basemap is loaded
+  const baseLayers = await Promise.all(layersArray.map(async layer => await createLayer(layersConfig[layer])));
+  loadModules(["esri/Basemap"]).then(([Basemap]) => {
+    const basemap = new Basemap({
+      baseLayers,
+      title: "half-earth-basemap",
+      id: "half-earth-basemap"
+    });
+    map.basemap = basemap;
+  })
 }
