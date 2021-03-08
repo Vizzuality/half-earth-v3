cdce2510742b736b9589a0544bbaa204929f879b
Alvaro Leal <pursuance@gmail.com>
2021-03-08 12:24:41 +0100

    Sidebar padding changes

--- src/components/category-box/category-box-component.jsx
+++ src/components/category-box/category-box-component.jsx
@@ -8,17 +8,20 @@ import styles from './category-box-styles.module.scss';
 const CategoryBox = ({ title, category, handleBoxClick, counter, isOpen }) => (
   <>
     <div className={styles.box} onClick={handleBoxClick}>
-      <div className={styles.title}>
-        {title}
-        {counter > 0 &&
-          <div className={styles.counter}>
-            <span className={styles.counterText}>{counter}</span>
-          </div>
-        }
-      </div>
       <div className={styles.categoryContainer}>
-        <p className={styles.category}>{category}</p>
-        <div className={cx(styles.icon, { [styles.openBox]: isOpen})}>
+        <img src="#" alt={category} className={styles.categoryThumbnail} />
+        <div className={styles.titleContainer}>
+          <div className={styles.title}>
+            {title}
+            {counter > 0 && (
+              <div className={styles.counter}>
+                <span className={styles.counterText}>{counter}</span>
+              </div>
+            )}
+          </div>
+          <p className={styles.category}>{category}</p>
+        </div>
+        <div className={cx(styles.icon, { [styles.openBox]: isOpen })}>
           <ArrowExpandIcon />
         </div>
       </div>
