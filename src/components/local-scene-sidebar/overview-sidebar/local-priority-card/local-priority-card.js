c48b1ed38a748ab5e31e810928bf5e3abba9e41f
weberjavi <weberjavi@gmail.com>
2021-04-26 08:42:13 +0200

    Move metadata fetching to container

--- src/components/local-scene-sidebar/overview-sidebar/local-priority-card/local-priority-card.js
+++ src/components/local-scene-sidebar/overview-sidebar/local-priority-card/local-priority-card.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import metadataActions from 'redux_modules/metadata';
-import metadataConfig from 'constants/metadata';
 import Component from './local-priority-card-component';
 
+import metadataConfig, {
+  MERGED_PROTECTION,
+  COUNTRY_PRIORITY
+} from 'constants/metadata';
+import metadataService from 'services/metadata-service';
+
 const LocalPriorityCardContainer = props => {
-  const handleInfoClick = slug => {
-    const { setModalMetadata } = props;
-    const md = metadataConfig[slug];
-    setModalMetadata({
-      slug: md.slug,
-      isOpen: true
-    });
-  }
+
+  const [priorityMetadata, setPriorityMetadata] = useState(null);
+  const [protectionMetadata, setProtectionsMetadata] = useState(null);
+
+  useEffect(() => {
+    const md = metadataConfig[MERGED_PROTECTION]
+    metadataService.getMetadata(md.slug).then( data => {
+      setProtectionsMetadata(data);
+    })
+  }, []);
+
+  useEffect(() => {
+    const md = metadataConfig[COUNTRY_PRIORITY]
+    metadataService.getMetadata(md.slug).then( data => {
+      setPriorityMetadata(data);
+    })
+  }, []);
 
   return (
   <Component
-    handleInfoClick={handleInfoClick}
+    priorityMetadata={priorityMetadata}
+    protectionMetadata={protectionMetadata}
     {...props}
   />
   )
