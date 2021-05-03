fc3a310ad2566a4953952ddcba292344b7bed2cf
weberjavi <weberjavi@gmail.com>
2021-05-03 09:41:32 +0200

    Wait until elevation layer resolves to proceed with exageration

--- src/components/terrain-exaggeration-layer/terrain-exaggeration-layer-component.jsx
+++ src/components/terrain-exaggeration-layer/terrain-exaggeration-layer-component.jsx
@@ -3,6 +3,7 @@ import { loadModules } from 'esri-loader';
 
 const exaggeratedElevationLayerComponent = ({ map, exaggeration = 2}) => {
 
+  
   useEffect(() => {
     loadModules(["esri/layers/ElevationLayer", "esri/layers/BaseElevationLayer"]).then(([ElevationLayer, BaseElevationLayer]) => {
       const ExaggeratedElevationLayer = BaseElevationLayer.createSubclass({
@@ -11,13 +12,28 @@ const exaggeratedElevationLayerComponent = ({ map, exaggeration = 2}) => {
           exaggeration
         },
       
-        load: function () {
-          this._elevation = new ElevationLayer({
-            url: "//elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
-          });
-      
-          this.addResolvingPromise(this._elevation.load());
-        },
+          // The load() method is called when the layer is added to the map
+          // prior to it being rendered in the view.
+          load: function () {
+
+            // TopoBathy3D contains elevation values for both land and ocean ground
+            this._elevation = new ElevationLayer({
+              url: "//elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
+            });
+
+            // wait for the elevation layer to load before resolving load()
+            this.addResolvingPromise(
+              this._elevation.load().then(() => {
+                // get tileInfo, spatialReference and fullExtent from the elevation service
+                // this is required for elevation services with a custom spatialReference
+                this.tileInfo = this._elevation.tileInfo;
+                this.spatialReference = this._elevation.spatialReference;
+                this.fullExtent = this._elevation.fullExtent;
+              })
+            );
+
+            return this;
+          },
       
         fetchTile: function (level, row, col) {
           // calls fetchTile() on the elevationlayer for the tiles
