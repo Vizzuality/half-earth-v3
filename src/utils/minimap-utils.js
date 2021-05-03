c21dc6f57cf19742ce848146cc6a1d228fd51feb
weberjavi <weberjavi@gmail.com>
2021-05-03 09:36:54 +0200

    simplify sync function

--- src/utils/minimap-utils.js
+++ src/utils/minimap-utils.js
@@ -1,40 +1,3 @@
-import { loadModules } from 'esri-loader';
-
-// Sample code for disabling interactions with the globe
-// https://developers.arcgis.com/javascript/latest/sample-code/view-disable-zoom/index.html
-export const disableInteractions = view => {
-  view.on("key-down", function(event) {
-    const prohibitedKeys = ["+", "-", "Shift", "_", "="];
-    const keyPressed = event.key;
-    if (prohibitedKeys.indexOf(keyPressed) !== -1) {
-      event.stopPropagation();
-    }
-  });
-
-  view.on("mouse-wheel", function(event) {
-    event.stopPropagation();
-  });
-
-  view.on("double-click", function(event) {
-    event.stopPropagation();
-  });
-
-  view.on("double-click", ["Control"], function(event) {
-    event.stopPropagation();
-  });
-
-  view.on("drag", function(event) {
-    event.stopPropagation();
-  });
-
-  view.on("drag", ["Shift"], function(event) {
-    event.stopPropagation();
-  });
-  
-  view.on("drag", ["Shift", "Control"], function(event) {
-    event.stopPropagation();
-  });
-};
 
 // Sample code about using VectorTileLayers on the globe
 // https://developers.arcgis.com/javascript/latest/sample-code/layers-vectortilelayer-json/index.html
@@ -82,92 +45,10 @@ export const minimapLayerStyles = {
 // Sample code for views synchronization
 // https://developers.arcgis.com/javascript/latest/sample-code/views-synchronize/index.html
 export const synchronizeWebScenes = (globeView, minimapView) => {
-  loadModules(["esri/core/watchUtils"]).then(([watchUtils]) => {
-    const synchronizeView = (view, others) => {
-      others = Array.isArray(others) ? others : [others];
-
-      let viewpointWatchHandle;
-      let viewStationaryHandle;
-      let otherInteractHandlers;
-      let scheduleId;
-
-      const clear = () => {
-        if (otherInteractHandlers) {
-          otherInteractHandlers.forEach(handle => handle.remove());
-        }
-        viewpointWatchHandle && viewpointWatchHandle.remove();
-        viewStationaryHandle && viewStationaryHandle.remove();
-        scheduleId && clearTimeout(scheduleId);
-        otherInteractHandlers = viewpointWatchHandle = viewStationaryHandle = scheduleId = null;
-      };
-
-      const interactWatcher = view.watch("interacting,animation", 
-       newValue => {
-        if (!newValue) return;
-        if (viewpointWatchHandle || scheduleId) return;
-        // start updating the other views at the next frame
-        scheduleId = setTimeout(() => {
-          scheduleId = null;
-          viewpointWatchHandle = view.watch("viewpoint",
-            newValue => {
-              others.forEach((otherView) => {
-                const newAltitude = { max: 12500000, min: 12500000 }
-                newValue.camera.tilt = 0;
-                otherView.viewpoint = newValue;
-                otherView.constraints.altitude = newAltitude;
-                otherView.zoom = 0;
-              });
-          });
-        }, 0);
-
-        // stop as soon as another view starts interacting, like if the user starts panning
-        otherInteractHandlers = others.map((otherView) => {
-          return watchUtils.watch(
-            otherView,
-            "interacting,animation",
-            (value) => { value && clear(); }
-          );
-        });
-
-        // or stop when the view is stationary again
-        viewStationaryHandle = watchUtils.whenTrue(
-          view,
-          "stationary",
-          clear
-        );
-      });
-
-      return {
-        remove: () => {
-          this.remove = () => {};
-          clear();
-          interactWatcher.remove();
-        }
-      };
-    };
-
-    /**
-     * utility method that synchronizes the viewpoints of multiple views
-     */
-    const synchronizeViews = (views) => {
-      let handles = views.map((view, idx, views) => {
-        const others = views.concat();
-        others.splice(idx, 1);
-        return synchronizeView(view, others);
-      });
-
-      return {
-        remove: () => {
-          this.remove = () => {};
-          handles.forEach((h) => {
-            h.remove();
-          });
-          handles = null;
-        }
-      };
-    };
-
-    // bind the views
-    synchronizeViews([globeView, minimapView]);
-  });
+  const sync = (newViewpoint, viewToSync) => {
+    viewToSync.viewpoint = newViewpoint;
+    viewToSync.zoom = 0;
+  };
+  // it returns the handler in case it needs to be removed
+  return globeView.watch("viewpoint", (newViewpoint) => sync(newViewpoint, minimapView));
 };
