3a44b705073322f10b56495619c14f9c60187f56
weberjavi <weberjavi@gmail.com>
2021-05-20 18:44:43 +0200

    Update modal and styles

--- src/components/user-data-modal/user-data-modal-component.jsx
+++ src/components/user-data-modal/user-data-modal-component.jsx
@@ -2,41 +2,28 @@ import React, { useState } from 'react';
 import { Modal } from 'he-components';
 import styles from './user-data-modal-styles.module.scss';
 import { ReactComponent as ScreenIcon } from 'icons/screen.svg';
+import Dropdown from 'components/dropdown';
 
-import { PROFESSIONS, STEP_1 } from 'constants/user-modal-constants';
-
-const RadioButton = ({ text, value, checked, name, onChange }) => {
-  return (
-    <div className={styles.radioButton}>
-      <input
-        type="radio"
-        name={name}
-        id={value}
-        value={value}
-        checked={checked}
-        onChange={onChange}
-        readOnly
-      />
-      <label htmlFor={value} className={styles.radioInput}>
-        {text}
-      </label>
-    </div>
-  )
-}
+import {
+  STEP_1,
+  USER_MODAL_QUESTIONS,
+  DROPDOWN_INITIAL_VALUE
+} from 'constants/user-modal-constants';
 
 const UserDataModalComponent = ({ isModalOpen, handleModalClose, storeFirstStepData, storeSecondStepData, requiredFieldsWarning }) => {
 
   const [activeStep, setActiveStep] = useState(STEP_1);
   const [userId, setUserId] = useState(null);
   const [userData, setUserData] = useState({
-    job_role: '',
-    job_role_other: '',
+    job_role: DROPDOWN_INITIAL_VALUE,
+    job_role_description: '',
+    map_usage: DROPDOWN_INITIAL_VALUE,
+    map_usage_description: '',
     email: '',
-    map_usage: ''
   });
   
   const userDataUpdate = (key, value) => {
-    setUserData({...userData, [key]:value})
+    setUserData({...userData, [key]:value});
   }
 
   const step1 = () => {
@@ -44,39 +31,34 @@ const UserDataModalComponent = ({ isModalOpen, handleModalClose, storeFirstStepD
     <section className={styles.stepContainer}>
       <p className={styles.title}>WELCOME TO THE NEW HALF-EARTH MAP:</p>
       <p className={styles.bodyText}>We are trying to improve your experience on the map. Can you tell us a bit about yourself?</p>
-      <p className={styles.question}>Which is your profession? *
-      {!userData.job_role && requiredFieldsWarning && <span className={styles.requiredWarning}>Required fields are empty!</span>}
-      </p>
-      <div className={styles.radioGroupContainer}>
-        {PROFESSIONS.map(profession => (
-          <RadioButton
-            name={'professions'}
-            key={profession}
-            id={profession}
-            value={profession}
-            text={profession}
-            onChange={(e) => userDataUpdate('job_role', e.target.value)}
-          />
+      <div className={styles.sectionsWrapper}>
+        {USER_MODAL_QUESTIONS.map(q => (
+          <section className={styles.questionSection}>
+            <p className={styles.question}>{q.text}
+              {userData[q.userStateKey].slug === 'placeholder' && q.required && requiredFieldsWarning &&
+                <span className={styles.requiredWarning}>Required fields are empty!</span>
+              }
+            </p>
+                <div className={styles.radioGroupContainer}>
+                  <Dropdown
+                    options={q.dropdownOptions}
+                    handleOptionSelection={(value) => userDataUpdate(q.userStateKey, value)}
+                    selectedOption={q.dropdownOptions.find(o => o.slug === userData[q.userStateKey]) || DROPDOWN_INITIAL_VALUE}
+                    width='full'
+                  />
+                </div>
+                <div className={styles.alternativeAnswerContainer}>
+                  <input
+                    type='text'
+                    className={styles.textInput}
+                    placeholder={q.inputPlaceholder}
+                    onChange={(e) => userDataUpdate(`${q.userStateKey}_description`, e.target.value)}
+                    disabled={userData[q.userStateKey] !== 'other' }
+                  />
+                </div>
+          </section>
         ))}
       </div>
-      <input
-        value={userData.job_role_other}
-        type='text'
-        className={styles.textInput}
-        placeholder={'Write your profession here'}
-        onChange={(event) => userDataUpdate('job_role_other', event.target.value)}
-        disabled={userData.job_role !== 'Other' }
-      />
-      <p className={styles.question}>Please tell us what you use the map for:*
-        {!userData.map_usage && requiredFieldsWarning && <span className={styles.requiredWarning}>Required fields are empty!</span>}
-      </p>
-      <input
-        value={userData.map_usage}
-        type='text'
-        className={styles.textInput}
-        placeholder={'What do you use the map for?'}
-        onChange={(event) => userDataUpdate('map_usage', event.target.value)}
-      />
       <button className={styles.button} onClick={() => storeFirstStepData(userData, setUserId, setActiveStep)}>SEND</button>
       </section>
     )
