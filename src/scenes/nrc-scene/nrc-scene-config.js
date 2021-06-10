90d1a766164cfd9254d0e61a37d005d7c0f83016
weberjavi <weberjavi@gmail.com>
2021-06-10 16:58:46 +0200

    Create dedicated nrc scene

---
+++ src/scenes/nrc-scene/nrc-scene-config.js
@@ -0,0 +1,44 @@
+import {
+  GRAPHIC_LAYER,
+  LANDSCAPE_FEATURES_LABELS_LAYER,
+  CITIES_LABELS_LAYER,
+  MERGED_WDPA_VECTOR_TILE_LAYER,
+  COUNTRY_MASK_LAYER,
+  COUNTRY_PRIORITY_LAYER,
+} from 'constants/layers-slugs';
+
+export default {
+  globe: {
+    activeLayers: [
+      { title: GRAPHIC_LAYER },
+      { title: COUNTRY_MASK_LAYER },
+      { title: CITIES_LABELS_LAYER },
+      { title: LANDSCAPE_FEATURES_LABELS_LAYER },
+      { title: COUNTRY_PRIORITY_LAYER, opacity: 0.4 },
+      { title: MERGED_WDPA_VECTOR_TILE_LAYER, opacity: 0.4 }
+    ],
+    padding: {
+      bottom: 60,
+      left: 300
+    },
+    isGlobeUpdating: false,
+    environment: {
+      background: {
+        type: 'color',
+        color: [0, 0, 0, 0]
+      },
+      starsEnabled: false,
+      atmosphereEnabled: false
+    },
+    ui: {
+      components: []
+    },
+    viewingMode: 'local',
+    constraints: {
+      tilt: {
+        max: 60
+      }
+    }
+  },
+  listeners: false
+};
