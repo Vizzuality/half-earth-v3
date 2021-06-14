fede5fdadc437c5d513fcdc79dc538ee8ab8e300
weberjavi <weberjavi@gmail.com>
2021-06-14 11:38:15 +0200

    Update store folder structure

---
+++ src/constants/pages-ui-defaults.js
@@ -0,0 +1,16 @@
+import {
+  LOCAL_SCENE_TABS_SLUGS,
+  BIODIVERSITY_DEFAULT_TAB
+} from 'constants/ui-params';
+
+export const NRC_UI_DEFAULTS = {
+  sceneMode: 'local',
+  countryChallengesSelectedFilter: 'filter_steward',
+  localSceneActiveTab: LOCAL_SCENE_TABS_SLUGS.OVERVIEW,
+  countryChallengesSelectedKey: 'N_SPECIES',
+  biodiversityLayerVariant: BIODIVERSITY_DEFAULT_TAB,
+  speciesModalSearch: null,
+  speciesModalSort: null,
+  rankingSearch: null,
+  sortRankingCategory: null
+}
