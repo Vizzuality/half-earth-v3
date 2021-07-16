89cb17f5277a6b21ebf07bd91af722e713d84da4
weberjavi <weberjavi@gmail.com>
2021-07-16 16:18:28 +0200

    Add debugging interceptor to requests on esri config

--- src/pages/data-globe/data-globe.js
+++ src/pages/data-globe/data-globe.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { loadModules } from 'esri-loader';
 import { connect } from 'react-redux';
 import { layersConfig } from 'constants/mol-layers-configs';
 import { FIREFLY_BASEMAP_LAYER, SATELLITE_BASEMAP_LAYER } from 'constants/layers-slugs';
@@ -7,9 +8,25 @@ import DataGlobeComponent from './data-globe-component.jsx';
 import { activateLayersOnLoad, setBasemap } from 'utils/layer-manager-utils';
 import * as urlActions from 'actions/url-actions';
 
+import { 
+  SPECIES_RANGE_SERVICE_URL
+ } from 'services/geo-processing-services/species-range-service';
 const actions = {...urlActions};
 
 const DataGlobeContainer = props => {
+
+  useEffect(() => {
+    loadModules(["esri/config"]).then(([esriConfig]) => {
+      esriConfig.request.interceptors.push({
+        // urls: 'https://nowcoast.noaa.gov/',
+        urls: SPECIES_RANGE_SERVICE_URL,
+        before: (params) => {
+          // params.requestOptions.query.f = 'jsapi'
+          console.log('INSIDE INTERCEPTOR', params)
+        }
+      })
+    })
+  },[])
 
   const { changeGlobe } = props;
   const handleGlobeUpdating = (updating) => changeGlobe({ isGlobeUpdating: updating });
