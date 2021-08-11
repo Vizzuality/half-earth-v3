4c0d4a0ddb2eba4c6bf552d5540b5aaaaba6b885
weberjavi <weberjavi@gmail.com>
2021-08-11 12:17:37 +0200

    Add layer counter to the right side of the box

--- src/components/category-box/category-box-component.jsx
+++ src/components/category-box/category-box-component.jsx
@@ -4,7 +4,7 @@ import cx from 'classnames';
 import { ReactComponent as ArrowExpandIcon } from 'icons/arrow_expand.svg';
 import styles from './category-box-styles.module.scss';
 
-const CategoryBox = ({ title, image, category, handleBoxClick, counter, isOpen }) => (
+const CategoryBox = ({ image, category, handleBoxClick, counter, isOpen }) => (
   <>
     <div className={styles.box} onClick={handleBoxClick}>
       <div className={styles.categoryContainer}>
@@ -14,14 +14,11 @@ const CategoryBox = ({ title, image, category, handleBoxClick, counter, isOpen }
           className={styles.categoryThumbnail}
         />
         <div className={styles.titleContainer}>
-          <div className={styles.title}>
-            {title}
-            {counter > 0 && (
-              <div className={styles.counter}>
-                <span className={styles.counterText}>{counter}</span>
-              </div>
-            )}
-          </div>
+          {counter > 0 && (
+            <div className={cx(styles.counter, { [styles.openBox]: isOpen })}>
+              <span className={styles.counterText}>{counter}</span>
+            </div>
+          )}
           <p className={styles.category}>{category}</p>
         </div>
         <div className={cx(styles.icon, { [styles.openBox]: isOpen })}>
@@ -34,14 +31,12 @@ const CategoryBox = ({ title, image, category, handleBoxClick, counter, isOpen }
 
 CategoryBox.propTypes = {
   category: PropTypes.string,
-  title: PropTypes.string,
   isSidebarOpen: PropTypes.bool,
   counter: PropTypes.number
 };
 
 CategoryBox.defaultProps = {
   category: {},
-  title: PropTypes.string,
   isSidebarOpen: true,
   counter: 0
 };
