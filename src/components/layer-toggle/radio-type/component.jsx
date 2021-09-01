0d386f93ff1f28547cb6bc234d1c73dcd6b840bc
weberjavi <weberjavi@gmail.com>
2021-09-01 18:20:46 +0200

    Update radio type component on layer toggle

--- src/components/layer-toggle/radio-type/component.jsx
+++ src/components/layer-toggle/radio-type/component.jsx
@@ -1,71 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { ReactComponent as InfoIcon } from 'icons/info.svg';
-import { ReactComponent as OpacityIcon } from 'icons/opacity.svg';
-import { ReactComponent as BringToBackIcon } from 'icons/bring_to_back.svg';
-import { ReactComponent as BringToFrontIcon } from 'icons/bring_to_front.svg';
 
 import RadioButton from 'components/radio-button';
+import LayerTools from 'components/layer-toggle/layers-tools';
 import styles from './styles.module.scss';
 
 const RadioGroup = ({
-  isChecked,
-  option,
   title,
+  option,
+  variant,
+  onClick,
+  isChecked,
   handleInfoClick,
-  variant
+  onOpacityClick,
+  onBringToBackClick,
+  onBringToFrontClick,
 }) => {
     const key = `radio-button-${title}-${option.value}-${variant}`;
     return (
-      <div
-        key={key}
-        className={cx(styles.radioOption, {
-          [styles.radioOptionSelected]: option.selected
-        })}
-      >
-        <RadioButton
-          name={title}
-          value={option.value}
-          checked={isChecked}
-          id={key}
-          text={option.name}
-        />
+      <div className={cx(
+        styles.container,
+        { [styles.checked]:isChecked}
+        )}>
+        <div
+          key={key}
+          onClick={(e) => onClick(e,option)}
+          className={styles.radioOption}
+        >
+          <RadioButton
+            name={title}
+            value={option.value}
+            checked={isChecked}
+            id={key}
+            text={option.name}
+          />
+        </div>
         {isChecked && (
-          <div className={styles.toggle}>
-            <BringToFrontIcon
-              className={styles.icon}
-              onClick={() => handleInfoClick(option)}
-              data-tip
-              data-for='infoLayerCheckboxButtonId'
-              data-effect='solid'
-              data-delay-show={0}
-            />
-            <BringToBackIcon
-              className={styles.icon}
-              onClick={() => handleInfoClick(option)}
-              data-tip
-              data-for='infoLayerCheckboxButtonId'
-              data-effect='solid'
-              data-delay-show={0}
-            />
-            <OpacityIcon
-              className={styles.icon}
-              onClick={() => handleInfoClick(option)}
-              data-tip
-              data-for='infoLayerCheckboxButtonId'
-              data-effect='solid'
-              data-delay-show={0}
-            />
-            <InfoIcon
-              className={styles.icon}
-              onClick={() => handleInfoClick(option, variant)}
-              data-tip
-              data-for="infoLayerButtonId"
-              data-effect="solid"
-              data-delay-show={0}
-            />
-          </div>
+          <LayerTools 
+            option={option}
+            handleInfoClick={handleInfoClick}
+            onOpacityClick={onOpacityClick}
+            onBringToBackClick={onBringToBackClick}
+            onBringToFrontClick={onBringToFrontClick}
+          />
         )}
       </div>
     );
