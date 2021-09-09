a369fb4160eb11e9514d2f235bfe28c1c0fb8031
weberjavi <weberjavi@gmail.com>
2021-09-09 14:52:54 +0200

    trying to fix runtime eror in production buid

--- src/scenes/data-scene/data-scene.js
+++ src/scenes/data-scene/data-scene.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react/cjs/react.development';
 import { getSelectedAnalysisLayer } from 'utils/analyze-areas-utils';
+import _intersectionBy from 'lodash/intersectionBy';
+import { PRECALCULATED_AOI_OPTIONS } from 'constants/analyze-areas-constants';
 import Component from './data-scene-component';
 
 
@@ -10,10 +12,11 @@ const Container = (props) => {
 
   useEffect(() => {
     if (activeLayers) {
-      const activeOption = getSelectedAnalysisLayer(activeLayers);
-      console.log(getSelectedAnalysisLayer)
-      console.log(activeOption)
-      setSelectedAnalysisLayer(activeOption);
+      const intersectionArray = _intersectionBy(PRECALCULATED_AOI_OPTIONS, activeLayers, 'title');
+      // const activeOption = getSelectedAnalysisLayer(activeLayers);
+      console.log(intersectionArray)
+      // console.log(activeOption)
+      setSelectedAnalysisLayer(intersectionArray[0]);
     }
   }, [activeLayers])
 
