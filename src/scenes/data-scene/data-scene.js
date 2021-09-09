60e8e019fb6d7bb5f26be882f7d21533ad37806d
weberjavi <weberjavi@gmail.com>
2021-09-09 14:57:28 +0200

    import  useEffect from react

--- src/scenes/data-scene/data-scene.js
+++ src/scenes/data-scene/data-scene.js
@@ -1,8 +1,5 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useState, useEffect } from 'react';
 import { getSelectedAnalysisLayer } from 'utils/analyze-areas-utils';
-import _intersectionBy from 'lodash/intersectionBy';
-import { PRECALCULATED_AOI_OPTIONS } from 'constants/analyze-areas-constants';
 import Component from './data-scene-component';
 
 
@@ -11,13 +8,8 @@ const Container = (props) => {
   const [selectedAnalysisLayer, setSelectedAnalysisLayer] = useState();
 
   useEffect(() => {
-    if (activeLayers) {
-      const intersectionArray = _intersectionBy(PRECALCULATED_AOI_OPTIONS, activeLayers, 'title');
-      // const activeOption = getSelectedAnalysisLayer(activeLayers);
-      console.log(intersectionArray)
-      // console.log(activeOption)
-      setSelectedAnalysisLayer(intersectionArray[0]);
-    }
+    const activeOption = getSelectedAnalysisLayer(activeLayers);
+    setSelectedAnalysisLayer(activeOption);
   }, [activeLayers])
 
   return (
