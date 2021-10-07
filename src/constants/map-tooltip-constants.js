3c9c28623940a88853f831229431e73fdbfef415
weberjavi <weberjavi@gmail.com>
2021-10-07 16:24:01 +0200

    remove ecoregions from tooltip

--- src/constants/map-tooltip-constants.js
+++ src/constants/map-tooltip-constants.js
@@ -1,17 +1,10 @@
 import {
-  ECOREGIONS_FEATURE_LAYER,
   ADMIN_AREAS_FEATURE_LAYER,
   WDPA_OECM_FEATURE_LAYER
 } from 'constants/layers-slugs';
 
 
 const MAP_TOOLTIP_CONFIG = {
-  [ECOREGIONS_FEATURE_LAYER] : {
-    title: 'ECO_NAME',
-    subtitle: '',
-    buttonText: 'analyze area',
-    id: 'fid'
-  },
   [ADMIN_AREAS_FEATURE_LAYER] : {
     title: 'NAME_1',
     subtitle: 'NAME_0',
