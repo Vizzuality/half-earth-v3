33af0b8ca88b388ad63b87ccd04a4a8e9192a06f
weberjavi <weberjavi@gmail.com>
2021-10-11 10:39:13 +0200

    Add metadata modal

--- src/pages/aoi/component.jsx
+++ src/pages/aoi/component.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import cx from 'classnames';
+import loadable from '@loadable/component'
 import AreaOfInterestScene from 'scenes/aoi-scene';
 import HalfEarthLogo from 'components/half-earth-logo';
 
 import styles from './styles.module.scss';
 import uiStyles from 'styles/ui.module.scss';
+// Dynamic imports
+const InfoModal = loadable(() => import('components/modal-metadata'));
 
-const AreaOfInterestPageComponent = () => (
+const AreaOfInterestPageComponent = ({hasMetadata}) => (
   <>
     <HalfEarthLogo className={cx(styles.hideOnPrint,uiStyles.halfEarthLogoTopLeft)}/>
     <AreaOfInterestScene />
+    {hasMetadata && <InfoModal />}
   </>
 );
 
--- src/pages/aoi/index.js
+++ src/pages/aoi/index.js
@@ -1,1 +1,10 @@
-export { default } from './component';
+import React from 'react';
+import {connect} from 'react-redux';
+import Component from './component';
+import mapStateToProps from './selectors';
+
+const Container = (props) => (
+  <Component {...props} />
+)
+
+export default connect(mapStateToProps, null)(Container);
