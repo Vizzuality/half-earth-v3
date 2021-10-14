2df2f61eb6172ed6b1bc10ae5d8cbff094d766e9
weberjavi <weberjavi@gmail.com>
2021-10-14 08:07:04 +0200

    Update minimap

--- src/constants/graphic-styles.js
+++ src/constants/graphic-styles.js
@@ -1,9 +1,9 @@
-export const MASK_STYLES = {
+ export const MASK_STYLES = {
   fillColor: [0, 0, 0],
-  fillOpacity: 1,
+  fillOpacity: 0.7,
   outlineColor: [147, 255, 95],
-  outlineOpacity: 0,
-  outlineWidth: 0
+  outlineOpacity: 0.9,
+  outlineWidth: 1,
 }
 
 export const COUNTRY_BORDER_STYLES = {
@@ -11,7 +11,7 @@ export const COUNTRY_BORDER_STYLES = {
   fillOpacity: 0,
   outlineColor: [216, 216, 216],
   outlineOpacity: 1,
-  outlineWidth: 0.5
+  outlineWidth: 1
 }
 
 export const GRID_CELL_STYLES = {
@@ -21,3 +21,54 @@ export const GRID_CELL_STYLES = {
   outlineOpacity: 0.9,
   outlineWidth: 2,
 }
+
+export const MINIMAP_EXTENT_GRAPHIC_STYLES = {
+  fillColor: [0, 0, 0],
+  fillOpacity: 0.3,
+  outlineColor: [24, 186, 180],
+  outlineOpacity: 0.9,
+  outlineWidth: 4,
+}
+
+// Sample code about using VectorTileLayers on the globe
+// https://developers.arcgis.com/javascript/latest/sample-code/layers-vectortilelayer-json/index.html
+export const MINIMAP_BASE_LAYER_STYLES = {
+  style: {
+    layers: [
+      {
+        layout: {},
+        paint: {
+          "fill-color": "#7C95AA"
+        },
+        source: "esri",
+        minzoom: 0,
+        "source-layer": "Land",
+        type: "fill",
+        id: "Land/0"
+      },
+      {
+        layout: {},
+        paint: {
+          "fill-color": "#0A212E"
+        },
+        source: "esri",
+        minzoom: 0,
+        "source-layer": "Marine area",
+        type: "fill",
+        id: "Marine area/1"
+      },
+    ],
+    glyphs:
+      "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer/resources/fonts/{fontstack}/{range}.pbf",
+    version: 8,
+    sprite:
+      "https://www.arcgis.com/sharing/rest/content/items/7675d44bb1e4428aa2c30a9b68f97822/resources/sprites/sprite",
+    sources: {
+      esri: {
+        url:
+          "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer",
+        type: "vector"
+      }
+    } 
+  }
+};
