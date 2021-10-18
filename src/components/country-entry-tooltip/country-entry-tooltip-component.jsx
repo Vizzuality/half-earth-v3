3c6e8004788508eff67541da3fec906830379a8d
weberjavi <weberjavi@gmail.com>
2021-10-18 14:21:23 +0200

    Update component layout and styles

--- src/components/country-entry-tooltip/country-entry-tooltip-component.jsx
+++ src/components/country-entry-tooltip/country-entry-tooltip-component.jsx
@@ -40,14 +40,32 @@ useEffect(() => {
     >
       <section className={styles.tooltipSection}>
         <img className={styles.tooltipFlag} src={`${process.env.PUBLIC_URL}/flags/${countryISO}.svg`} alt="" />
-        <span className={styles.tooltipName}>{countryName}</span>
+        <span className={styles.tooltipName}>Brazil</span>
       </section>
       <CloseIcon className={styles.tooltipClose} onClick={handleTooltipClose}/>
+      <section className={styles.spiInfo}>
+        <p className={styles.spi}>68.95</p>
+        <p className={styles.subtitle}>National species protection index</p>
+      </section>
+      <section className={styles.countryInfo}>
+        <div className={styles.infoPill}>
+          <span className={styles.numeric}>414</span>
+          <span className={styles.text}>land vertebrate species of which 7 are endemic</span>
+        </div>
+        <div className={styles.infoPill}>
+          <span className={styles.numeric}>18%</span>
+          <span className={styles.text}>land is protected</span>
+        </div>
+        <div className={styles.infoPill}>
+          <span className={styles.numeric}>34,16%</span>
+          <span className={styles.text}>of additional land protection is needed</span>
+        </div>
+      </section>
       <button
         className={styles.tooltipExplore}
         onClick={onExploreCountryClick}
       >
-        explore
+        national report card
       </button>
     </div>
   ) : null;
