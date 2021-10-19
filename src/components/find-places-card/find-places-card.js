04785f8436a2f9a51b635549f2bc6ccb9bbaf518
weberjavi <weberjavi@gmail.com>
2021-10-19 07:29:34 +0200

    bring back old search widget hook

--- src/components/find-places-card/find-places-card.js
+++ src/components/find-places-card/find-places-card.js
@@ -3,14 +3,14 @@ import { connect } from 'react-redux';
 import { searchTermsAnalyticsEvent } from 'actions/google-analytics-actions';
 import urlActions from 'actions/url-actions';
 import Component from './find-places-card-component';
-import { useSearchWidgetLogic } from 'hooks/esri';
+import { useSearchWidget } from 'hooks/esri';
 
 const actions = { searchTermsAnalyticsEvent, ...urlActions };
 
 const FindPlacesContainer = (props) => {
   const { view, searchTermsAnalyticsEvent, searchWidgetConfig } = props;
 
-  const { handleOpenSearch, handleCloseSearch, searchWidget } = useSearchWidgetLogic(
+  const { handleOpenSearch, handleCloseSearch, searchWidget } = useSearchWidget(
     view,
     searchTermsAnalyticsEvent,
     searchWidgetConfig
--- src/hooks/esri.js
+++ src/hooks/esri.js
@@ -103,3 +103,78 @@ export const useSearchWidgetLogic = (view, searchTermsAnalyticsEvent, searchWidg
     searchWidget
   }
 }
+
+
+export const useSearchWidget = (view, searchTermsAnalyticsEvent, searchWidgetConfig) => {
+  const [searchWidget, setSearchWidget ] = useState(null);
+  const { searchSources, postSearchCallback} = searchWidgetConfig || {};
+  const keyEscapeEventListener = (evt) => {
+    evt = evt || window.event;
+    if (evt.keyCode === 27 && view && searchWidget) {
+      handleCloseSearch();
+    }
+  };
+
+  const handleOpenSearch = () => {
+    if(searchWidget === null) {
+      setSearchWidget(undefined); // reset search widget in case of multiple quick clicks
+      const container = document.createElement("div");
+      container.setAttribute("id", "searchWidget");
+      loadModules(["esri/widgets/Search", "esri/layers/FeatureLayer", "esri/tasks/Locator"]).then(([Search, FeatureLayer, Locator]) => {
+        const sWidget = new Search({
+          view: view,
+          locationEnabled: true, // do not show the Use current location box when clicking in the input field
+          popupEnabled: false, // hide location popup
+          resultGraphicEnabled: false, // hide location pin
+          container,
+          sources: searchSources(FeatureLayer, Locator),
+          includeDefaultSources: false
+        });
+        setSearchWidget(sWidget);
+      }).catch((err) => console.error(err));
+    }
+  };
+
+  const handleCloseSearch = () => {
+    view.ui.remove(searchWidget);
+    document.removeEventListener('keydown', keyEscapeEventListener);
+    setSearchWidget(null);
+  }
+
+  const handleSearchStart = () => {
+    handleCloseSearch();
+  }
+
+  const addSearchWidgetToView = async () => {
+    await view.ui.add(searchWidget, "top-left");
+    const esriSearch = document.querySelector('#searchWidget');
+    const rootNode = document.getElementById("root");
+    if(esriSearch) {
+      rootNode.appendChild(esriSearch);
+      setTimeout(() => {
+        const input = document.querySelector('.esri-search__input');
+        input && input.focus()
+      }, 300);
+    }
+  }
+
+  useEffect(() => {
+    if( searchWidget ) {
+      addSearchWidgetToView();
+      document.addEventListener('keydown', keyEscapeEventListener);
+      searchWidget.viewModel.on("search-start", handleSearchStart);
+      searchWidget.on('select-result', (event) => postSearchCallback(event));
+      searchWidget.on('suggest-complete', (event) => searchTermsAnalyticsEvent(event.searchTerm));
+    }
+
+    return function cleanUp() {
+      document.removeEventListener('keydown', keyEscapeEventListener);
+    }
+  }, [searchWidget]);
+
+  return {
+    handleOpenSearch,
+    handleCloseSearch,
+    searchWidget
+  }
+}
