19ec082533c24c45004473ccc959f1ba3ed669e4
weberjavi <weberjavi@gmail.com>
2021-11-24 09:16:34 +0100

    Create new analytics categories and actions

--- src/constants/google-analytics-constants.js
+++ src/constants/google-analytics-constants.js
@@ -1,33 +1,37 @@
 export const CATEGORIES = {
-  LAYER_INTERACTION: 'Layer interaction',
-  PROJECT_INFO: 'Project related info lookup',
-  FOLLOW_UP_ACTIONS: 'Follow up actions', // actions that will allow the user to take action outside the app
-  NAVIGATION: 'Navigate between scenes or views'
+  GENERAL_MENU: 'General Menu',
+  DISCOVER_STORIES: 'Discover Stories',
+  EXPLORE_DATA: 'Explore Data',
+  AOI: 'Analyze Area (AOI)',
+  NRC: 'NRC and SPI'
 }
 
 export const ACTIONS = {
-  // LAYER_INTERACTION
-  TOGGLE_LAYER: 'toggle layer',
-  ADD_LAYER: 'add layer',
-  REMOVE_LAYER: 'remove layer',
-  CHANGE_OPACITY: 'Change layer opacity',
-  CHANGE_ORDER: 'Change layers order',
-  OPEN_INFO: 'Open info modal',
-  UPDATE_SETTINGS: 'Update map settings',
-  //PROJECT_INFO
-  OPEN_HALFEARTH_METER: 'Open Half-Earth meter',
-  CONSULT_ABOUT_PAGE: 'Open about page',
-  // FOLLOW_UP_ACTIONS
-  SHARE_MAP: 'Open share modal',
-  CONTRIBUTE_DATA: 'Contribute data to the project',
-  DOWNLOAD_REPORT: 'Download National Report Card PDF',
-  // NAVIGATION
-  ENTER_LANDSCAPE: 'Enter landscape mode',
-  CHANGE_SCENE: 'Navigate to new scene',
-  ENTER_COUNTRY_FROM_TOOLTIP: 'Enter country from map tooltip',
-  ENTER_COUNTRY_FROM_SEARCH: 'Enter country from find places',
-  ENTER_COUNTRY: 'Visit a country card',
-  FIND_PLACES_SEARCH: 'Search term from find places',
-  CHANGE_NRC_VIEW: 'Change NRC view',
-  OPEN_SPECIES_LIST: 'Open All Species list'
+  // GENERAL_MENU
+  SOCIAL: 'Click on "Join the Conversation"',
+  SHARE: 'click on "Share the map"',
+  //DISCOVER_STORIES
+  CLICK_ON_STORY: 'Clicks on story',
+  CHANGE_TAXA: 'Click on change taxa',
+  // EXPLORE_DATA
+  TOGGLE_LAYER: 'Toggle layer',
+  // AOI
+  UPLOAD_SHAPE: 'Upload shape',
+  DRAW_SHAPE: 'Draw shape',
+  OPEN_HISTORY: 'Open AOI history',
+  PRECOMPUTED_AOI: 'Selects predefined geometry',
+  SEARCH_SPECIES: 'Select species in search',
+  TOGGLE_LAYER: 'Toggle layer',
+  SHARE_AOI: 'Share AOI',
+  // NRC
+  CLICK_ON_COUNTRY: 'Click on Country (open map tooltip)',
+  ENTER_NRC: 'Go to "National Report Card"',
+  SEARCH_COUNTRY: 'Type Country in Searchbar',
+  SPECIES_LIST: 'Open All Species list',
+  CHALLENGES: 'Selected challenges view',
+  RANKING: 'Selected ranking view',
+  OVERVIEW: 'Selected map view',
+  DOWNLOAD_PDF: 'Download National Report Card PDF',
+  SPI_INFO: 'Species Protection Index info button clicked',
+  RANKING_INFO: 'Countries ranking info button clicked '
 }
