bbe3bbeef640d41f452b34fa9464ccf6b9fcf002
weberjavi <weberjavi@gmail.com>
2021-12-01 10:53:20 +0100

    Update overview sidebar styles and add pdf analytics

--- src/containers/sidebars/local-scene-sidebar/local-scene-sidebar.js
+++ src/containers/sidebars/local-scene-sidebar/local-scene-sidebar.js
@@ -5,38 +5,24 @@ import Component from './local-scene-sidebar-component';
 import * as urlActions from 'actions/url-actions';
 import metadataActions from 'redux_modules/metadata';
 import countryDataActions from 'redux_modules/country-data';
-import { downloadCountryPdfAnalyticsEvent, selectNRCSectionAnalyticsEvent } from 'actions/google-analytics-actions';
-import { DATA, NATIONAL_REPORT_CARD } from 'router'
+import { NATIONAL_REPORT_CARD_LANDING, NATIONAL_REPORT_CARD } from 'router'
 
-const actions = { ...urlActions, ...countryDataActions, ...metadataActions, downloadCountryPdfAnalyticsEvent };
+const actions = { ...urlActions, ...countryDataActions, ...metadataActions };
 
 const LocalSceneSidebarContainer = (props) => {
   const {
-    scene,
     browsePage,
-    countryName,
-    downloadCountryPdfAnalyticsEvent
+    handlePrintReport,
   } = props;
 
   const handleSceneModeChange = () => {
-    browsePage({ type: DATA, query: { globe: { center: [scene.view.center.longitude, scene.view.center.latitude], zoom: 4}}});
+    browsePage({ type: NATIONAL_REPORT_CARD_LANDING });
   }
 
   const handleTabSelection = slug => {
     const { browsePage, countryISO } = props;
     browsePage({type: NATIONAL_REPORT_CARD, payload: { iso: countryISO, view:  slug }});
-    selectNRCSectionAnalyticsEvent(slug);
   };
-
-  const handlePrintReport = () => {
-    const today = new Date();
-    const date = Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric'}).format(today);
-    const tempTitle = document.title;
-    document.title = `Half-Earth National Report Card ${date} - ${countryName}`;
-    window.print();
-    downloadCountryPdfAnalyticsEvent(countryName)
-    document.title = tempTitle;
-  }
 
   return (
     <Component
--- src/containers/sidebars/local-scene-sidebar/overview-sidebar/index.js
+++ src/containers/sidebars/local-scene-sidebar/overview-sidebar/index.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import Component from './component.jsx';
 import { connect } from 'react-redux';
 import mapStateToProps from 'containers/sidebars/local-scene-sidebar/local-scene-sidebar-selectors';
-import { visitNrcOverviewAnalytics } from 'actions/google-analytics-actions';
-const actions = { visitNrcOverviewAnalytics }
+import { visitNrcOverviewAnalytics, downloadNrcPdfAnalytics } from 'actions/google-analytics-actions';
+const actions = { visitNrcOverviewAnalytics, downloadNrcPdfAnalytics }
 
 const Container = (props) => {
 
@@ -12,7 +12,23 @@ const Container = (props) => {
       visitNrcOverviewAnalytics() 
     }, [])
 
-    return <Component {...props}/>
+    const handlePrintReport = () => {
+      const { downloadNrcPdfAnalytics, countryName } = props;
+      const today = new Date();
+      const date = Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric'}).format(today);
+      const tempTitle = document.title;
+      document.title = `Half-Earth National Report Card ${date} - ${countryName}`;
+      window.print();
+      downloadNrcPdfAnalytics(countryName)
+      document.title = tempTitle;
+    }
+
+    return (
+      <Component
+        handlePrintReport={handlePrintReport}  
+        {...props}
+      />
+    )
 }
 
 export default connect(mapStateToProps, actions)(Container);
