5ebf5fbd263ac52228c2a243e89f38cf64682407
weberjavi <weberjavi@gmail.com>
2021-12-01 08:39:39 +0100

    Add visit overview on nrc analytics

--- src/containers/sidebars/local-scene-sidebar/overview-sidebar/index.js
+++ src/containers/sidebars/local-scene-sidebar/overview-sidebar/index.js
@@ -1,5 +1,16 @@
+import React, { useEffect } from 'react';
 import Component from './component.jsx';
 import { connect } from 'react-redux';
 import mapStateToProps from 'containers/sidebars/local-scene-sidebar/local-scene-sidebar-selectors';
+import { visitNrcOverviewAnalytics } from 'actions/google-analytics-actions';
+const actions = { visitNrcOverviewAnalytics }
 
-export default connect(mapStateToProps, null)(Component);
+const Container = (props) => {
+    useEffect(() => {
+      const { visitNrcOverviewAnalytics } = props;
+      visitNrcOverviewAnalytics() 
+    }, [])
+    return <Component {...props}/>
+}
+
+export default connect(mapStateToProps, actions)(Container);
