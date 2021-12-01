52f2adc931c60684279a4db81e5ad2ee8548c06d
weberjavi <weberjavi@gmail.com>
2021-12-01 08:45:40 +0100

    Add ranking view analytics

--- src/containers/sidebars/local-scene-sidebar/ranking-sidebar/index.js
+++ src/containers/sidebars/local-scene-sidebar/ranking-sidebar/index.js
@@ -1,3 +1,17 @@
+import React, { useEffect } from 'react';
 import Component from './component.jsx';
+import { connect } from 'react-redux';
+import { visitNrcRankingAnalytics } from 'actions/google-analytics-actions';
+const actions = { visitNrcRankingAnalytics }
 
-export default Component;
+const Container = (props) => {
+
+    useEffect(() => {
+      const { visitNrcRankingAnalytics } = props;
+      visitNrcRankingAnalytics() 
+    }, [])
+
+    return <Component {...props}/>
+}
+
+export default connect(null, actions)(Container);
