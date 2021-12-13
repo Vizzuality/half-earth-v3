e2b0843c9c62e4cf494918ca1aad0c9e92f8ae98
weberjavi <weberjavi@gmail.com>
2021-12-13 14:22:02 +0100

    Remove unused forage util function

--- src/utils/local-forage-utils.js
+++ src/utils/local-forage-utils.js
@@ -1,14 +1,5 @@
 import localforage from 'localforage';
 
-export function fetchDataAndUpdateForageItem(itemKey, fetchGpDataFunction, geometry) {
-  return new Promise((resolve, reject) => {
-    fetchGpDataFunction(geometry).then((data) => {
-      writeToForageItem(itemKey, data);
-      resolve(data);
-    }).catch((error) => reject(error))
-  })
-}
-
 export function writeToForageItem(itemKey, newValues) {
   return new Promise((resolve, reject) => {
     localforage.getItem(itemKey).then((storedValues) => {
