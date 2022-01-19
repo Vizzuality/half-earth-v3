f30af242d4181a8475277904b5899ae5dceac0ed
Alvaro leal <pursuance@gmail.com>
2022-01-19 10:16:25 +0100

    Add outline to geometry

--- src/containers/scenes/aoi-scene/component.jsx
+++ src/containers/scenes/aoi-scene/component.jsx
@@ -4,12 +4,12 @@ import Scene from 'components/scene';
 import Widgets from 'containers/widgets';
 import AoiSidebar from 'containers/sidebars/aoi-sidebar';
 import LabelsLayer from 'containers/layers/labels-layer';
-import MaskGraphicLayer from 'containers/layers/mask-graphic-layer';
+import MaskAndOutlineGraphicLayer from 'containers/layers/mask-and-outline-graphic-layer';
 import ArcgisLayerManager from 'containers/managers/arcgis-layer-manager';
 import LocalSceneViewManager from 'containers/managers/local-scene-view-manager';
 import TerrainExaggerationLayer from 'containers/layers/terrain-exaggeration-layer';
 
-const { REACT_APP_ARGISJS_API_VERSION:API_VERSION } = process.env
+const { REACT_APP_ARGISJS_API_VERSION: API_VERSION } = process.env;
 
 const AoiSceneComponent = ({
   geometry,
@@ -19,36 +19,27 @@ const AoiSceneComponent = ({
   activeLayers,
   sceneSettings,
   contextualData,
-}) => (
-  <Scene
-    sceneName={'aoi-scene'}
-    sceneSettings={sceneSettings}
-    loaderOptions={{ url: `https://js.arcgis.com/${API_VERSION}` }}
-    onMapLoad={onMapLoad}
-  >
-    <ArcgisLayerManager
-      userConfig={userConfig}
-      activeLayers={activeLayers}
-    />
-    <MaskGraphicLayer
-      geometry={geometry}
-    />
-    <LocalSceneViewManager
-      localGeometry={geometry}
-    />
-    <Widgets
-      activeLayers={activeLayers}
-    />
-    <TerrainExaggerationLayer /> 
-    <LabelsLayer
-      activeLayers={activeLayers}
-    />
-    <AoiSidebar
-      speciesData={speciesData}
-      activeLayers={activeLayers}
-      contextualData={contextualData}
-    />
-  </Scene>
-);
+}) => {
+  return (
+    <Scene
+      sceneName={'aoi-scene'}
+      sceneSettings={sceneSettings}
+      loaderOptions={{ url: `https://js.arcgis.com/${API_VERSION}` }}
+      onMapLoad={onMapLoad}
+    >
+      <ArcgisLayerManager userConfig={userConfig} activeLayers={activeLayers} />
+      <MaskAndOutlineGraphicLayer geometry={geometry} />
+      <LocalSceneViewManager localGeometry={geometry} />
+      <Widgets activeLayers={activeLayers} />
+      <TerrainExaggerationLayer />
+      <LabelsLayer activeLayers={activeLayers} />
+      <AoiSidebar
+        speciesData={speciesData}
+        activeLayers={activeLayers}
+        contextualData={contextualData}
+      />
+    </Scene>
+  );
+};
 
 export default AoiSceneComponent;
