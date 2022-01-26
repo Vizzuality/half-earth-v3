f8b6575e51799bd046c5d2a85c2c3c46a1678908
Alvaro leal <pursuance@gmail.com>
2022-01-26 13:30:59 +0100

    Linting

--- src/components/source-annotation/component.jsx
+++ src/components/source-annotation/component.jsx
@@ -10,44 +10,46 @@ const Component = ({
   metaDataSources,
   isJSX = false,
 }) => {
-  const lastSource = sources && sources.length -1;
+  const lastSource = sources && sources.length - 1;
   const isMultiSource = sources && sources.length > 1;
   return (
     <section className={className}>
       {metaDataSources && (
-        <cite className={cx(
-          styles.metadataSource,
-          {[styles.light]: theme === 'light'}
-        )}>
+        <cite
+          className={cx(styles.metadataSource, {
+            [styles.light]: theme === 'light',
+          })}
+        >
           {!isJSX && (
             <ReactMarkdown
-              renderers={{link: ({href, children}) => <a href={href} target="_blank" rel="noopener noreferrer">{children[0].props.children}</a>}}
+              renderers={{
+                link: ({ href, children }) => (
+                  <a href={href} target="_blank" rel="noopener noreferrer">
+                    {children[0].props.children}
+                  </a>
+                ),
+              }}
               children={`Source: ${metaDataSources}`}
             />
           )}
           {isJSX && <span>Source: {metaDataSources}</span>}
         </cite>
-        )
-      }
-      {sources && (<span className={styles.sourcesWrapper}>
-        {`Source: `}
-        {sources.map((source, index) => (
-          <>
-          {(isMultiSource && index === lastSource) && <span> and </span> }
-            <cite
-              className={styles.source}
-            >
-              {`${source.label}`}
-            </cite>
-            {index !== lastSource && <span>, </span> }
-            {index === lastSource && <span>.</span> }
-          </>
-        ))
-        }
-      </span>)}
+      )}
+      {sources && (
+        <span className={styles.sourcesWrapper}>
+          {`Source: `}
+          {sources.map((source, index) => (
+            <>
+              {isMultiSource && index === lastSource && <span> and </span>}
+              <cite className={styles.source}>{`${source.label}`}</cite>
+              {index !== lastSource && <span>, </span>}
+              {index === lastSource && <span>.</span>}
+            </>
+          ))}
+        </span>
+      )}
     </section>
-
-  )
-}
+  );
+};
 
 export default Component;
