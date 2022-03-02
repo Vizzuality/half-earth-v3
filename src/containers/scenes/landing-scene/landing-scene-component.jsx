7f54f3b786a30be15dcf37236f1361d87679b234
anamontiaga <amontiaga@gmail.com>
2022-03-02 15:01:35 +0100

    landing: scene

--- src/containers/scenes/landing-scene/landing-scene-component.jsx
+++ src/containers/scenes/landing-scene/landing-scene-component.jsx
@@ -1,53 +1,22 @@
 // Dependencies
 import React, { useMemo } from 'react';
-import cx from 'classnames';
-import loadable from '@loadable/component';
 // Components
 import Scene from 'components/scene';
-import Widgets from 'containers/widgets';
-import ArcgisLayerManager from 'containers/managers/arcgis-layer-manager';
-import CountryLabelsLayer from 'containers/layers/country-labels-layer';
-import FeatureHighlightLayer from 'containers/layers/feature-highlight-layer';
-import MapTooltip from 'components/map-tooltip';
 import MenuFooter from 'components/mobile-only/menu-footer';
-import DataGlobalSidebar from 'containers/sidebars/data-global-sidebar';
 import MenuSettings from 'components/mobile-only/menu-settings';
 // Constants
 import { MobileOnly, useMobile } from 'constants/responsive';
 
-import styles from './data-scene-styles.module.scss';
-import animationStyles from 'styles/common-animations.module.scss';
-
-// Dynamic imports
-const Spinner = loadable(() => import('components/spinner'));
-const LabelsLayer = loadable(() => import('containers/layers/labels-layer'));
-
 const { REACT_APP_ARGISJS_API_VERSION: API_VERSION } = process.env;
 
 const LandingSceneComponent = ({
-  sceneMode,
   onMapLoad,
-  userConfig,
-  countryISO,
-  countryName,
   openedModal,
-  activeLayers,
   activeOption,
   sceneSettings,
   isSidebarOpen,
-  activeCategory,
-  isGlobeUpdating,
-  isFullscreenActive,
-  handleGlobeUpdating,
-  countedActiveLayers,
-  isBiodiversityActive,
-  selectedAnalysisLayer,
-  isLandscapeSidebarCollapsed,
-  handleTooltipActionButtonClick,
-  handleHighlightLayerFeatureClick,
 }) => {
   const isMobile = useMobile();
-  const sidebarHidden = isFullscreenActive || isMobile;
   const updatedSceneSettings = useMemo(
     () => ({ ...sceneSettings, ...(isMobile && { padding: { left: 0 } }) }),
     [isMobile]
@@ -55,31 +24,12 @@ const LandingSceneComponent = ({
   return (
     <>
       <Scene
-        onMapLoad={onMapLoad}
-        sceneName={'data-scene'}
+        // onMapLoad={onMapLoad}
+        sceneName={'landing-scene'}
         sceneSettings={updatedSceneSettings}
         loaderOptions={{ url: `https://js.arcgis.com/${API_VERSION}` }}
-        initialRotation
       >
-        <ArcgisLayerManager
-          userConfig={userConfig}
-          activeLayers={activeLayers}
-        />
-        {isGlobeUpdating && <Spinner floating />}
-        <DataGlobalSidebar
-          activeLayers={activeLayers}
-          activeOption={activeOption}
-          isSidebarOpen={isSidebarOpen}
-          activeCategory={activeCategory}
-          isFullscreenActive={isFullscreenActive}
-          countedActiveLayers={countedActiveLayers}
-          handleGlobeUpdating={handleGlobeUpdating}
-          isBiodiversityActive={isBiodiversityActive}
-          isLandscapeSidebarCollapsed={isLandscapeSidebarCollapsed}
-          className={cx(styles.sidebarContainer, {
-            [animationStyles.leftHidden]: sidebarHidden,
-          })}
-        />
+
         <MobileOnly>
           <MenuFooter
             activeOption={activeOption}
@@ -87,25 +37,7 @@ const LandingSceneComponent = ({
           />
           <MenuSettings activeOption={activeOption} openedModal={openedModal} />
         </MobileOnly>
-        <CountryLabelsLayer
-          sceneMode={sceneMode}
-          countryISO={countryISO}
-          countryName={countryName}
-          activeLayers={activeLayers}
-        />
-        {selectedAnalysisLayer && (
-          <FeatureHighlightLayer
-            featureLayerSlug={selectedAnalysisLayer.slug}
-            onFeatureClick={handleHighlightLayerFeatureClick}
-          />
-        )}
-        <Widgets
-          openedModal={openedModal}
-          activeLayers={activeLayers}
-          isFullscreenActive={isFullscreenActive}
-        />
-        <MapTooltip onActionButtonClick={handleTooltipActionButtonClick} />
-        <LabelsLayer activeLayers={activeLayers} />
+
       </Scene>
     </>
   );
