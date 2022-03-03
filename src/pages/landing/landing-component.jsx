c924d0684ee049f9d19525e9e5fd81da1ef47d87
anamontiaga <amontiaga@gmail.com>
2022-03-03 15:31:51 +0100

    landing: clean non used code from scene

--- src/pages/landing/landing-component.jsx
+++ src/pages/landing/landing-component.jsx
@@ -11,51 +11,21 @@ import uiStyles from 'styles/ui.module.scss';
 const InfoModal = loadable(() => import('components/modal-metadata'));
 
 const LandingComponent = ({
-  sceneMode,
-  countryISO,
-  userConfig,
-  countryName,
   hasMetadata,
   openedModal,
-  activeLayers,
   activeOption,
-  handleMapLoad,
   sceneSettings,
   isSidebarOpen,
-  activeCategory,
-  isGlobeUpdating,
-  selectedSpecies,
-  isFullscreenActive,
-  handleGlobeUpdating,
-  countedActiveLayers,
-  isBiodiversityActive,
-  countryTooltipDisplayFor,
-  isLandscapeSidebarCollapsed,
 }) => {
   return (
     <>
       <HalfEarthLogo className={uiStyles.halfEarthLogoTopLeft} />
       <MainMenu />
       <LandingScene
-        sceneMode={sceneMode}
-        userConfig={userConfig}
-        countryISO={countryISO}
-        countryName={countryName}
         openedModal={openedModal}
         activeOption={activeOption}
-        activeLayers={activeLayers}
         sceneSettings={sceneSettings}
         isSidebarOpen={isSidebarOpen}
-        isBiodiversityActive={isBiodiversityActive}
-        countedActiveLayers={countedActiveLayers}
-        activeCategory={activeCategory}
-        selectedSpecies={selectedSpecies}
-        isGlobeUpdating={isGlobeUpdating}
-        isFullscreenActive={isFullscreenActive}
-        handleGlobeUpdating={handleGlobeUpdating}
-        countryTooltipDisplayFor={countryTooltipDisplayFor}
-        isLandscapeSidebarCollapsed={isLandscapeSidebarCollapsed}
-        onMapLoad={(map) => handleMapLoad(map, activeLayers)}
       />
       {hasMetadata && <InfoModal />}
     </>
