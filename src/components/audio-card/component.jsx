e972d62b2fa86b8c99e182d660fdeafc280eb16c
anamontiaga <amontiaga@gmail.com>
2022-03-03 15:19:44 +0100

    landing audio cards: imporve styles

--- src/components/audio-card/component.jsx
+++ src/components/audio-card/component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import cx from 'classnames';
 
 import styles from './styles.module.scss';
+import { ReactComponent as VolumeControl } from 'icons/volume-control.svg';
+
 
 const Component = ({
   className,
@@ -16,13 +18,20 @@ const Component = ({
     className={cx(className, styles.container)}
   >
     <div className={styles.numberContainer}>
-      <p>{number}.</p>
+      <p className={styles.number}>{number}.</p>
       <img className={styles.gif} src={gif} alt={title} />
     </div>
+
     <div className={styles.content}>
       <p className={styles.title}>{title}</p>
       <p className={styles.description}>{description}</p>
     </div>
+
+    <div className={styles.volumeContainer}>
+      <p>4-7 min</p>
+      <VolumeControl className={styles.volumeIcon} />
+    </div>
+
   </button>
 );
 
