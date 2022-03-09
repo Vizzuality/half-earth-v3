b93ee0b5c73960e22095955508f6711bd5c471b1
Alvaro leal <pursuance@gmail.com>
2022-03-09 20:44:16 +0100

    Add guards

--- src/store/selectors/aoi-selectors.js
+++ src/store/selectors/aoi-selectors.js
@@ -4,7 +4,7 @@ import { selectGlobeUrlState } from 'selectors/location-selectors';
 export const selectAoisGeometries = ({ aoisGeometries }) => aoisGeometries.data;
 export const selectAoiId = ({location}) => location.payload.id;
 export const selectLocationAreaType = ({location}) => location.query && location.query.areaType;
-export const selectAreaTypeSelected = ({ aois }) => aois.areaTypeSelected;
+export const selectAreaTypeSelected = ({ aois }) => aois && aois.areaTypeSelected;
 
 
 export const selectAreaType = createSelector([selectAreaTypeSelected, selectGlobeUrlState],
