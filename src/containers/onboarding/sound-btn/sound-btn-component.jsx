727181fc5cc5dc75a1bb60d0967d0bc5f309f298
anamontiaga <amontiaga@gmail.com>
2022-03-10 13:36:34 +0100

    onboarding: add simple sound with react player

--- src/containers/onboarding/sound-btn/sound-btn-component.jsx
+++ src/containers/onboarding/sound-btn/sound-btn-component.jsx
@@ -1,30 +1,49 @@
 // dependencies
 import { ReactComponent as PlayIcon } from 'icons/play.svg';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
 import migalaSound from 'sounds/migala.mp3';
-import useSound from 'use-sound';
 import styles from './sound-btn-styles.module.scss';
 
 
 const SoundButtonComponent = () => {
-  const [play] = useSound(migalaSound, { interrupt: true });
+  const [playing, setPlaying] = useState(false)
 
   useEffect(() => {
-    play();
-  }, []);
+    setPlaying(true);
+  }, [])
+
 
   return (
     <div className={styles.soundContainer}>
-      <div>
-        x
-      </div>
       <button
-        className={styles.soundBtn}
-        onClick={() => { }}
+        style={{ backgroundColor: 'white' }}
+        className={styles.closeButton}
+        onClick={() => setPlaying(false)}
       >
+        x
+      </button>
+      <button className={styles.soundBtn}>
+
+        <ReactPlayer
+          url={[{ src: migalaSound, type: 'audio/mp3' }]}
+          controls
+          playing={playing}
+          wrapper={"audio"}
+          onPlay={() => setPlaying(true)}
+          onPause={() => setPlaying(false)}
+          config={{
+            file: {
+              attributes: { preload: "auto" },
+              forceAudio: true,
+            },
+          }}
+        />
         <PlayIcon />
-      </button >
-    </div>
+
+      </button>
+
+    </div >
   )
 }
 
