9c1795813b800aa20fb932a7213ee7b673d6f0e0
Alvaro leal <pursuance@gmail.com>
2022-03-16 13:00:57 +0100

    Add StepsArc component

--- src/containers/onboarding/sound-btn/sound-btn-component.jsx
+++ src/containers/onboarding/sound-btn/sound-btn-component.jsx
@@ -1,4 +1,3 @@
-
 import Modal from 'containers/modals/onboarding-modal';
 import { motion } from 'framer-motion';
 import { ReactComponent as CloseIcon } from 'icons/close.svg';
@@ -8,16 +7,16 @@ import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { LANDING } from 'router';
 import migalaSound from 'sounds/migala.mp3';
+import StepsArcs from '../step-arcs';
 import styles from './sound-btn-styles.module.scss';
-
 
 const SoundButtonComponent = ({ browsePage, changeUI }) => {
   const [playing, setPlaying] = useState(false);
-  const [finishModal, setFinishModal] = useState(false)
+  const [finishModal, setFinishModal] = useState(false);
 
   useEffect(() => {
     setPlaying(true);
-  }, [])
+  }, []);
 
   const handleSwitchMode = () => {
     setFinishModal(false);
@@ -35,82 +34,76 @@ const SoundButtonComponent = ({ browsePage, changeUI }) => {
         <DotsIcon />
       </div>
       <div className={styles.rightColumn}>
-        <button
-          className={styles.closeButton}
-          onClick={handleFinishOnBoarding}
-        >
+        <button className={styles.closeButton} onClick={handleFinishOnBoarding}>
           <CloseIcon />
           <p>QUIT</p>
         </button>
         <button className={styles.soundBtn}>
-
           <ReactPlayer
             url={[{ src: migalaSound, type: 'audio/mp3' }]}
             controls
             playing={playing}
-            wrapper={"audio"}
+            wrapper={'audio'}
             onPlay={() => setPlaying(true)}
             onPause={() => setPlaying(false)}
             config={{
               file: {
-                attributes: { preload: "auto" },
+                attributes: { preload: 'auto' },
                 forceAudio: true,
               },
             }}
           />
 
-          {!playing && (
-            <PlayIcon />
-          )}
+          {!playing && <PlayIcon />}
 
           {playing && (
-            <div
-              className={styles.audioBars}
-            >
-              <motion.div
-                className={styles.audioBar}
-                animate={{
-                  scaleY: [1, 3, 1, 1, 3, 1],
-                }}
-                transition={{
-                  duration: 2,
-                  repeat: Infinity,
-                }}
-              />
-              <motion.div
-                className={styles.audioBar}
-                animate={{
-                  scaleY: [3, 1, 3, 1, 1, 3],
-                }}
-                transition={{
-                  duration: 2,
-                  repeat: Infinity,
-                }}
-              />
-              <motion.div
-                className={styles.audioBar}
-                animate={{
-                  scaleY: [1, 3, 1, 1, 3, 1],
-                }}
-                transition={{
-                  duration: 2,
-                  repeat: Infinity,
-                }}
-              />
-            </div>
+            <>
+              <StepsArcs numberOfArcs={5} currentStep={0} />
+              <div className={styles.audioBars}>
+                <motion.div
+                  className={styles.audioBar}
+                  animate={{
+                    scaleY: [1, 3, 1, 1, 3, 1],
+                  }}
+                  transition={{
+                    duration: 2,
+                    repeat: Infinity,
+                  }}
+                />
+                <motion.div
+                  className={styles.audioBar}
+                  animate={{
+                    scaleY: [3, 1, 3, 1, 1, 3],
+                  }}
+                  transition={{
+                    duration: 2,
+                    repeat: Infinity,
+                  }}
+                />
+                <motion.div
+                  className={styles.audioBar}
+                  animate={{
+                    scaleY: [1, 3, 1, 1, 3, 1],
+                  }}
+                  transition={{
+                    duration: 2,
+                    repeat: Infinity,
+                  }}
+                />
+              </div>
+            </>
           )}
-
         </button>
       </div>
       <Modal
         isOpen={finishModal}
-        title='What would you like to do next?'
-        description='You just finished the audio tour you can either go on a new tour or explore the HE map on your own.'
+        title="What would you like to do next?"
+        description="You just finished the audio tour you can either go on a new tour or explore the HE map on your own."
         handleBack={() => browsePage({ type: LANDING })}
         handleClose={handleSwitchMode}
       />
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 export default SoundButtonComponent;
