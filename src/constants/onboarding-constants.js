d826600aac1b2e0977110f256861830fc5eaef5f
anamontiaga <amontiaga@gmail.com>
2022-03-23 20:31:06 +0100

    add text to scripts

--- src/constants/onboarding-constants.js
+++ src/constants/onboarding-constants.js
@@ -7,8 +7,10 @@ export const SCRIPTS = {
       { startTime: 37.3, endTime: 42.2, text: 'potential by exploring the Amazon, an area of incredible biodiversity under great threat.' }
     ],
     priority: [
-      { startTime: 0, endTime: 12.8, text: 'The priority layer identifies the minimum amount of land needed to provide a sufficient amount of habitat for all species represented on the map. In this ranking of priority places, yellow areas correspond to those with higher priority for conservation, as they contribute to safeguarding habitats protecting the most species. Blue tones refer to areas of lower priority values. Areas with no color have not been identified as a part of the global network needed for comprehensive biodiversity protection, but may still contain places of unique and irreplaceable value to biodiversity. In the Amazon, priority areas, like these in the Andes, correspond to the thick yellow stripe on the Western part of the continent, as well as a significant area throughout the Amazon River Basin. Note that our priority models are a starting point of identifying high-impact conservation protection. By changing parameters on the map, different conservation factors shift outcomes. In addition, other parameters involving various socio-economic factors must play a role in specific conservation efforts ultimately undertaken.' },
-      { startTime: 12.8, endTime: 14, text: `To better understand what a priority area is, let's explore two key concepts: Richness and Rarity. Click on the "richness" button to know more.` },
+      { startTime: 0, endTime: 12.8, text: 'The priority layer identifies the minimum amount of land needed to provide a sufficient amount of habitat for all species represented on the' },
+      { startTime: 12.8, endTime: 14, text: 'map. In this ranking of priority places, yellow areas correspond to those with higher priority for conservation, as they contribute to safeguarding' },
+      { startTime: 25.2, endTime: 37.3, text: 'habitats protecting the most species. Blue tones refer to areas of lower priority values. Areas with no color have not been identified as a part of' },
+      { startTime: 37.3, endTime: 42.2, text: 'the global network needed for comprehensive biodiversity protection, but may still contain places of unique and irreplaceable value to biodiversity. In the Amazon, priority areas, like these in the Andes, correspond to the thick yellow stripe on the Western part of the continent, as well as a significant area throughout the Amazon River Basin. Note that our priority models are a starting point of identifying high-impact conservation protection. By changing parameters on the map, different conservation factors shift outcomes. In addition, other parameters involving various socio-economic factors must play a role in specific conservation efforts ultimately undertaken. To better understand what a priority area is, let’s explore two key concepts: Richness and Rarity. Click on the "richness" button to know more' },
     ],
     richness: [
       { startTime: 0, endTime: 12.8, text: 'Richness is simply a measure of the total number of known species identified in a location. The richness layer on the map is a view of how species richness is distributed across the globe. Places in yellow host more species than places colored blue. You can see how the Amazon basin stands out as one of the richest areas in species biodiversity in the region and world.' },
@@ -18,15 +20,41 @@ export const SCRIPTS = {
     rarity: [
       { startTime: 0, endTime: 12.8, text: 'Rarity is a measure of how geographically widespread species in a given region are on average. For example, some species are found only in very restricted areas, such as islands or mountain chains like the Andes. Since their geographical range is so limited, they are rare and also more vulnerable to threats, and protecting such species is a conservation priority.' },
       { startTime: 12.8, endTime: 20, text: 'To get a view of how well places harboring high species richness and rarity are currently protected, click on the Existing Protection tab to activate that map layer.' },
-    ]
+    ],
+    // // CHANGE >> Times below are not correct
+    protection: [
+      { startTime: 0, endTime: 12.8, text: 'Although many ecologically valuable places are already protected, existing protection does not always coincide with the highest species rarity or' },
+      { startTime: 12.8, endTime: 20, text: 'richness. This gap emerges as a possible opportunity for future protection and funding allocation for conservation.In the Amazon region,' },
+      { startTime: 25.2, endTime: 37.3, text: 'observe that large parts of the ecosystems are under protection, including under the form of customary rights granted to indigenous or' },
+      { startTime: 37.3, endTime: 42.2, text: 'local communities, in addition to formal government protection such as national parks. Indigenous and local communities are strong stewards' },
+      { startTime: 42.2, endTime: 50.8, text: 'and defenders of biodiversity, and as such should be recognized as crucial participants in protecting biodiversity. Human encroachment is ongoing' },
+      { startTime: 50.8, endTime: 60, text: 'globally, and can even occur in protected areas. All over the planet, including the Amazon, human-related activities are causing habitat degradation' },
+      { startTime: 50.8, endTime: 60, text: 'and accelerating species extinction. Click on the Human Pressures tab to explore how human activities overlap with natural ecosystems.' },
+    ],
+    'human-pressures': [
+      { startTime: 0, endTime: 12.8, text: 'The layer showing human pressures illustrates how much human encroachment occurs from urbanization and other economic activities. In the Amazon,' },
+      { startTime: 12.8, endTime: 20, text: 'clear-cutting and burning forest for rangeland, along with mining, roads and settlements are the main factors responsible for habitat degradation.' },
+      { startTime: 25.2, endTime: 37.3, text: 'When planning new protected areas or managing existing ones, human pressures should be considered. Some species are less tolerant than others to' },
+      { startTime: 37.3, endTime: 42.2, text: 'human disturbances. However, lands that have already undergone high human modification are more costly to restore.' },
+    ],
+    spi: [
+      { startTime: 0, endTime: 12.8, text: 'The Amazon region spreads across 9 countries. Brazil, for example, is home to a huge portion of biodiversity-rich rainforest, while other nations have' },
+      { startTime: 12.8, endTime: 20, text: 'less. The network of priority protected areas in these regions is therefore a complicated patchwork that necessarily involves diverse' },
+      { startTime: 25.2, endTime: 37.3, text: 'governments, agencies, and communities. The Half-Earth Project has developed a powerful indicator that measures how effective a nation' },
+      { startTime: 37.3, endTime: 42.2, text: 'is in conserving biodiversity in their territory. We call this indicator the Species Protection Index, or SPI. The SPI takes into account the land and' },
+      { startTime: 42.2, endTime: 50.8, text: 'water protected, the needs of rare endemic species, as well as the richness, or total species number, in both protected and unprotected habitats.' },
+      { startTime: 50.8, endTime: 60, text: ' The SPI can be used to measure and compare how well each country is doing in meeting its conservation targets today and for goals going forward. To learn more about the SPI and the' },
+      { startTime: 60, endTime: 70, text: 'specific challenges faced by each country, you can visit the “National Report Card” section, which you can find on the menu, or listen to the tour on this topic.' },
+    ],
+    closure: [
+      { startTime: 0, endTime: 12.8, text: 'There are many paths to slow down the species extinction crisis and safeguard the bulk of Earth’s biodiversity. We invite you to explore' },
+      { startTime: 12.8, endTime: 20, text: 'the Half-Earth Project map on your own or immerse yourself in another of our guided tours.' },
+    ],
   },
   'national-report-cards': {
     intro: [
       { startTime: 0, endTime: 12.8, text: `The Half-Earth Project calls to protect half of the land and sea to safeguard the bulk of Earth's biodiversity and preserve the bulk of species. To achieve this ambitious goal, every nation on earth has an essential role. We’re using a metric we’ve developed, the Species Protection Index or SPI, as the basis for a National Report Card on every country in the world.` },
-      {
-        startTime: 12.8, endTime: 25.2, text: `Report cards make it possible to see the varied conservation opportunities and challenges faced by each country in the world.
-
-Let's see what a report card looks like.` }
+      { startTime: 12.8, endTime: 25.2, text: `Report cards make it possible to see the varied conservation opportunities and challenges faced by each country in the world. Let's see what a report card looks like.` }
     ],
     spi: [
       { startTime: 0, endTime: 12.8, text: `Starting at a global scale, you see a map layer of Species Protection Index values across the world, a visual expression of global biodiversity protection. The SPI provides an estimation of how well each country is meeting conservation targets by considering the amount of land or sea currently protected, the number of rare species, as well as the total number of species found both inside and outside protected areas. Using the accumulation of this data, the National Report Cards provide an answer to the question of how well each country is doing to protect the biodiversity it has stewardship over. On the Half-Earth Project Map, each country in the world is colored by their SPI value. From no color at 100, through progressively lighter shades of purple and red to bright orange at 0. Generally, the SPI is higher in countries effectively managing species diversity and promoting conservation within its borders. A lower SPI score demonstrates a need for more conservation efforts.` },
