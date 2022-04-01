aed13ae4f561c13d0681ac3afbb6a664ec3212ee
anamontiaga <amontiaga@gmail.com>
2022-04-01 21:52:11 +0200

    remove logs

--- src/containers/landing/hero/hero-component.jsx
+++ src/containers/landing/hero/hero-component.jsx
@@ -93,7 +93,7 @@ const HeroComponent = ({ className, changeUI, browsePage }) => {
               browsePage({ type: NATIONAL_REPORT_CARD_LANDING });
               changeUI({
                 onboardingType: 'national-report-cards',
-                onboardingStep: 1,
+                onboardingStep: 0,
               });
             }}
           />
