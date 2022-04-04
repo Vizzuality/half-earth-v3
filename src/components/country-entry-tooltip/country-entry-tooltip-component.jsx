66ef5e4eed3401e414cc0e8cb87ae3a6ab62a62b
Alvaro leal <pursuance@gmail.com>
2022-04-04 12:36:34 +0200

    Remove unneded tooltipPlacement classes

--- src/components/country-entry-tooltip/country-entry-tooltip-component.jsx
+++ src/components/country-entry-tooltip/country-entry-tooltip-component.jsx
@@ -117,7 +117,6 @@ const CountryEntryTooltipComponent = ({
 
       {tooltipScreenPosition && (
         <OnboardingTooltip
-          className={styles.tooltipPlacement}
           tooltipPlacement={{
             top: tooltipScreenPosition.y + (tooltipScreenPosition.height - 55),
             left:
