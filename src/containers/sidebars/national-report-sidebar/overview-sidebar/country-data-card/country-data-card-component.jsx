96be2d561f4e7dcfd50dd7db35a6966f90ec75e1
anamontiaga <amontiaga@gmail.com>
2022-04-22 10:54:56 +0200

    feat: recover old code and set feature flag

--- src/containers/sidebars/national-report-sidebar/overview-sidebar/country-data-card/country-data-card-component.jsx
+++ src/containers/sidebars/national-report-sidebar/overview-sidebar/country-data-card/country-data-card-component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import cx from 'classnames';
 import AreaChart from 'components/charts/area-chart';
-
 import DonutChart from 'components/charts/donut-chart';
 import styles from './country-data-card-styles.module.scss';
 import { ReactComponent as BulbIcon } from 'icons/bulb.svg';
@@ -11,8 +11,11 @@ const CountryDataCardComponent = ({
   countryData,
   currentProtection,
   countryDescription,
-  endemicVertebratesCount
+  endemicVertebratesCount,
+  handleInfoClick,
+  indexStatement,
 }) => {
+  const { REACT_APP_FEATURE_MARINE } = process.env;
   const { prop_protected_mar, nspecies_mar, SPI_mar, total_endemic_mar } = countryData;
   const mockData = [
     {
@@ -61,72 +64,123 @@ const CountryDataCardComponent = ({
       areas: [70, 50]
     }
   ];
+  console.log({ REACT_APP_FEATURE_MARINE })
 
   return (
     <div className={styles.container}>
+      {!REACT_APP_FEATURE_MARINE && (
+        <section className={styles.indexOverview}>
+          <div className={styles.overviewTextWrapper}>
+            <button onClick={handleInfoClick} className={styles.overviewText}>
+              The national species protection index:
+            </button>
+          </div>
+          <div className={styles.indexWrapper}>
+            <div className={styles.indexBar}>
+              <div className={styles.progressMark} style={{ left: `${SPI}%` }} />
+              <div
+                className={styles.improvementArea}
+                style={{ left: `${SPI}%`, width: `${100 - SPI}%` }}
+              />
+            </div>
+            <div className={styles.index}>{`${SPI}`}</div>
+          </div>
+          <p className={styles.indexStatement}>{indexStatement}</p>
+        </section>
+      )}
       <section className={styles.indexWidget}>
         <div className={styles.indexExplanation}>
-          <p className={styles.indexExplanationText}>National Species Protection Index</p>
-          <div className={styles.donutContainer}>
-            <DonutChart
-              chartXPosition={48}
-              chartYPosition={60}
-              colors={["#A24033", "#E9E9E9"]}
-              data={[
-                { name: "SPI Ter", value: SPI },
-                { name: "Rest", value: 100 - SPI }
-              ]}
-              height={130}
-              innerRadius={'80%'}
-              legendXPosition={53}
-              legendYPosition={58}
-              legendValue={SPI}
-              legendText='LAND SPI'
-              outerRadius={'95%'}
-              width={120}
-            />
-            <div>
-              <p className={styles.legendText}>
-                The Land SPI is calculated based on the <b>protected land ({currentProtection}%),</b>
-                the <b>total of vertebrate species ({vertebratesCount})</b> and the amount of which
-                of these are <b>endemic ({endemicVertebratesCount}).</b>
-              </p>
+          <p className={styles.indexExplanationText}>{REACT_APP_FEATURE_MARINE ? 'National Species Protection Index' : 'This index is based on:'}</p>
+          {!REACT_APP_FEATURE_MARINE && (
+            <div className={styles.indexBaseNumbersWrapper}>
+              <div
+                className={cx(
+                  styles.indexBaseDataElement,
+                  styles.protectionNumber
+                )}
+              >
+                <p className={styles.baseNumber}>{`${currentProtection}`}</p>
+                <p className={styles.numberText}>% of land</p>
+                <p className={styles.numberText}>currently protected</p>
+              </div>
+              <div className={styles.indexBaseDataElement}>
+                <p className={styles.baseNumber}>{`${vertebratesCount}`}</p>
+                <p className={styles.numberText}>total land</p>
+                <p className={styles.numberText}>vertebrate species</p>
+              </div>
+              <div className={styles.indexBaseDataElement}>
+                <p
+                  className={styles.baseNumber}
+                >{`${endemicVertebratesCount}`}</p>
+                <p className={styles.numberText}>endemic land</p>
+                <p className={styles.numberText}>vertebrate species</p>
+              </div>
             </div>
-          </div>
-          <div className={styles.donutContainer}>
-            <DonutChart
-              chartXPosition={48}
-              chartYPosition={60}
-              colors={["#FFC01C", "#E9E9E9"]}
-              data={[
-                { name: "SPI Marine", value: SPI_mar },
-                { name: "Rest", value: 100 - SPI_mar }
-              ]}
-              height={130}
-              innerRadius={'80%'}
-              legendXPosition={53}
-              legendYPosition={58}
-              legendValue={SPI_mar}
-              legendText='MARINE SPI'
-              outerRadius={'95%'}
-              width={120}
-            />
-            <div>
-              <p className={styles.legendText}>
-                The Marine SPI is based on the  <b>protected marine areas ({prop_protected_mar}%),</b>
-                the <b>total of marine mammals and fishes ({nspecies_mar})</b> species and the
-                amount of which of these are <b>endemic ({total_endemic_mar}).</b>
-              </p>
-            </div>
-          </div>
+          )}
+          {REACT_APP_FEATURE_MARINE && (
+            <>
+              <div className={styles.donutContainer}>
+                <DonutChart
+                  chartXPosition={48}
+                  chartYPosition={60}
+                  colors={["#A24033", "#E9E9E9"]}
+                  data={[
+                    { name: "SPI Ter", value: SPI },
+                    { name: "Rest", value: 100 - SPI }
+                  ]}
+                  height={130}
+                  innerRadius={'80%'}
+                  legendXPosition={53}
+                  legendYPosition={58}
+                  legendValue={SPI}
+                  legendText='LAND SPI'
+                  outerRadius={'95%'}
+                  width={120}
+                />
+                <div>
+                  <p className={styles.legendText}>
+                    The Land SPI is calculated based on the <b>protected land ({currentProtection}%),</b>
+                    the <b>total of vertebrate species ({vertebratesCount})</b> and the amount of which
+                    of these are <b>endemic ({endemicVertebratesCount}).</b>
+                  </p>
+                </div>
+              </div>
+              <div className={styles.donutContainer}>
+                <DonutChart
+                  chartXPosition={48}
+                  chartYPosition={60}
+                  colors={["#FFC01C", "#E9E9E9"]}
+                  data={[
+                    { name: "SPI Marine", value: SPI_mar },
+                    { name: "Rest", value: 100 - SPI_mar }
+                  ]}
+                  height={130}
+                  innerRadius={'80%'}
+                  legendXPosition={53}
+                  legendYPosition={58}
+                  legendValue={SPI_mar}
+                  legendText='MARINE SPI'
+                  outerRadius={'95%'}
+                  width={120}
+                />
+                <div>
+                  <p className={styles.legendText}>
+                    The Marine SPI is based on the  <b>protected marine areas ({prop_protected_mar}%),</b>
+                    the <b>total of marine mammals and fishes ({nspecies_mar})</b> species and the
+                    amount of which of these are <b>endemic ({total_endemic_mar}).</b>
+                  </p>
+                </div>
+              </div>
 
-          <AreaChart
-            area1={{ key: "species", stroke: "#8884d8" }}
-            area2={{ key: "areas", stroke: "#008f39", fill: "#008f39", fillOpacity: 0.4 }}
-            data={mockData}
-            height={200}
-            width={'100%'}
-          />
+              <AreaChart
+                area1={{ key: "species", stroke: "#8884d8" }}
+                area2={{ key: "areas", stroke: "#008f39", fill: "#008f39", fillOpacity: 0.4 }}
+                data={mockData}
+                height={200}
+                width={'100%'}
+              />
+            </>
+          )}
 
         </div>
         <div className={styles.hint}>
--- src/containers/sidebars/national-report-sidebar/overview-sidebar/country-data-card/country-data-card.js
+++ src/containers/sidebars/national-report-sidebar/overview-sidebar/country-data-card/country-data-card.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import metadataActions from 'redux_modules/metadata';
+import metadataConfig from 'constants/metadata';
 import { checkSpiInfoAnalytics } from 'actions/google-analytics-actions';
+import { SPECIES_PROTECTION_INDEX } from 'constants/metadata';
 
 import Component from './country-data-card-component';
 
@@ -9,8 +11,22 @@ const actions = { checkSpiInfoAnalytics, ...metadataActions }
 
 const CountryDataCardContainer = props => {
 
+  const handleInfoClick = () => {
+    const {
+      setModalMetadata,
+      checkSpiInfoAnalytics
+    } = props;
+    checkSpiInfoAnalytics('Species Protection Index')
+    const md = metadataConfig[SPECIES_PROTECTION_INDEX]
+    setModalMetadata({
+      slug: md.slug,
+      isOpen: true
+    });
+  }
+
   return (
     <Component
+      handleInfoClick={handleInfoClick}
       {...props}
     />
   )
