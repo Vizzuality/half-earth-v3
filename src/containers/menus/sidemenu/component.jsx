c3fc1afd064295e9cc9b9c85837569f6f06bb3d5
anamontiaga <amontiaga@gmail.com>
2022-08-26 16:48:45 +0200

    sidemenu: ui buttons

--- src/containers/menus/sidemenu/component.jsx
+++ src/containers/menus/sidemenu/component.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import cx from 'classnames';
+import { ReactComponent as HelpIcon } from 'icons/help.svg';
+import { ReactComponent as SearchIcon } from 'icons/search-menu.svg';
+import { ReactComponent as ShareIcon } from 'icons/share.svg';
 
 import MinimapWidget from 'containers/menus/sidemenu/minimap-widget';
 import ZoomControls from 'containers/menus/sidemenu/zoom-controls';
 
+import Button from 'components/button';
+
 import { useMobile } from 'constants/responsive';
 
 import uiStyles from 'styles/ui.module.scss';
+
+import styles from './styles.module';
 
 function SideMenuComponent({
   map,
@@ -23,10 +30,16 @@ function SideMenuComponent({
   const hiddenWidget = hidden || isOnMobile;
   return (
     <div
-      className={cx({
+      className={cx(styles.menuContainer, {
         [uiStyles.onboardingOverlay]: typeof onboardingStep === 'number',
       })}
     >
+      <Button
+        Icon={SearchIcon}
+        type="icon-square"
+        className={styles.searchBtn}
+        handleClick={() => console.info('search')}
+      />
       {!hideZoom && (onboardingStep === null || onboardingStep === undefined) && (
         <ZoomControls
           map={map}
@@ -35,6 +48,18 @@ function SideMenuComponent({
           hidden={hiddenWidget}
         />
       )}
+      <Button
+        Icon={HelpIcon}
+        type="icon-square"
+        className={styles.shareBtn}
+        handleClick={() => console.info('help')}
+      />
+      <Button
+        Icon={ShareIcon}
+        type="icon-square"
+        className={styles.shareBtn}
+        // handleClick={() => handleAoiShareToggle(id)}
+      />
       {!hideMiniMap && (
         <MinimapWidget
           map={map}
