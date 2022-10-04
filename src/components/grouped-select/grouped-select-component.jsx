4d43c78b788958f12976579df694d88e77da35b9
Alvaro Leal <pursuance@gmail.com>
2022-10-04 12:46:30 +0200

    Remove uneeded hooks

--- src/components/grouped-select/grouped-select-component.jsx
+++ src/components/grouped-select/grouped-select-component.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unstable-nested-components */
-import React, { useState } from 'react';
+import React from 'react';
 import Select from 'react-select';
 
 import { useT } from '@transifex/react';
