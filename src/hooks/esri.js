a4e7d0fb5f55a1861e9c7e5d717897d49e121f02
Alvaro Leal <pursuance@gmail.com>
2022-10-21 13:44:32 +0200

    Remove logs

--- src/hooks/esri.js
+++ src/hooks/esri.js
@@ -267,7 +267,6 @@ export const useSketchWidget = ({
       });
 
       sketchTool.on('update', (event) => {
-        console.log('update', event);
         const { toolEventInfo, graphics } = event;
         if (graphics && toolEventInfo && toolEventInfo.type.endsWith('-stop')) {
           checkAndMarkValidArea(graphics[0]);
@@ -276,7 +275,6 @@ export const useSketchWidget = ({
       });
 
       sketchTool.on('delete', () => {
-        console.log('deeee');
         setUpdatedGeometry(null);
       });
     }
