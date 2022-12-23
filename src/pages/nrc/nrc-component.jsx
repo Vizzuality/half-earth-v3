e40f63f89b70ab05ded55e23f6827290becc4c79
Alvaro Leal <pursuance@gmail.com>
2022-12-23 12:07:41 +0100

    Create slider effect

--- src/pages/nrc/nrc-component.jsx
+++ src/pages/nrc/nrc-component.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { NATIONAL_REPORT_CARD } from 'router';
 
 import loadable from '@loadable/component';
 
+import { useT } from '@transifex/react';
+
 import cx from 'classnames';
+import { motion } from 'framer-motion';
 
 import { useOnboardingOpenSection } from 'containers/onboarding/onboarding-hooks';
 
+import Button from 'components/button';
 import HalfEarthLogo from 'components/half-earth-logo';
 import NrcContent from 'components/nrc-content';
 import RankingChart from 'components/ranking-chart';
 
 import uiStyles from 'styles/ui.module.scss';
+
+import { ReactComponent as ArrowExpandIcon } from 'icons/arrow_expand.svg';
 
 import nrcBackground from 'images/nrc-background.png';
 
@@ -46,9 +52,10 @@ function NationalReportCard({
     countryISO,
     locationRoute: NATIONAL_REPORT_CARD,
   });
-
+  const t = useT();
   const { marine } = chartData;
   const coastal = !!marine;
+  const [fullRanking, setFullRanking] = useState(false);
 
   return (
     <>
@@ -61,27 +68,48 @@ function NationalReportCard({
       <HalfEarthLogo
         className={cx(styles.hideOnPrint, uiStyles.halfEarthLogoTopLeft)}
       />
-      <div
-        className={cx(styles.hideOnPrint, styles.nrcContentContainer, {
-          [uiStyles.onboardingMode]: !!onboardingType,
-        })}
-      >
-        <NrcContent
-          countryISO={countryISO}
-          countryName={countryName}
-          countryId={countryId}
-        />
-      </div>
-      <div
-        className={cx(styles.hideOnPrint, styles.rankingContainer, {
-          [uiStyles.onboardingMode]: !!onboardingType,
-        })}
-      >
-        <RankingChart
-          coastal={coastal}
-          countryISO={countryISO}
-          className={styles.rankingChart}
-        />
+      <div className={styles.container}>
+        <motion.div
+          initial={{ width: 320 }}
+          animate={{ width: fullRanking ? '70vw' : 320 }}
+          transition={{ duration: 0.5 }}
+          className={cx(styles.hideOnPrint, styles.rankingContainer, {
+            [uiStyles.onboardingMode]: !!onboardingType,
+          })}
+        >
+          <RankingChart
+            coastal={coastal}
+            countryISO={countryISO}
+            className={styles.rankingChart}
+          />
+        </motion.div>
+        <motion.div
+          className={cx(styles.hideOnPrint, styles.nrcContentContainer, {
+            [uiStyles.onboardingMode]: !!onboardingType,
+          })}
+        >
+          <motion.div
+            className={cx(styles.fullRankingButtonContainer, {
+              [styles.fullRanking]: fullRanking,
+            })}
+            initial={{ left: -4 }}
+            animate={{ left: fullRanking ? -26 : -4 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Button
+              type="rectangular-primary"
+              Icon={ArrowExpandIcon}
+              className={styles.fullRankingButton}
+              handleClick={() => setFullRanking(!fullRanking)}
+              label={fullRanking ? t('Default Ranking') : t('Full Ranking')}
+            />
+          </motion.div>
+          <NrcContent
+            countryISO={countryISO}
+            countryName={countryName}
+            countryId={countryId}
+          />
+        </motion.div>
       </div>
       {hasMetadata && <InfoModal />}
     </>
