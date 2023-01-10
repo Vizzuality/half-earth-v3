eaa075ccbe1deac42c987d4337da9f5272bbdcf1
Alvaro Leal <pursuance@gmail.com>
2023-01-10 13:12:19 +0100

    Fix legend and styles

--- src/components/ranking-chart/ranking-chart-constants.js
+++ src/components/ranking-chart/ranking-chart-constants.js
@@ -2,33 +2,41 @@ export const LEGEND_ITEMS = [
   {
     legend: 'Endemic species',
     color: '#F8D300',
-  },
-  {
-    legend: 'Some modification',
-    color: '#B284FD',
-  },
-  {
-    legend: 'Protected areas',
-    color: '#008604',
+    category: 'species',
   },
   {
     legend: 'Non endemic species',
     color: '#F87200',
+    category: 'species',
+  },
+  {
+    legend: 'Some modification',
+    color: '#B284FD',
+    category: 'humanModification',
   },
   {
     legend: 'Very high modification',
     color: '#731CFF',
-  },
-  {
-    legend: 'Protection needed',
-    color: '#B3E74B',
+    category: 'humanModification',
   },
   {
     legend: 'Non-human modification',
     color: '#A0AFB8',
+    category: 'humanModification',
+  },
+  {
+    legend: 'Protected areas',
+    color: '#008604',
+    category: 'protection',
+  },
+  {
+    legend: 'Protection needed',
+    color: '#B3E74B',
+    category: 'protection',
   },
   {
     legend: 'Protection non-needed',
     color: '#AFB8A0',
+    category: 'protection',
   },
 ];
