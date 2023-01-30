23f927b4832dade823feb53fed0b06bf864b9327
Ana Montiaga <amontiaga@gmail.com>
2023-01-30 17:36:12 +0100

    (fix): show endemic marine mammals and fishes on marine (#600)
    * fix endemic container in all devices
    
    * fix tablet styles
    
    * fix tablet styles
    
    * endemic sea mammals on card
    
    * marine mammals icon and improve styles on pdf

--- src/containers/nrc-content/nrc-vertebrates/nrc-vertebrates-component.jsx
+++ src/containers/nrc-content/nrc-vertebrates/nrc-vertebrates-component.jsx
@@ -9,11 +9,14 @@ import cx from 'classnames';
 
 import Button from 'components/button';
 
+import { LAND_MARINE } from 'constants/country-mode-constants';
 import { useMobile } from 'constants/responsive';
 
 import { ReactComponent as AmphibiansIcon } from 'icons/taxa_amphibians.svg';
 import { ReactComponent as BirdsIcon } from 'icons/taxa_birds.svg';
-import { ReactComponent as MammalsIcon } from 'icons/taxa_marine_mammals.svg';
+import { ReactComponent as FishesIcon } from 'icons/taxa_fishes.svg';
+import { ReactComponent as MammalsIcon } from 'icons/taxa_mammals.svg';
+import { ReactComponent as MammalsMarIcon } from 'icons/taxa_marine_mammals.svg';
 import { ReactComponent as ReptilesIcon } from 'icons/taxa_reptiles.svg';
 
 import styles from './nrc-vertebrates-styles.module.scss';
@@ -22,47 +25,66 @@ function NRCVertebrates({
   countryData,
   setNRCSidebarView,
   isShrunken,
+  selectedLandMarineOption,
   setFullRanking,
 }) {
   const t = useT();
   const isMobile = useMobile();
   const locale = useLocale();
 
+  const land = selectedLandMarineOption.slug === LAND_MARINE.land;
+
   const {
     amphibians,
     birds,
+    fishes_mar,
     mammals,
+    mammals_mar,
     reptiles,
     endemic_amphibians,
     endemic_birds,
+    endemic_fishes_mar,
     endemic_mammals,
+    endemic_mammals_mar,
     endemic_reptiles,
   } = countryData || {};
 
   const SPECIES_COMPOSITION = [
     {
       specie: 'amphibians',
-      endemic: endemic_amphibians,
+      endemic: land && endemic_amphibians,
       total: amphibians,
       icon: AmphibiansIcon,
     },
     {
       specie: 'birds',
-      endemic: endemic_birds,
+      endemic: land && endemic_birds,
       total: birds,
       icon: BirdsIcon,
     },
     {
       specie: 'reptiles',
-      endemic: endemic_reptiles,
+      endemic: land && endemic_reptiles,
       total: reptiles,
       icon: ReptilesIcon,
     },
     {
+      specie: 'mammalsMar',
+      endemic: !land && endemic_mammals_mar,
+      total: mammals_mar,
+      icon: MammalsMarIcon,
+    },
+    {
       specie: 'mammals',
-      endemic: endemic_mammals,
+      endemic: land && endemic_mammals,
       total: mammals,
       icon: MammalsIcon,
+    },
+    {
+      specie: 'fishes',
+      endemic: !land && endemic_fishes_mar,
+      total: fishes_mar,
+      icon: FishesIcon,
     },
   ];
 
@@ -82,6 +104,12 @@ function NRCVertebrates({
                 _comment="10 {endemic mammals} of 200"
               />
             ),
+            mammalsMar: (
+              <T
+                _str="endemic sea mammals"
+                _comment="10 {endemic mammals} of 200"
+              />
+            ),
             birds: (
               <T _str="endemic birds" _comment="10 {endemic mammals} of 200" />
             ),
@@ -97,7 +125,11 @@ function NRCVertebrates({
                 _comment="10 {endemic mammals} of 200"
               />
             ),
+            fishes: (
+              <T _str="endemic fishes" _comment="10 {endemic mammals} of 200" />
+            ),
           }[s.specie];
+          if (s.endemic === false) return null;
           return (
             <div className={styles.endemicCard} key={s.specie}>
               <s.icon className={styles.endemicIcon} />
