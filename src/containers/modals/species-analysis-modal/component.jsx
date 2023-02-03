4fca122c354513fb945865222c438eaa0182b70f
Alvaro Leal <pursuance@gmail.com>
2023-02-03 18:37:03 +0100

    Add card inside modal

--- src/containers/modals/species-analysis-modal/component.jsx
+++ src/containers/modals/species-analysis-modal/component.jsx
@@ -6,20 +6,37 @@ import { useT } from '@transifex/react';
 
 import { Modal } from 'he-components';
 
+import SpeciesCard from 'containers/sidebars/aoi-sidebar/species-card/component';
+
 import styles from './styles.module';
 
 import SrsChart from './srs-chart';
 
 const Spinner = loadable(() => import('components/spinner'));
 
-function SpeciesAnalysisModal({ isOpen, handleModalClose, loading }) {
+function SpeciesAnalysisModal({
+  isOpen,
+  handleModalClose,
+  loading,
+  cardProps,
+}) {
   const t = useT();
   return (
     <Modal isOpen={isOpen} onRequestClose={handleModalClose} theme={styles}>
-      <div className={styles.modalContainer}>
-        {loading && <Spinner floating />}
-        <h1>{t('Which species need more protection?')}</h1>
-        <SrsChart width={1000} height={800} />
+      <div className={styles.modalContent}>
+        <div className={styles.cardContainer}>
+          <SpeciesCard {...cardProps} insideModal />
+        </div>
+        <div>
+          {loading ? (
+            <Spinner floating />
+          ) : (
+            <>
+              <h1>{t('Which species need more protection?')}</h1>
+              <SrsChart width={800} height={800} />
+            </>
+          )}
+        </div>
       </div>
     </Modal>
   );
