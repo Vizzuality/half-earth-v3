f6947eb93572063fb527ebe9c4f0a39b472a83dc
Ana Montiaga <amontiaga@gmail.com>
2023-02-09 11:14:13 +0100

    zoom out when aoi is a protected area (#614)

--- src/containers/sidebars/aoi-sidebar/selectors.js
+++ src/containers/sidebars/aoi-sidebar/selectors.js
@@ -2,6 +2,10 @@ import { createStructuredSelector } from 'reselect';
 
 import { getSidebarTabActive } from 'selectors/ui-selectors';
 
+const selectPrecalculatedLayerSlug = ({ location }) =>
+  location.query && location.query.precalculatedLayerSlug;
+
 export default createStructuredSelector({
   sidebarTabActive: getSidebarTabActive,
+  precalculatedLayerSlug: selectPrecalculatedLayerSlug,
 });
