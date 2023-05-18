cc11279fda049b20095326be69b61afbbb3b01ec
Alvaro Leal <pursuance@gmail.com>
2023-05-18 18:25:05 +0200

    Add tooltip with values for species bars

--- src/components/charts/species-bar/component.jsx
+++ src/components/charts/species-bar/component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { T } from '@transifex/react';
+
 import Tooltip from '@tippyjs/react';
 import cx from 'classnames';
 
@@ -7,7 +9,7 @@ import styles from './styles.module.scss';
 
 import { ReactComponent as InfoIcon } from 'icons/infoTooltip.svg';
 
-function Component({
+function SpeciesBar({
   title,
   className,
   percentage,
@@ -22,25 +24,43 @@ function Component({
   } else if (barAnnotation > 82) {
     updatedBarAnnotation = 82;
   }
-  return (
-    <section
-      className={cx(className, styles.container, {
-        [styles.dark]: theme === 'dark',
-      })}
+  const renderTitle = () => (
+    <div className={styles.barTitleWrapper}>
+      <p className={styles.barTitle}>{title}</p>
+      <span className={styles.iconWrapper}>
+        <Tooltip
+          className="light"
+          content={<div className={styles.tooltip}>{tooltipContent}</div>}
+          delay={100}
+          position="bottom"
+        >
+          <InfoIcon className={styles.icon} />
+        </Tooltip>
+      </span>
+    </div>
+  );
+  const renderBar = () => (
+    <Tooltip
+      className="light"
+      content={
+        <div className={styles.tooltip}>
+          {barAnnotation ? (
+            <>
+              <div>
+                <T _str="Protection:" _comment="Protection: 5%" /> {percentage}%
+              </div>
+              <div>
+                <T _str="Target:" _comment="Target: 5%" /> {barAnnotation}%
+              </div>
+            </>
+          ) : (
+            <>{Math.round(percentage)}%</>
+          )}
+        </div>
+      }
+      delay={100}
+      position="bottom"
     >
-      <div className={styles.barTitleWrapper}>
-        <p className={styles.barTitle}>{title}</p>
-        <span className={styles.iconWrapper}>
-          <Tooltip
-            className="light"
-            content={<div className={styles.tooltip}>{tooltipContent}</div>}
-            delay={100}
-            position="bottom"
-          >
-            <InfoIcon className={styles.icon} />
-          </Tooltip>
-        </span>
-      </div>
       <div className={styles.barWrapper}>
         <div className={styles.bar}>
           <div className={styles.value} style={{ width: `${percentage}%` }} />
@@ -56,8 +76,19 @@ function Component({
           />
         </div>
       </div>
+    </Tooltip>
+  );
+
+  return (
+    <section
+      className={cx(className, styles.container, {
+        [styles.dark]: theme === 'dark',
+      })}
+    >
+      {renderTitle()}
+      {renderBar()}
     </section>
   );
 }
 
-export default Component;
+export default SpeciesBar;
