32eae830455bdb5c33ed66f0b5289f46125c2215
Alvaro Leal <pursuance@gmail.com>
2023-06-21 16:16:06 +0200

    Style disclaimer

--- src/containers/onboarding/onboarding-disclaimer/onboarding-disclaimer-component.jsx
+++ src/containers/onboarding/onboarding-disclaimer/onboarding-disclaimer-component.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import { T } from '@transifex/react';
 
+import { ReactComponent as WarningIcon } from 'icons/warning.svg';
+
 import styles from './onboarding-disclaimer-styles.module.scss';
 
 function OnboardingDisclaimer() {
   return (
     <div className={styles.disclaimerContainer}>
       <p className={styles.disclaimer}>
-        <T _str="During the onboarding process, certain features of the platform may not be accessible" />
+        <WarningIcon className={styles.icon} />
+        <span className={styles.text}>
+          <T _str="During the onboarding process, some features may not be working" />
+        </span>
       </p>
     </div>
   );
