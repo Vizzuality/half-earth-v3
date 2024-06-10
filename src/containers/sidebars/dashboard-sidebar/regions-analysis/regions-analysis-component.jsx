115d2a437dd75315c671af343706ef86ebaf7ee6
Chris Manciero <cmanciero@gmail.com>
2024-06-10 14:30:08 -0400

    add regions analysis draft

--- src/containers/sidebars/dashboard-sidebar/regions-analysis/regions-analysis-component.jsx
+++ src/containers/sidebars/dashboard-sidebar/regions-analysis/regions-analysis-component.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
+import Button from 'components/button';
+
+import hrTheme from 'styles/themes/hr-theme.module.scss';
+
+import Checkbox from '../../../../components/checkbox/checkbox-component';
+
 import styles from './regions-analysis-styles.module.scss';
 
 function RegionsAnalysisComponent() {
-  return <section className={styles.container}>Regions Analysis</section>;
+  return (
+    <section className={styles.container}>
+      <span className={styles.sectionTitle}>Regions Analysis</span>
+      <hr className={hrTheme.dark} />
+      <p>
+        Select a region type below to explore a table of regions in which this
+        species is expected to occur.
+      </p>
+      <div className={styles.choices}>
+        <Checkbox
+          option={{ value: 'selectAll', name: 'Select/deselect All' }}
+        />
+        <Checkbox
+          option={{ value: 'protectedAreas', name: 'Protected Areas' }}
+        />
+        <Checkbox
+          option={{
+            value: 'proposedProtectedAreas',
+            name: 'Proposed Protected Areas',
+          }}
+        />
+        <Checkbox option={{ value: 'Provinces', name: 'Provinces' }} />
+        <Checkbox option={{ value: 'priorityAreas', name: 'Priority Areas' }} />
+        <Checkbox
+          option={{ value: 'Community Forests', name: 'Community Forests' }}
+        />
+      </div>
+      <div className={styles.search}>
+        <input type="text" placeholder="Search for a Region" />
+        <Button
+          type="rectangular"
+          className={styles.saveButton}
+          label="download data"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default RegionsAnalysisComponent;
