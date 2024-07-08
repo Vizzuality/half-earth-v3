3c3d2850ef68b010b15e763f519b7c5ea7267a73
Alvaro <pursuance@gmail.com>
2024-07-08 12:25:25 +0200

    Add metadata and aoiGeometries as needed st

--- src/store/redux-modules/aois-geometries/index.js
+++ src/store/redux-modules/aois-geometries/index.js
@@ -1,10 +1,7 @@
-import reducerRegistry from 'reducerRegistry';
-
 import * as actions from './actions';
 import reducers, { initialState } from './reducers';
 
-const reduxConfig = { actions, reducers, initialState };
+export const reduxConfig = { actions, reducers, initialState };
 
-reducerRegistry.registerModule('aoisGeometries', reduxConfig);
 
 export default actions;
--- src/store/redux-modules/metadata/metadata.js
+++ src/store/redux-modules/metadata/metadata.js
@@ -1,10 +1,6 @@
-import reducerRegistry from 'reducerRegistry';
-
 import * as actions from './metadata-actions';
 import reducers, { initialState } from './metadata-reducers';
 
 export const reduxConfig = { actions, reducers, initialState };
-
-reducerRegistry.registerModule('metadata', reduxConfig);
 
 export default actions;
