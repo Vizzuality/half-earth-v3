b650a30a7fb3a4eea1e363e69e3a886963d48abd
Chris Manciero <cmanciero@gmail.com>
2024-09-16 13:10:34 -0400

    regional analysis radio buttons

--- src/containers/sidebars/dashboard-sidebar/regions-analysis/regions-analysis-component.jsx
+++ src/containers/sidebars/dashboard-sidebar/regions-analysis/regions-analysis-component.jsx
@@ -1,6 +1,10 @@
 import React, { useContext } from 'react';
 import cx from 'classnames';
 import Button from 'components/button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 import SearchLocation from 'components/search-location';
 
 import { SEARCH_TYPES } from 'constants/search-location-constants';
@@ -11,37 +15,31 @@ import Checkbox from '../../../../components/checkbox/checkbox-component';
 
 import styles from './regions-analysis-styles.module.scss';
 import { LightModeContext } from '../../../../context/light-mode';
+import { useT } from '@transifex/react';
 
 function RegionsAnalysisComponent(props) {
+  const t = useT();
   const { view, selectedOption } = props;
   const { lightMode } = useContext(LightModeContext);
 
   return (
     <section className={cx(lightMode ? styles.light : '', styles.container)}>
-      <span className={styles.sectionTitle}>Regions Analysis</span>
+      <span className={styles.sectionTitle}>{t('Regions Analysis')}</span>
       <hr className={hrTheme.dark} />
       <p>
-        Select a region type below to explore a table of regions in which this
-        species is expected to occur.
+        {t('Select a region type below to explore a table of regions in which this species is expected to occur.')}
       </p>
       <div className={styles.choices}>
-        <Checkbox
-          option={{ value: 'selectAll', name: 'Select/deselect All' }}
-        />
-        <Checkbox
-          option={{ value: 'protectedAreas', name: 'Protected Areas' }}
-        />
-        <Checkbox
-          option={{
-            value: 'proposedProtectedAreas',
-            name: 'Proposed Protected Areas',
-          }}
-        />
-        <Checkbox option={{ value: 'Provinces', name: 'Provinces' }} />
-        <Checkbox option={{ value: 'priorityAreas', name: 'Priority Areas' }} />
-        <Checkbox
-          option={{ value: 'Community Forests', name: 'Community Forests' }}
-        />
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value="protectedAreas" control={<Radio />} label={t('Protected Areas')} />
+          <FormControlLabel value="proposedProtectedAreas" control={<Radio />} label={t('Proposed Protected Areas')} />
+          <FormControlLabel value="provinces" control={<Radio />} label={t('Provinces')} />
+          <FormControlLabel value="priorityAreas" control={<Radio />} label={t('Priority Areas')} />
+          <FormControlLabel value="communityForests" control={<Radio />} label={t('Community Forests')} />
+        </RadioGroup>
       </div>
       <div className={styles.search}>
         <SearchLocation
