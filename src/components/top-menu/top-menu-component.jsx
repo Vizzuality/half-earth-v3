966900218c68f43d9727125a70c1c47a28e42274
Chris Manciero <cmanciero@gmail.com>
2024-09-20 11:35:40 -0400

    add bar

--- src/components/top-menu/top-menu-component.jsx
+++ src/components/top-menu/top-menu-component.jsx
@@ -5,6 +5,7 @@ function TopMenuComponent(props) {
   return (
     <div className={styles.container}>
       <a href="#" target='_blank'>About Us</a>
+      <span>|</span>
       <a href="#">Login</a>
     </div>
   )
