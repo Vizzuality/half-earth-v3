a51979e7459aa9920aef5938abee03a387544278
Chris Manciero <cmanciero@gmail.com>
2024-10-20 11:49:13 -0400

    species name added

--- src/containers/sidebars/dashboard-sidebar/biodiversity-indicators/biodiversity-indicators-component.jsx
+++ src/containers/sidebars/dashboard-sidebar/biodiversity-indicators/biodiversity-indicators-component.jsx
@@ -22,6 +22,7 @@ function BioDiversityComponent(props) {
     globalProtectionScore,
     protectionTableData,
     protectionArea,
+    speciesInfo,
     globalProtectionArea } = props
 
   const [isLoading, setIsLoading] = useState(true);
@@ -36,6 +37,7 @@ function BioDiversityComponent(props) {
     <section className={cx(lightMode ? styles.light : '', styles.container)}>
       <span className={styles.sectionTitle}>{t('Biodiversity Indicators')}</span>
       <hr className={hrTheme.dark} />
+      <span className={styles.speciesName}>{speciesInfo.scientificname}</span>
       {isLoading && <Loading height={200} />}
       {!isLoading && <>
         <div className={styles.tabs}>
