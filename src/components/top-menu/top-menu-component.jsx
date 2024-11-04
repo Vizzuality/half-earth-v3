a85cd50495cb4ac86cafd872435dfef18eed5154
Chris Manciero <cmanciero@gmail.com>
2024-11-04 15:57:24 -0500

    login and logout

--- src/components/top-menu/top-menu-component.jsx
+++ src/components/top-menu/top-menu-component.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import styles from './top-menu-styles.module.scss';
+import IdentityManager from '@arcgis/core/identity/IdentityManager';
 import { useT } from '@transifex/react';
 
 function TopMenuComponent(props) {
   const t = useT();
-  const { user } = props;
+  const { user, setLoggedIn } = props;
+
+  const logOut = () => {
+    IdentityManager.destroyCredentials();
+    setLoggedIn(false);
+  }
 
   const UserProfile = () => {
-    return (
+    return (<>
       <a href="https://eowilson.maps.arcgis.com/home/index.html" target="_blank" className={styles.profile}>
         <img src={user.thumbnailUrl} width="30px" height="30px" />
       </a>
+      <span>|</span>
+      <button onClick={logOut}>Logout</button>
+    </>
     )
   }
   return (
