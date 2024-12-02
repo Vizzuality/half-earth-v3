5fe6c321df34eadfa762b586c48c7b20bf4c062d
Chris Manciero <cmanciero@gmail.com>
2024-12-02 15:26:55 -0500

    updates

--- src/components/dashboard-nav/dashboard-nav-component.jsx
+++ src/components/dashboard-nav/dashboard-nav-component.jsx
@@ -1,22 +1,27 @@
-import React, { useContext } from 'react'
-import styles from './dashboard-nav-styles.module.scss'
-import StacksIcon from 'icons/stacks.svg?react'
-import OkapiIcon from 'icons/okapi.svg?react'
-import TimeLineIcon from 'icons/timeline.svg?react'
-import HomeIcon from 'icons/house-solid.svg?react'
-import ElephantIcon from 'icons/elephant.svg?react'
-import DuikerIcon from 'icons/duiker_icon_black.svg?react'
-import SouthAmericaIcon from '@mui/icons-material/SouthAmerica'
-import WhatshotIcon from '@mui/icons-material/Whatshot'
-import { useT } from '@transifex/react'
-import cx from 'classnames'
-import { LightModeContext } from '../../context/light-mode'
-import { NAVIGATION } from '../../utils/dashboard-utils'
+import React, { useContext } from 'react';
+
+import { useT } from '@transifex/react';
+
+import SouthAmericaIcon from '@mui/icons-material/SouthAmerica';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import cx from 'classnames';
+
+import DuikerIcon from 'icons/duiker_icon_black.svg?react';
+import ElephantIcon from 'icons/elephant.svg?react';
+import HomeIcon from 'icons/house-solid.svg?react';
+import OkapiIcon from 'icons/okapi.svg?react';
+import StacksIcon from 'icons/stacks.svg?react';
+import TimeLineIcon from 'icons/timeline.svg?react';
+
+import { LightModeContext } from '../../context/light-mode';
+import { NAVIGATION } from '../../utils/dashboard-utils';
+
+import styles from './dashboard-nav-styles.module.scss';
 
 function DashboardNavComponent(props) {
-  const t = useT()
-  const { selectedIndex, setSelectedIndex, scientificName, countryISO } = props
-  const { lightMode, } = useContext(LightModeContext)
+  const t = useT();
+  const { selectedIndex, setSelectedIndex, scientificName, countryISO } = props;
+  const { lightMode } = useContext(LightModeContext);
 
   const titles = {
     HOME: 'home',
@@ -24,13 +29,13 @@ function DashboardNavComponent(props) {
     DATA_LAYER: 'data-layer',
     BIO_DIVERSITY: 'bio-diversity',
     REGION_ANALYSIS: 'region-analysis',
-    TRENDS: 'trends'
-  }
+    TRENDS: 'trends',
+  };
 
   const updateHistory = (page, title) => {
-    window.history.pushState({ selectedIndex: page }, '', ``)
-    setSelectedIndex(page)
-  }
+    window.history.pushState({ selectedIndex: page }, '', ``);
+    setSelectedIndex(page);
+  };
 
   return (
     <section className={cx(lightMode ? styles.light : '', styles.sidenav)}>
@@ -59,45 +64,61 @@ function DashboardNavComponent(props) {
           type="button"
           aria-label={t('Species')}
           className={cx({
-            [styles.selected]: selectedIndex >= NAVIGATION.SPECIES && selectedIndex <= NAVIGATION.REGION_ANALYSIS,
+            [styles.selected]:
+              selectedIndex >= NAVIGATION.SPECIES &&
+              selectedIndex <= NAVIGATION.REGION_ANALYSIS,
           })}
           onClick={() => {
-            updateHistory(NAVIGATION.DATA_LAYER, titles.DATA_LAYER)
+            updateHistory(NAVIGATION.DATA_LAYER, titles.DATA_LAYER);
           }}
         >
           {countryISO === 'COD' && <OkapiIcon className={styles.speciesIcon} />}
-          {countryISO === 'LBR' && <DuikerIcon className={styles.speciesIcon} />}
-          {countryISO === 'GIN' && <ElephantIcon className={styles.speciesIcon} />}
-          {countryISO === 'SLE' && <ElephantIcon className={styles.speciesIcon} />}
-          {countryISO === 'GUY' && <ElephantIcon className={styles.speciesIcon} />}
+          {countryISO === 'LBR' && (
+            <DuikerIcon className={styles.speciesIcon} />
+          )}
+          {countryISO === 'GIN' && (
+            <ElephantIcon className={styles.speciesIcon} />
+          )}
+          {countryISO === 'SLE' && (
+            <ElephantIcon className={styles.speciesIcon} />
+          )}
+          {countryISO === 'GUY' && (
+            <ElephantIcon className={styles.speciesIcon} />
+          )}
         </button>
-        {selectedIndex >= NAVIGATION.SPECIES && selectedIndex <= NAVIGATION.REGION_ANALYSIS && <div className={styles.subNav}>
-          <button
-            type="button"
-            disabled={!scientificName}
-            aria-label={t('Data Analysis')}
-            className={cx({
-              [styles.selected]: selectedIndex === NAVIGATION.DATA_LAYER,
-              [styles.disabled]: !scientificName
-            })}
-            onClick={() => updateHistory(NAVIGATION.DATA_LAYER, titles.DATA_LAYER)}
-          >
-            <StacksIcon />
-          </button>
-          <button
-            type="button"
-            disabled={!scientificName}
-            aria-label={t('Biodiversity Indicators')}
-            className={cx({
-              [styles.selected]: selectedIndex === NAVIGATION.BIO_IND,
-              [styles.disabled]: !scientificName
-            })}
-            onClick={() => updateHistory(NAVIGATION.BIO_IND, titles.BIO_DIVERSITY)}
-          >
-            <TimeLineIcon />
-          </button>
-        </div>
-        }
+        {selectedIndex >= NAVIGATION.SPECIES &&
+          selectedIndex <= NAVIGATION.REGION_ANALYSIS && (
+            <div className={styles.subNav}>
+              <button
+                type="button"
+                disabled={!scientificName}
+                aria-label={t('Data Analysis')}
+                className={cx({
+                  [styles.selected]: selectedIndex === NAVIGATION.DATA_LAYER,
+                  [styles.disabled]: !scientificName,
+                })}
+                onClick={() =>
+                  updateHistory(NAVIGATION.DATA_LAYER, titles.DATA_LAYER)
+                }
+              >
+                <StacksIcon />
+              </button>
+              <button
+                type="button"
+                disabled={!scientificName}
+                aria-label={t('Biodiversity Indicators')}
+                className={cx({
+                  [styles.selected]: selectedIndex === NAVIGATION.BIO_IND,
+                  [styles.disabled]: !scientificName,
+                })}
+                onClick={() =>
+                  updateHistory(NAVIGATION.BIO_IND, titles.BIO_DIVERSITY)
+                }
+              >
+                <TimeLineIcon />
+              </button>
+            </div>
+          )}
         <button
           type="button"
           aria-label={t('Trends')}
@@ -110,7 +131,7 @@ function DashboardNavComponent(props) {
         </button>
       </div>
     </section>
-  )
+  );
 }
 
-export default DashboardNavComponent
+export default DashboardNavComponent;
