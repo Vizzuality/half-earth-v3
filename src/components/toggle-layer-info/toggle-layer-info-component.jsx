102a9c68e2c81575b811e71cfbd0d8334ad5406d
Chris Manciero <cmanciero@gmail.com>
2025-01-24 07:25:45 -0500

    update layer toggle

--- src/components/toggle-layer-info/toggle-layer-info-component.jsx
+++ src/components/toggle-layer-info/toggle-layer-info-component.jsx
@@ -13,7 +13,12 @@ function ToggleLayerInfoComponent(props) {
   const parentLayers = [
     LAYER_OPTIONS.EXPERT_RANGE_MAPS,
     LAYER_OPTIONS.POINT_OBSERVATIONS,
+  ];
+
+  const customLayers = [
     LAYER_OPTIONS.HABITAT,
+    LAYER_OPTIONS.PROTECTED_AREAS,
+    LAYER_OPTIONS.ADMINISTRATIVE_LAYERS,
   ];
 
   // display info for each layer
@@ -21,38 +26,77 @@ function ToggleLayerInfoComponent(props) {
     let parent = false;
     let url = `${DASHBOARD_URLS.DATASET_LAYER_INFO}${item.dataset_id}`;
 
-    if (parentLayers.includes(item.id)) {
-      url = `${DASHBOARD_URLS.DATASET_LAYER_GROUP_INFO}`;
-    }
+    if (customLayers.includes(item.id)) {
+      let data;
+      if (item.id === LAYER_OPTIONS.HABITAT) {
+        data = [
+          {
+            label: 'Description',
+            value: t(
+              'The habitat loss/gain map is developed from the species expert range map, species habitat preference data, and annually-updated environmental and land cover data. These maps use the year 2001 as a baseline to determine a species total suitable habitat area, where suitable habitat is based on the application of habitat preferences and environmental/land cover data to the expert range map, and track what areas of the range have been lost or regained since 2001.'
+            ),
+          },
+        ];
+      } else if (item.id === LAYER_OPTIONS.PROTECTED_AREAS) {
+        data = [
+          {
+            label: 'Description',
+            value: t(
+              'From the World Database of Protected Areas (protectedplant.net).'
+            ),
+          },
+        ];
+      } else if (item.id === LAYER_OPTIONS.ADMINISTRATIVE_LAYERS) {
+        data = [
+          {
+            label: 'Description',
+            value: t(
+              'From the Database of Global Administrative Areas (gadm.org).'
+            ),
+          },
+        ];
+      }
 
-    if (item.id === LAYER_OPTIONS.EXPERT_RANGE_MAPS) {
-      url += `?id=range`;
-      parent = true;
-    } else if (item.id === LAYER_OPTIONS.POINT_OBSERVATIONS) {
-      url += `?id=points`;
-      parent = true;
-    } else if (item.id === LAYER_OPTIONS.HABITAT) {
-      url += `?id=points`;
-      parent = true;
-    }
-    // DATASET_LAYER_GROUP_INFO
-    const response = await fetch(url);
-    const data = await response.json();
-    if (parent) {
       setLayerInfo({ info: data, title: item.label });
     } else {
-      setLayerInfo({ info: data.metadata, title: item.label });
+      if (parentLayers.includes(item.id)) {
+        url = `${DASHBOARD_URLS.DATASET_LAYER_GROUP_INFO}`;
+      }
+
+      if (item.id === LAYER_OPTIONS.EXPERT_RANGE_MAPS) {
+        url += `?id=range`;
+        parent = true;
+      } else if (item.id === LAYER_OPTIONS.POINT_OBSERVATIONS) {
+        url += `?id=points`;
+        parent = true;
+      } else if (item.id === LAYER_OPTIONS.HABITAT) {
+        url += `?id=points`;
+        parent = true;
+      }
+      // DATASET_LAYER_GROUP_INFO
+      const response = await fetch(url);
+      const data = await response.json();
+      if (parent) {
+        setLayerInfo({ info: data, title: item.label });
+      } else {
+        setLayerInfo({ info: data.metadata, title: item.label });
+      }
     }
   };
   return (
-    <button
-      type="button"
-      onClick={() => displayInfo(layer)}
-      aria-label="Toggle info visibility"
-      title={t('Info and metadata')}
-    >
-      <InfoIcon />
-    </button>
+    <>
+      {layer.type !== 'PRIVATE' && (
+        <button
+          type="button"
+          onClick={() => displayInfo(layer)}
+          aria-label="Toggle info visibility"
+          title={t('Info and metadata')}
+        >
+          <InfoIcon />
+        </button>
+      )}
+      {layer.type === 'PRIVATE' && <span />}
+    </>
   );
 }
 
