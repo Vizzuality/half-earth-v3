c354983f2a5f71e9470df5918143da3c89d85a99
Chris Manciero <cmanciero@gmail.com>
2025-01-31 13:15:25 -0500

    scrolling highlighting

--- src/containers/sidebars/dashboard-sidebar/tutorials/tutorials-component.jsx
+++ src/containers/sidebars/dashboard-sidebar/tutorials/tutorials-component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 
 import { useT } from '@transifex/react';
 
@@ -16,13 +16,50 @@ import styles from './tutorials-components-styles.module.scss';
 
 function TutorialsComponent() {
   const t = useT();
+  const divRef = useRef(null);
   const { lightMode } = useContext(LightModeContext);
   const [selectedSection, setSelectedSection] = useState();
 
   const goToSection = (section) => {
+    setSelectedSection(section);
     const sectionElement = document.getElementById(section);
     sectionElement.scrollIntoView({ behavior: 'smooth' });
   };
+
+  useEffect(() => {
+    const div = divRef.current;
+
+    if (div) {
+      const handleScroll = () => {
+        let currentActiveHeading = null;
+
+        Object.values(tutorialSections).forEach((section) => {
+          if (section) {
+            const sectionElement = document.getElementById(section);
+            const rect = sectionElement.getBoundingClientRect();
+            const viewportHeight = window.innerHeight;
+
+            // Check if the heading is in view (adjust threshold as needed)
+            const isInView = rect.top <= viewportHeight * 0.3; // Example: 25% from top and bottom
+
+            if (isInView) {
+              currentActiveHeading = section;
+            }
+          }
+        });
+
+        setSelectedSection(currentActiveHeading);
+      };
+
+      div.addEventListener('scroll', handleScroll);
+      handleScroll(); // Initial check
+
+      return () => {
+        div.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+
   return (
     <section className={cx(lightMode ? styles.light : '', styles.container)}>
       <span className={styles.sectionTitle}>{t('Tutorials & Help')}</span>
@@ -33,7 +70,9 @@ function TutorialsComponent() {
           <h3>Jump to:</h3>
           <button
             type="button"
-            className={styles.section}
+            className={cx(styles.section, {
+              [styles.selected]: selectedSection === tutorialSections.SPECIES,
+            })}
             onClick={() => goToSection(tutorialSections.SPECIES)}
           >
             {t('Species')}
@@ -42,6 +81,10 @@ function TutorialsComponent() {
             <li>
               <button
                 type="button"
+                className={cx({
+                  [styles.selected]:
+                    selectedSection === tutorialSections.DATA_LAYERS,
+                })}
                 onClick={() => goToSection(tutorialSections.DATA_LAYERS)}
               >
                 {t('Data Layers')}
@@ -50,6 +93,10 @@ function TutorialsComponent() {
             <li>
               <button
                 type="button"
+                className={cx({
+                  [styles.selected]:
+                    selectedSection === tutorialSections.INDICATOR_SCORES,
+                })}
                 onClick={() => goToSection(tutorialSections.INDICATOR_SCORES)}
               >
                 {t('Indicator Scores')}
@@ -58,14 +105,19 @@ function TutorialsComponent() {
           </ul>
           <button
             type="button"
-            className={styles.section}
+            className={cx(styles.section, {
+              [styles.selected]: selectedSection === tutorialSections.REGIONS,
+            })}
             onClick={() => goToSection(tutorialSections.REGIONS)}
           >
             {t('Regions')}
           </button>
           <button
             type="button"
-            className={styles.section}
+            className={cx(styles.section, {
+              [styles.selected]:
+                selectedSection === tutorialSections.INDICATORS,
+            })}
             onClick={() => goToSection(tutorialSections.INDICATORS)}
           >
             {t('Indicators')}
@@ -74,6 +126,9 @@ function TutorialsComponent() {
             <li>
               <button
                 type="button"
+                className={cx({
+                  [styles.selected]: selectedSection === tutorialSections.SPI,
+                })}
                 onClick={() => goToSection(tutorialSections.SPI)}
               >
                 {t('Species Protection Index')}
@@ -82,6 +137,9 @@ function TutorialsComponent() {
             <li>
               <button
                 type="button"
+                className={cx({
+                  [styles.selected]: selectedSection === tutorialSections.SHI,
+                })}
                 onClick={() => goToSection(tutorialSections.SHI)}
               >
                 {t('Species Habitat Index')}
@@ -90,6 +148,9 @@ function TutorialsComponent() {
             <li>
               <button
                 type="button"
+                className={cx({
+                  [styles.selected]: selectedSection === tutorialSections.SII,
+                })}
                 onClick={() => goToSection(tutorialSections.SII)}
               >
                 {t('Species Informcation Index')}
@@ -97,7 +158,7 @@ function TutorialsComponent() {
             </li>
           </ul>
         </nav>
-        <div className={styles.information}>
+        <div ref={divRef} className={styles.information}>
           <SpeciesSectionComponent />
           <RegionsSectionComponent />
           <IndicatorsSectionComponent />
