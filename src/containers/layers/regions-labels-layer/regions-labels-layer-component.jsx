578c1c39c23dcdf040c300dc7459078f77665080
Chris Manciero <cmanciero@gmail.com>
2025-02-06 13:42:22 -0500

    removed unused font

--- src/containers/layers/regions-labels-layer/regions-labels-layer-component.jsx
+++ src/containers/layers/regions-labels-layer/regions-labels-layer-component.jsx
@@ -25,7 +25,6 @@ function RegionsLabelsLayerComponent(props) {
         type: 'text',
         color: [213, 207, 202],
         font: {
-          family: 'Helvetica',
           size: 10,
           weight: 'normal',
         },
