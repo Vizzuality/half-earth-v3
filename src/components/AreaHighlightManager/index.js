85ce6e9306798b8f97b535e0197b2ad5a38e34e9
Chris Manciero <cmanciero@gmail.com>
2025-02-19 09:08:14 -0500

    create area hover/click manager

---
+++ src/components/AreaHighlightManager/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import AreaHighlightManagerComponent from './area-highlight-manager-component';
+
+function AreaHighlightManager(props) {
+  return <AreaHighlightManagerComponent {...props} />;
+}
+
+export default AreaHighlightManager;
