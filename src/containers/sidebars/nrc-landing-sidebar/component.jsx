e7fc1f8c821c22623516a0681bc36e95a5759c7f
Chris Manciero <cmanciero@gmail.com>
2025-04-03 17:22:26 -0400

    search layer updates

--- src/containers/sidebars/nrc-landing-sidebar/component.jsx
+++ src/containers/sidebars/nrc-landing-sidebar/component.jsx
@@ -42,7 +42,7 @@ function NRCLandingSidebar({
       id: GLOBAL_SPI_FEATURE_LAYER,
       title: GLOBAL_SPI_FEATURE_LAYER,
       slug: GLOBAL_SPI_FEATURE_LAYER, // TODO: change specific slug for metadata
-      portalId: 'd777027d86e4487d8c2ca1c13de471dc',
+      portalId: 'c34d59ba3de244b4abd18c00986a0bb2',
       metadataTitle: t('Land SPI'),
     },
     {
