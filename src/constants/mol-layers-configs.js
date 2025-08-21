6e0e40d016717d07131ac2bb0990e9ee06d389d2
Chris Manciero <cmanciero@gmail.com>
2025-08-21 14:56:27 -0400

    use new layer for bestPlaces

--- src/constants/mol-layers-configs.js
+++ src/constants/mol-layers-configs.js
@@ -333,6 +333,7 @@ export const layersConfig = {
     slug: FEATURED_PLACES_LAYER,
     type: LAYER_TYPES.FEATURE_LAYER,
     url: LAYERS_URLS[FEATURED_PLACES_LAYER],
+    portalId: '51f5377402444b04b0a48b223d7431a3',
     bbox: null,
   },
   [DISCOVER_PLACES_LAYER]: {
